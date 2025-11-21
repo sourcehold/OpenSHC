69a9bd5579596f2a3c56970c63ee038823879dab
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2025-11-21 15:04:46 +0100

    [PROJECT] remove auto format from dependencies and format headers a bit

--- dependencies/recreated/include/Mss32.h
+++ dependencies/recreated/include/Mss32.h
@@ -7,7 +7,6 @@
 */
 
 #include <windows.h>
-
 #include <mmsystem.h>
 
 struct _DIG_DRIVER {
@@ -25,7 +24,7 @@ typedef _SAMPLE* HSAMPLE;
 #define AIL_NO_ERROR 0
 
 // SHC: Maybe also used for STREAM status?
-#define SMP_PLAYING 0x0004 // Sample is playing
+#define SMP_PLAYING 0x0004              // Sample is playing
 
 // SHC: Format, used by set_sample_type and AIL_minimum_sample_buffer_size
 #define DIG_F_16BITS_MASK 1
@@ -35,62 +34,51 @@ typedef _SAMPLE* HSAMPLE;
 // SHC: Also present in mmsystem header
 #define WAVE_FORMAT_PCM 1
 
-extern "C" __declspec(dllimport) void* __stdcall AIL_mem_alloc_lock(unsigned int size);
-extern "C" __declspec(dllimport) void __stdcall AIL_mem_free_lock(void* ptr);
+extern "C" __declspec(dllimport) void*          __stdcall AIL_mem_alloc_lock                (unsigned int size);
+extern "C" __declspec(dllimport) void           __stdcall AIL_mem_free_lock                 (void* ptr);
 
-extern "C" __declspec(dllimport) int __stdcall AIL_file_size(const char* filename);
-extern "C" __declspec(dllimport) void* __stdcall AIL_file_read(const char* filename, void* dest);
+extern "C" __declspec(dllimport) int            __stdcall AIL_file_size                     (const char* filename);
+extern "C" __declspec(dllimport) void*          __stdcall AIL_file_read                     (const char* filename, void* dest);
 
-extern "C" __declspec(dllimport) int __stdcall AIL_startup();
-extern "C" __declspec(dllimport) void __stdcall AIL_shutdown();
-extern "C" __declspec(dllimport) void __stdcall AIL_close_digital_driver(HDIGDRIVER dig);
-extern "C" __declspec(dllimport) int __stdcall AIL_waveOutOpen(HDIGDRIVER* drvr, LPHWAVEOUT* lphWaveOut, int wDeviceID,
-    LPWAVEFORMATEX lpFormat); // SHC: unknown where "-1" for device ID comes from, did not find defined value
-extern "C" __declspec(dllimport) char* __stdcall AIL_set_redist_directory(
-    const char* dir); // SHC: uses "miles", no idea from where
+extern "C" __declspec(dllimport) int            __stdcall AIL_startup                       ();
+extern "C" __declspec(dllimport) void           __stdcall AIL_shutdown                      ();
+extern "C" __declspec(dllimport) void           __stdcall AIL_close_digital_driver          (HDIGDRIVER dig);
+extern "C" __declspec(dllimport) int            __stdcall AIL_waveOutOpen                   (HDIGDRIVER* drvr, LPHWAVEOUT* lphWaveOut, int wDeviceID, LPWAVEFORMATEX lpFormat);             // SHC: unknown where "-1" for device ID comes from, did not find defined value
+extern "C" __declspec(dllimport) char*          __stdcall AIL_set_redist_directory          (const char* dir);                                                                              // SHC: uses "miles", no idea from where
 
-extern "C" __declspec(dllimport) HSAMPLE __stdcall AIL_allocate_sample_handle(HDIGDRIVER dig);
-extern "C" __declspec(dllimport) void __stdcall AIL_release_sample_handle(HSAMPLE S);
-extern "C" __declspec(dllimport) void __stdcall AIL_init_sample(HSAMPLE S);
-extern "C" __declspec(dllimport) int __stdcall AIL_set_named_sample_file(
-    HSAMPLE S, const char* file_type_suffix, const void* file_image, int file_size, int block);
-extern "C" __declspec(dllimport) void __stdcall AIL_set_sample_type(HSAMPLE S, int format, unsigned int flags);
-extern "C" __declspec(dllimport) void __stdcall AIL_start_sample(HSAMPLE S);
-extern "C" __declspec(dllimport) void __stdcall AIL_stop_sample(HSAMPLE S);
-extern "C" __declspec(dllimport) void __stdcall AIL_resume_sample(HSAMPLE S);
-extern "C" __declspec(dllimport) void __stdcall AIL_end_sample(HSAMPLE S);
-extern "C" __declspec(dllimport) void __stdcall AIL_set_sample_playback_rate(HSAMPLE S, int playback_rate);
-extern "C" __declspec(dllimport) void __stdcall AIL_set_sample_volume(HSAMPLE S, int volume); // Sample volume 0-127
-extern "C" __declspec(dllimport) void __stdcall AIL_set_sample_pan(
-    HSAMPLE S, int pan); // Mono panpot/stereo balance (0=L ... 127=R)
-extern "C" __declspec(dllimport) void __stdcall AIL_set_sample_loop_count(
-    HSAMPLE S, int loop_count); // # of cycles-1 (1=one-shot, 0=indefinite)
-extern "C" __declspec(dllimport) unsigned int __stdcall AIL_sample_status(HSAMPLE s);
-extern "C" __declspec(dllimport) int __stdcall AIL_minimum_sample_buffer_size(
-    HDIGDRIVER dig, int playback_rate, int format);
-extern "C" __declspec(dllimport) int __stdcall AIL_sample_buffer_ready(
-    HSAMPLE S); // SHC: code suggests the returned value is the -1 if not, or buffer number is yes
-extern "C" __declspec(dllimport) void __stdcall AIL_load_sample_buffer(
-    HSAMPLE S, unsigned int buff_num, const void* buffer, unsigned int len);
+extern "C" __declspec(dllimport) HSAMPLE        __stdcall AIL_allocate_sample_handle        (HDIGDRIVER dig);
+extern "C" __declspec(dllimport) void           __stdcall AIL_release_sample_handle         (HSAMPLE S);
+extern "C" __declspec(dllimport) void           __stdcall AIL_init_sample                   (HSAMPLE S);
+extern "C" __declspec(dllimport) int            __stdcall AIL_set_named_sample_file         (HSAMPLE S, const char* file_type_suffix, const void* file_image, int file_size, int block);
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_sample_type               (HSAMPLE S, int format, unsigned int flags);
+extern "C" __declspec(dllimport) void           __stdcall AIL_start_sample                  (HSAMPLE S);
+extern "C" __declspec(dllimport) void           __stdcall AIL_stop_sample                   (HSAMPLE S);
+extern "C" __declspec(dllimport) void           __stdcall AIL_resume_sample                 (HSAMPLE S);
+extern "C" __declspec(dllimport) void           __stdcall AIL_end_sample                    (HSAMPLE S);
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_sample_playback_rate      (HSAMPLE S, int playback_rate);
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_sample_volume             (HSAMPLE S, int volume);                                                                        // Sample volume 0-127
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_sample_pan                (HSAMPLE S, int pan);                                                                           // Mono panpot/stereo balance (0=L ... 127=R)
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_sample_loop_count         (HSAMPLE S, int loop_count);                                                                    // # of cycles-1 (1=one-shot, 0=indefinite)
+extern "C" __declspec(dllimport) unsigned int   __stdcall AIL_sample_status                 (HSAMPLE s);
+extern "C" __declspec(dllimport) int            __stdcall AIL_minimum_sample_buffer_size    (HDIGDRIVER dig, int playback_rate, int format);
+extern "C" __declspec(dllimport) int            __stdcall AIL_sample_buffer_ready           (HSAMPLE S);                                                                                    // SHC: code suggests the returned value is the -1 if not, or buffer number is yes
+extern "C" __declspec(dllimport) void           __stdcall AIL_load_sample_buffer            (HSAMPLE S, unsigned int buff_num, const void* buffer, unsigned int len);
 
 struct _STREAM {
     // ... object much bigger
 };
 typedef _STREAM* HSTREAM;
 
-extern "C" __declspec(dllimport) HSTREAM __stdcall AIL_open_stream(
-    HDIGDRIVER dig, const char* filename, int stream_mem); // SHC: 0 used for mem (default, or no buffer?)
-extern "C" __declspec(dllimport) void __stdcall AIL_close_stream(HSTREAM stream);
-extern "C" __declspec(dllimport) void __stdcall AIL_start_stream(HSTREAM stream);
-extern "C" __declspec(dllimport) void __stdcall AIL_pause_stream(HSTREAM stream, int onoff);
-extern "C" __declspec(dllimport) void __stdcall AIL_set_stream_volume(
-    HSTREAM stream, int volume); // SHC: Same as sample volume?
-extern "C" __declspec(dllimport) void __stdcall AIL_set_stream_loop_count(
-    HSTREAM stream, unsigned int count); // SHC: Same as sample loop count?
-extern "C" __declspec(dllimport) int __stdcall AIL_stream_status(HSTREAM stream);
+extern "C" __declspec(dllimport) HSTREAM        __stdcall AIL_open_stream                   (HDIGDRIVER dig, const char* filename, int stream_mem);                                         // SHC: 0 used for mem (default, or no buffer?)
+extern "C" __declspec(dllimport) void           __stdcall AIL_close_stream                  (HSTREAM stream);
+extern "C" __declspec(dllimport) void           __stdcall AIL_start_stream                  (HSTREAM stream);
+extern "C" __declspec(dllimport) void           __stdcall AIL_pause_stream                  (HSTREAM stream, int onoff);
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_stream_volume             (HSTREAM stream, int volume);                                                                   // SHC: Same as sample volume?
+extern "C" __declspec(dllimport) void           __stdcall AIL_set_stream_loop_count         (HSTREAM stream, unsigned int count);                                                           // SHC: Same as sample loop count?
+extern "C" __declspec(dllimport) int            __stdcall AIL_stream_status                 (HSTREAM stream);
 
 // SHC: Needs implementation
-int __cdecl MSS_auto_cleanup();
-#define AIL_startup() (MSS_auto_cleanup(), AIL_startup())
+                                 int            __cdecl   MSS_auto_cleanup                  ();
+#define                                                   AIL_startup()                     (MSS_auto_cleanup(), AIL_startup())
 
 #endif // MSS32
--- dependencies/recreated/include/binkw32.h
+++ dependencies/recreated/include/binkw32.h
@@ -7,43 +7,43 @@
 */
 
 struct BINK {
-    unsigned int Width; // Width (1 based, 640 for example)
-    unsigned int Height; // Height (1 based, 480 for example)
-    unsigned int Frames; // Number of frames (1 based, 100 = 100 frames)
-    unsigned int FrameNum; // Frame to *be* displayed (1 based)
+    unsigned int Width;                 // Width (1 based, 640 for example)
+    unsigned int Height;                // Height (1 based, 480 for example)
+    unsigned int Frames;                // Number of frames (1 based, 100 = 100 frames)
+    unsigned int FrameNum;              // Frame to *be* displayed (1 based)
     // ... object much bigger
 };
 typedef BINK* HBINK;
 
 // SHC: Only flag used by BinkOpen
-#define BINKNOSKIP 0x00080000L // Don't skip frames if falling behind
+#define BINKNOSKIP 0x00080000L          // Don't skip frames if falling behind
 
 // SHC: Only flag next to the surface type used by BinkCopyToBuffer
-#define BINKCOPYALL 0x80000000L // copy all pixels (not just changed)
+#define BINKCOPYALL 0x80000000L         // copy all pixels (not just changed)
 
 // SHC: Only possible surfaces returned by BinkDDSurfaceType
-#define BINKSURFACE555 9
+#define BINKSURFACE555 9 
 #define BINKSURFACE565 10
 
-extern "C" __declspec(dllimport) HBINK __stdcall BinkOpen(const char* name, unsigned int flags);
-extern "C" __declspec(dllimport) int __stdcall BinkDoFrame(HBINK bnk);
-extern "C" __declspec(dllimport) void __stdcall BinkNextFrame(HBINK bnk);
-extern "C" __declspec(dllimport) int __stdcall BinkWait(HBINK bnk);
-extern "C" __declspec(dllimport) void __stdcall BinkClose(HBINK bnk);
-extern "C" __declspec(dllimport) int __stdcall BinkPause(HBINK bnk, int pause);
-extern "C" __declspec(dllimport) int __stdcall BinkCopyToBuffer(
-    HBINK bnk, void* dest, int destpitch, int destheight, int destx, int desty, unsigned int flags);
-extern "C" __declspec(dllimport) void __stdcall BinkSetVolume(HBINK bnk, unsigned int trackid, int volume);
+extern "C" __declspec(dllimport) HBINK          __stdcall BinkOpen              (const char* name, unsigned int flags);
+extern "C" __declspec(dllimport) int            __stdcall BinkDoFrame           (HBINK bnk);
+extern "C" __declspec(dllimport) void           __stdcall BinkNextFrame         (HBINK bnk);
+extern "C" __declspec(dllimport) int            __stdcall BinkWait              (HBINK bnk);
+extern "C" __declspec(dllimport) void           __stdcall BinkClose             (HBINK bnk);
+extern "C" __declspec(dllimport) int            __stdcall BinkPause             (HBINK bnk, int pause);
+extern "C" __declspec(dllimport) int            __stdcall BinkCopyToBuffer      (HBINK bnk, void* dest, int destpitch, int destheight, int destx, int desty, unsigned int flags);
+extern "C" __declspec(dllimport) void           __stdcall BinkSetVolume         (HBINK bnk, unsigned int trackid, int volume);
 
-struct BINKSND { };
-typedef unsigned int(__stdcall* BINKSNDOPEN)(
-    BINKSND* BnkSnd, unsigned int freq, int bits, int chans, unsigned int flags, HBINK bink);
-typedef BINKSNDOPEN(__stdcall* BINKSNDSYSOPEN)(unsigned int param);
+struct BINKSND {
+    // ... object much bigger
+};
+typedef                          unsigned int   (__stdcall* BINKSNDOPEN)        (BINKSND* BnkSnd, unsigned int freq, int bits, int chans, unsigned int flags, HBINK bink);
+typedef                          BINKSNDOPEN    (__stdcall* BINKSNDSYSOPEN)     (unsigned int param);
 
-extern "C" __declspec(dllimport) int __stdcall BinkSetSoundSystem(BINKSNDSYSOPEN open, unsigned int param);
-extern "C" __declspec(dllimport) BINKSNDOPEN __stdcall BinkOpenMiles(unsigned int param);
-#define BinkSoundUseMiles(hdigdriver) BinkSetSoundSystem(BinkOpenMiles, (unsigned int)hdigdriver)
+extern "C" __declspec(dllimport) int            __stdcall BinkSetSoundSystem    (BINKSNDSYSOPEN open, unsigned int param);
+extern "C" __declspec(dllimport) BINKSNDOPEN    __stdcall BinkOpenMiles         (unsigned int param);
+#define BinkSoundUseMiles(hdigdriver)                     BinkSetSoundSystem    (BinkOpenMiles, (unsigned int)hdigdriver)
 
-extern "C" __declspec(dllimport) int __stdcall BinkDDSurfaceType(void* lpDDS);
+extern "C" __declspec(dllimport) int            __stdcall BinkDDSurfaceType     (void* lpDDS);
 
 #endif // BINKW32
