86deb84e474da53d1b4e64432d0804781c917232
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-05-15 21:50:04 +0200

    reimplement: SHC_3BB0A8C1_0x00479B70 experiment with bitfields

--- src/OpenSHC/Audio/SoundSystem/meth_0x479b70.cpp
+++ src/OpenSHC/Audio/SoundSystem/meth_0x479b70.cpp
@@ -11,19 +11,15 @@ namespace Audio {
                 return;
             }
 
-            if ((*((unsigned int*)&this->streamFlagsUnkAndLoopCount_0x34[3]) & 0x20000000)
-                && this->streamActiveUnk_0x20[3] && AIL_stream_status(this->stream_0xc[3]) != SMP_PLAYING) {
-                unsigned int const copyWithoutFlag
-                    = *((unsigned int*)&this->streamFlagsUnkAndLoopCount_0x34[3]) & 0xdfffffff;
-                this->streamFlagsUnkAndLoopCount_0x34[3] = *((UnkSoundFlagsAndLoopCount*)&copyWithoutFlag);
+            if (this->streamFlagsUnkAndLoopCount_0x34[3].unknownFlag1 && this->streamActiveUnk_0x20[3]
+                && AIL_stream_status(this->stream_0xc[3]) != SMP_PLAYING) {
+                this->streamFlagsUnkAndLoopCount_0x34[3].unknownFlag1 = false;
                 AIL_set_sample_volume(
                     this->musicSampleUnk_0x170, (this->streamVolume[0] * this->streamFileVolumeNextUnk_0x48[0]) / 100);
             }
-            if ((*((unsigned int*)&this->streamFlagsUnkAndLoopCount_0x34[4]) & 0x20000000)
-                && this->streamActiveUnk_0x20[4] && AIL_stream_status(this->stream_0xc[4]) != SMP_PLAYING) {
-                unsigned int const copyWithoutFlag
-                    = *((unsigned int*)&this->streamFlagsUnkAndLoopCount_0x34[4]) & 0xdfffffff;
-                this->streamFlagsUnkAndLoopCount_0x34[4] = *((UnkSoundFlagsAndLoopCount*)&copyWithoutFlag);
+            if (this->streamFlagsUnkAndLoopCount_0x34[4].unknownFlag1 && this->streamActiveUnk_0x20[4]
+                && AIL_stream_status(this->stream_0xc[4]) != SMP_PLAYING) {
+                this->streamFlagsUnkAndLoopCount_0x34[4].unknownFlag1 = false;
                 AIL_set_sample_volume(
                     this->musicSampleUnk_0x170, (this->streamVolume[0] * this->streamFileVolumeNextUnk_0x48[0]) / 100);
             }
