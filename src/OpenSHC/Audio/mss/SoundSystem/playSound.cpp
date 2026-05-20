742d5f24cc1113144435c5ab526a99614963b3ae
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-05-20 21:46:40 +0200

    reimplement: SHC_3BB0A8C1_0x00479DF0 100%

---
+++ src/OpenSHC/Audio/mss/SoundSystem/playSound.cpp
@@ -0,0 +1,25 @@
+#include "OpenSHC/Audio/MSS/SoundSystem.func.hpp"
+
+#include "OpenSHC/string-literals.hpp"
+
+namespace OpenSHC {
+namespace Audio {
+    namespace MSS {
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x00479DF0
+        void SoundSystem::playSound(int soundIndex)
+        {
+            if (!this->waveOutOpenUnk_0x8 || !this->soundActiveUnk_0x0
+                || this->soundFileCurrSampleNum_0x28c[soundIndex] < 0) {
+                return;
+            }
+
+            AIL_set_named_sample_file(this->sample_0x190[this->soundFileCurrSampleNum_0x28c[soundIndex] + -1],
+                s__wav_005a6cc4, this->soundFileDataPointerArray_0x122c[soundIndex],
+                this->soundFileSizes_0x21cc[soundIndex], 0);
+            AIL_start_sample(this->sample_0x190[this->soundFileCurrSampleNum_0x28c[soundIndex] + -1]);
+        }
+
+    }
+}
+}
