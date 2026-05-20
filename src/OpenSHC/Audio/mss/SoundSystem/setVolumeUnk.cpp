3901581b68b73bdba0fd3bb1cb3c2240406062ba
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-05-20 22:18:37 +0200

    reimplement: SHC_3BB0A8C1_0x00479E60 100%

---
+++ src/OpenSHC/Audio/mss/SoundSystem/setVolumeUnk.cpp
@@ -0,0 +1,39 @@
+#include "OpenSHC/Audio/MSS/SoundSystem.func.hpp"
+
+namespace OpenSHC {
+namespace Audio {
+    namespace MSS {
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x00479E60
+        void SoundSystem::setVolumeUnk(int streamIndex, int volume)
+        {
+            if (!this->waveOutOpenUnk_0x8) {
+                return;
+            }
+
+            if (streamIndex == -1) {
+                this->sampleVolume = volume;
+                for (int i = 0; i < 31; ++i) {
+                    // TODO: Most structures are either 32 or 1 and 31. We need to unify these.
+                    //   Either everything else is of or sample should take in the int value before.
+                    AIL_set_sample_volume(
+                        this->sample_0x190[i], (this->sampleSndStructVolumePercentage_0x3174[i + 1] * volume) / 100);
+                }
+                return;
+            }
+            this->streamVolume[streamIndex] = volume;
+            if (!this->streamActiveUnk_0x20[streamIndex]) {
+                return;
+            }
+            if (streamIndex == 0) {
+                AIL_set_sample_volume(
+                    this->musicSampleUnk_0x170, (this->streamFileVolumeCurrentUnk_0x5c[0] * volume) / 100);
+                return;
+            }
+            AIL_set_stream_volume(
+                this->stream_0xc[streamIndex], (this->streamFileVolumeCurrentUnk_0x5c[streamIndex] * volume) / 100);
+        }
+
+    }
+}
+}
