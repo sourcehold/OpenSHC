3bbcdca18f82ed985399de6e1d2f5e2c1cd3e6e4
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-05-21 20:55:04 +0200

    reimplement: SHC_3BB0A8C1_0x00479E60 improve comment

--- src/OpenSHC/Audio/mss/SoundSystem/setVolumeUnk.cpp
+++ src/OpenSHC/Audio/mss/SoundSystem/setVolumeUnk.cpp
@@ -14,8 +14,10 @@ namespace Audio {
             if (streamIndex == -1) {
                 this->sampleVolume = volume;
                 for (int i = 0; i < 31; ++i) {
-                    // TODO: Most structures are either 32 or 1 and 31. We need to unify these.
-                    //   Either everything else is of or sample should take in the int value before.
+                    // TODO: A lot of structures of SoundSystem use an 32 element array. sample_0x190 for example
+                    //   uses 31 and might be "missing" its first element. The 0 seems to be used for "something" else
+                    //   in general. So either we should one day treat all these as "a value and a 31 element array" or
+                    //   like a single 32 array.
                     AIL_set_sample_volume(
                         this->sample_0x190[i], (this->sampleSndStructVolumePercentage_0x3174[i + 1] * volume) / 100);
                 }
