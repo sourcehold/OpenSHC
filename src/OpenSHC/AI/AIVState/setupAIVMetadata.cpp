8e54bea50239220e45f295bd7ccfcf74950f72d5
sourcehold <sourcehold@users.noreply.github.com>
2026-05-22 13:52:03 +0200

    improve loop

--- src/OpenSHC/AI/AIVState/setupAIVMetadata.cpp
+++ src/OpenSHC/AI/AIVState/setupAIVMetadata.cpp
@@ -9,29 +9,25 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004ECEF0
     int AIVState::setupAIVMetadata(int playerID)
     {
-        int aivID = 1;
-        AIVSpec* pSlot = &this->SEC_AIVS[1];
-        do {
-            if (pSlot->playerID == 0) {
-                this->SEC_AIVS[aivID].playerID = playerID;
-
-                int aiType = DAT_GameState::ptr->playerDataArray[playerID].aiType;
-                this->SEC_AIVS[aivID].aiType = aiType;
-
-                this->SEC_AIVS[aivID].currentStepGoal = 0;
-                this->SEC_AIVS[aivID].aivPoorCounter = 0;
-                this->SEC_AIVS[aivID].aivSubType = 0;
-
-                this->SEC_AIVS[aivID].aivPoorLimit_OR_AIC_buildInterval
-                    = MACRO_CALL_MEMBER(AICState_Func::getAIBuildInterval, DAT_AICState::ptr)(playerID);
-
-                this->aivCount = this->aivCount + 1;
-
-                return aivID;
+        for (int aivID = 1; aivID < 9; ++aivID) {
+            if (this->SEC_AIVS[aivID].playerID != 0) {
+                continue;
             }
-            aivID = aivID + 1;
-            pSlot = pSlot + 1;
-        } while (aivID < 9);
+
+            this->SEC_AIVS[aivID].playerID = playerID;
+
+            this->SEC_AIVS[aivID].aiType = DAT_GameState::ptr->playerDataArray[playerID].aiType;
+
+            this->SEC_AIVS[aivID].currentStepGoal = 0;
+            this->SEC_AIVS[aivID].aivPoorCounter = 0;
+            this->SEC_AIVS[aivID].aivSubType = 0;
+
+            this->SEC_AIVS[aivID].aivPoorLimit_OR_AIC_buildInterval
+                = MACRO_CALL_MEMBER(AICState_Func::getAIBuildInterval, DAT_AICState::ptr)(playerID);
+
+            ++this->aivCount;
+            return aivID;
+        }
         return 0;
     }
 
