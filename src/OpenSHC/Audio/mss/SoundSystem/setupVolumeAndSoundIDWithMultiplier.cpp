88f9a0e30267e4a01581e1a0486560bd574a8624
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-05-31 22:34:09 +0200

    reimplement: SHC_3BB0A8C1_0x0047A220 100%

---
+++ src/OpenSHC/Audio/mss/SoundSystem/setupVolumeAndSoundIDWithMultiplier.cpp
@@ -0,0 +1,39 @@
+#include "../SoundSystem.func.hpp"
+
+#include "OpenSHC/Audio/SFX/SFXState.func.hpp"
+#include "OpenSHC/DE/SHCDE/eMusicIDs.hpp"
+
+#include "OpenSHC/Globals/DAT_GameCore.hpp"
+#include "OpenSHC/Globals/DAT_SFXDefinedData.hpp"
+#include "OpenSHC/Globals/DAT_SFXState.hpp"
+
+namespace OpenSHC {
+namespace Audio {
+    namespace MSS {
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x0047A220
+        void SoundSystem::setupVolumeAndSoundIDWithMultiplier(eMusicIDsInt soundID, int soundMultiplier)
+        {
+            // NOTE: Is this certain that these ids fit to the original game?
+            if (soundID == DE::SHCDE::MUSIC_TUNE_NARR1) {
+                // missionNumber1to20 might be unsigned, but found signs inconclusive
+                if (DAT_GameCore::ptr->missionNumber1to20 - 1U <= 19) {
+                    soundID = DAT_GameCore::ptr->missionNumber1to20 + 46;
+                } else {
+                    soundID = DE::SHCDE::MUSIC_TUNE_OFF;
+                }
+            }
+
+            if (soundID == this->currentSoundID_0x3278) {
+                return;
+            }
+            this->currentSoundID_0x3278 = soundID;
+            int const fileVolume = MACRO_CALL_MEMBER(SFX::SFXState_Func::getSoundVolumeForFilename, DAT_SFXState::ptr)(
+                DAT_SFXDefinedData::ptr->DAT_SFX_Pointers[soundID].musicFile);
+            this->currentSoundIDVolumeUnk_0x327c = (fileVolume * soundMultiplier) / 0x7f;
+            this->mbr_0x3280 = 0;
+        }
+
+    }
+}
+}
