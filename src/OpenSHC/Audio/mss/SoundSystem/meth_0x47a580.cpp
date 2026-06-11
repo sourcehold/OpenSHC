c6f958c77c523d43b4849394be9062731ae5dec0
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-11 21:50:48 +0200

    reimplement: SHC_3BB0A8C1_0x0047A580 100%

---
+++ src/OpenSHC/Audio/mss/SoundSystem/meth_0x47a580.cpp
@@ -0,0 +1,27 @@
+#include "../SoundSystem.func.hpp"
+
+#include "OpenSHC/Globals/DAT_SoundEffectsHelperData1.hpp"
+
+namespace OpenSHC {
+namespace Audio {
+    namespace MSS {
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x0047A580
+        void SoundSystem::meth_0x47a580()
+        {
+            if (DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.field0_0x0 == 5
+                && DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.field6_0x18 == 0
+                && DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.troopValueLevel != 0
+                && DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.musicTracker == 0
+                && DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.volumeLevel != 1
+                && DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.field7_0x1c == 0) {
+                DAT_SoundEffectsHelperData1::ptr->SEC_Section1079.musicTracker = 1;
+                MACRO_CALL_MEMBER(SoundSystem_Func::setSomeSoundTime, this)();
+                MACRO_CALL_MEMBER(SoundSystem_Func::setupVolumeAndSoundIDWithMultiplier, this)(
+                    DE::SHCDE::MUSIC_TUNE_BATTLE_L2_GLORY1, 100);
+            }
+        }
+
+    }
+}
+}
