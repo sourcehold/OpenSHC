a6898876f150a39e39ea6225b0c1e6d698e1953d
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-15 19:28:28 +0200

    reimplement: SHC_3BB0A8C1_0x0047A660 fix enum

--- src/OpenSHC/Audio/mss/SoundSystem/setupLossMusic.cpp
+++ src/OpenSHC/Audio/mss/SoundSystem/setupLossMusic.cpp
@@ -10,7 +10,8 @@ namespace Audio {
         void SoundSystem::setupLossMusic()
         {
             MACRO_CALL_MEMBER(SoundSystem_Func::setupVolumeAndSoundIDWithMultiplier, this)(
-                DAT_SoundEffectsHelperData1::ptr->DAT_LossMusicVariation + DE::SHCDE::MUSIC_TUNE_LOSE1, 100);
+                (eMusicIDs)(DAT_SoundEffectsHelperData1::ptr->DAT_LossMusicVariation + DE::SHCDE::MUSIC_TUNE_LOSE1),
+                100);
             ++DAT_SoundEffectsHelperData1::ptr->DAT_LossMusicVariation;
             if (DAT_SoundEffectsHelperData1::ptr->DAT_LossMusicVariation > 1) {
                 DAT_SoundEffectsHelperData1::ptr->DAT_LossMusicVariation = 0;
