3893ef702a5e064fd328a5e817015d9df113cfa6
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-16 21:02:33 +0200

    [EXPERIMENT/STRUCT_RESOLVER] add general structure to handle asm files

--- src/precomp/StructResolver.h
+++ src/precomp/StructResolver.h
@@ -7,6 +7,10 @@ struct StructResolver {
 private:
     template <typename T, int gameAddress> struct Instance;
 
+    template <typename T, int gameAddress> struct Extern {
+        static T instance;
+    };
+
     template <typename T, bool implemented, int gameAddress> struct InternalResolver;
     template <typename T, int gameAddress> struct InternalResolver<T, true, gameAddress> {
         static T* const ptr;
@@ -39,7 +43,8 @@ public:
 };
 
 template <typename T, int gameAddress>
-T* const StructResolver::InternalResolver<T, false, gameAddress>::ptr = reinterpret_cast<T*>(gameAddress);
+T* const StructResolver::InternalResolver<T, false, gameAddress>::ptr
+    = &StructResolver::Extern<T, gameAddress>::instance;
 
 template <typename T, int gameAddress>
 T* const StructResolver::InternalResolver<T, true, gameAddress>::ptr
@@ -56,6 +61,13 @@ StructResolver::Resolver<T, implemented, gameAddress>::Initializer::Initializer(
 {
     initialize(AddressUsageKeeper<gameAddress>::initialized, isImplemented, gameAddress, Ptr::ptr, getTypeName<T>());
 }
+
+// TODO: rather then adding a template to hold the ref which would require a long mangled name,
+//   if we have a generatable name, we use a macro to define an extern C and use this instead.
+//   In this case, the asm file only needs to be touched if the general constract of the address changes
+//   and should we only ever use the addresses of the globals, this might be never.
+//   However, if filtered and generated names are required, maybe it could also be possible to create a generator for
+//   the more complicated mangled names? Any general type change would need to also adept the ASM file.
 
 #define MACRO_STRUCT_RESOLVER(STRUCT_TYPE, IMPLEMENTED, GAME_ADDRESS)                                                  \
     template struct StructResolver::Resolver<STRUCT_TYPE, IMPLEMENTED, GAME_ADDRESS>;                                  \
