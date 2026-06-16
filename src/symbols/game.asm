6c03d48477ad74fc4e2f966ba9d62841b83424ab
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-16 21:02:33 +0200

    [EXPERIMENT/STRUCT_RESOLVER] add general structure to handle asm files

---
+++ src/symbols/game.asm
@@ -0,0 +1,10 @@
+.386
+.model flat
+
+PUBLIC ?instance@?$Extern@USoundEffectsHelperData1@Audio@OpenSHC@@$0NPDHPA@@StructResolver@@2USoundEffectsHelperData1@Audio@OpenSHC@@A
+
+_TEXT SEGMENT
+?instance@?$Extern@USoundEffectsHelperData1@Audio@OpenSHC@@$0NPDHPA@@StructResolver@@2USoundEffectsHelperData1@Audio@OpenSHC@@A EQU 00DF37F0h
+_TEXT ENDS
+
+END
