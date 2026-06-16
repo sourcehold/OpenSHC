dbb3a59a82b7d96528c3cb3678923c398d0f5947
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-16 15:26:41 +0200

    [SoundSystem] rename local temporary for sound id

--- src/OpenSHC/Audio/mss/SoundSystem/setupVolumeAndSoundID.cpp
+++ src/OpenSHC/Audio/mss/SoundSystem/setupVolumeAndSoundID.cpp
@@ -14,26 +14,26 @@ namespace Audio {
         // FUNCTION: STRONGHOLDCRUSADER 0x0047A1B0
         void SoundSystem::setupVolumeAndSoundID(eMusicIDs soundID)
         {
-            int soundIDTemp = (int)soundID;
+            int soundIDInt = (int)soundID;
 
             // Indicator to handle as mission sound (?)
-            if (soundIDTemp == DE::SHCDE::MUSIC_TUNE_NARR1) {
+            if (soundIDInt == DE::SHCDE::MUSIC_TUNE_NARR1) {
                 // missionNumber1to20 might be unsigned, but found signs inconclusive
                 if (DAT_GameCore::ptr->missionNumber1to20 - 1U <= 19) {
                     // This makes the soundID "missionNumber1to20 + 46", which would use the enums 47 to 66.
                     // These are only filled by 4 enums for 4 mission intros, so this space could be made for 20.
-                    soundIDTemp = DAT_GameCore::ptr->missionNumber1to20 + 46;
+                    soundIDInt = DAT_GameCore::ptr->missionNumber1to20 + 46;
                 } else {
-                    soundIDTemp = DE::SHCDE::MUSIC_TUNE_OFF;
+                    soundIDInt = DE::SHCDE::MUSIC_TUNE_OFF;
                 }
             }
 
-            if (soundIDTemp == this->currentSoundID_0x3278) {
+            if (soundIDInt == this->currentSoundID_0x3278) {
                 return;
             }
-            this->currentSoundID_0x3278 = soundIDTemp;
+            this->currentSoundID_0x3278 = soundIDInt;
             int const fileVolume = MACRO_CALL_MEMBER(SFX::SFXState_Func::getSoundVolumeForFilename, DAT_SFXState::ptr)(
-                DAT_SFXDefinedData::ptr->DAT_SFX_Pointers[soundIDTemp].musicFile);
+                DAT_SFXDefinedData::ptr->DAT_SFX_Pointers[soundIDInt].musicFile);
             // 0x7f is the maximum volume of the MSS32 volume API.
             this->currentSoundIDVolumeUnk_0x327c = (fileVolume * 100) / 0x7f;
             this->mbr_0x3280 = 0;
