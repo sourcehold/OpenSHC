bec5868acc863bc623be415723e41ec48b52a7dc
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-06-27 00:29:36 +0200

    reimplement: SHC_3BB0A8C1_0x0047B060 still register issues

--- src/OpenSHC/Audio/mss/SoundSystem/playSoundStreamUnk.cpp
+++ src/OpenSHC/Audio/mss/SoundSystem/playSoundStreamUnk.cpp
@@ -1,6 +1,7 @@
 #include "../SoundSystem.func.hpp"
 
 #include "OpenSHC/OS.func.hpp"
+#include "OpenSHC/Audio/mss/SoundFlagsAndLoopCountBitwiseFlagEnum.hpp"
 
 #include "OpenSHC/Globals/DAT_WindowAndDirectDraw.hpp"
 
@@ -10,15 +11,14 @@ namespace Audio {
 
         // FUNCTION: STRONGHOLDCRUSADER 0x0047B060
         void SoundSystem::playSoundStreamUnk(
-            SHC_SoundStream sndStreamIndex, char* filename, UnkSoundFlagsAndLoopCount flagsAndLoopCount)
+            SHC_SoundStream sndStreamIndex, char* filename, SoundFlagsAndLoopCount flagsAndLoopCount)
         {
             if (!this->waveOutOpenUnk_0x8) {
                 return;
             }
 
-            UnkSoundFlagsAndLoopCount const unkSoundFlagsAndLoopCount = { 1, 0, 0, 0, 0 };
-            if (flagsAndLoopCount.unknownFlag2) {
-                this->streamFlagsUnkAndLoopCount_0x34[sndStreamIndex] = unkSoundFlagsAndLoopCount;
+            if (flagsAndLoopCount & FLAG_SOUND_UNKNOWN_FLAG_2) {
+                this->streamFlagsUnkAndLoopCount_0x34[sndStreamIndex] = 1;
             }
             MACRO_CALL_MEMBER(SoundSystem_Func::endSoundStream, this)(sndStreamIndex);
 
@@ -32,18 +32,19 @@ namespace Audio {
                 AIL_set_sample_type(this->musicSampleUnk_0x170, DIG_F_STEREO_16, 0);
                 AIL_set_sample_playback_rate(this->musicSampleUnk_0x170, 22050);
                 AIL_set_sample_pan(this->musicSampleUnk_0x170, 64);
-                AIL_set_sample_volume(
-                    this->musicSampleUnk_0x170, (this->streamVolume[0] * this->streamFileVolumeNextUnk_0x48[0]) / 100);
-                this->streamFileVolumeCurrentUnk_0x5c[0] = this->streamFileVolumeNextUnk_0x48[0];
+                AIL_set_sample_volume(this->musicSampleUnk_0x170,
+                    (this->streamVolume[sndStreamIndex] * this->streamFileVolumeNextUnk_0x48[sndStreamIndex]) / 100);
+                this->streamFileVolumeCurrentUnk_0x5c[sndStreamIndex]
+                    = this->streamFileVolumeNextUnk_0x48[sndStreamIndex];
                 this->musicSampleFileHandleUnk_0x174 = MACRO_CALL(OS_Func::_ucrt_open)(filename, _O_BINARY, 0);
-                this->streamFlagsUnkAndLoopCount_0x34[0] = flagsAndLoopCount;
+                this->streamFlagsUnkAndLoopCount_0x34[sndStreamIndex] = flagsAndLoopCount;
                 this->mbr_0x188 = 0;
                 if (this->musicSampleFileHandleUnk_0x174 == -1) {
                     return;
                 }
                 MACRO_CALL_MEMBER(SoundSystem_Func::playMusicUnk, this)();
                 MACRO_CALL_MEMBER(SoundSystem_Func::playMusicUnk, this)();
-                this->streamActiveUnk_0x20[0] = true;
+                this->streamActiveUnk_0x20[sndStreamIndex] = true;
                 return;
             }
 
@@ -52,20 +53,21 @@ namespace Audio {
                 return;
             }
 
-            this->streamActiveUnk_0x20[sndStreamIndex] = 1;
+            this->streamActiveUnk_0x20[sndStreamIndex] = true;
             this->streamFlagsUnkAndLoopCount_0x34[sndStreamIndex] = flagsAndLoopCount;
-            if (flagsAndLoopCount.unknownFlag1 && (this->streamActiveUnk_0x20[0] != 0)) {
+            if ((flagsAndLoopCount & FLAG_SOUND_UNKNOWN_FLAG_1) && this->streamActiveUnk_0x20[0]) {
                 AIL_set_sample_volume(this->musicSampleUnk_0x170,
-                    ((this->streamFileVolumeNextUnk_0x48[0] / 2) * this->streamVolume[0]) / 100);
+                    this->streamVolume[0] * (this->streamFileVolumeNextUnk_0x48[0] / 2) / 100);
                 this->streamFileVolumeCurrentUnk_0x5c[0] = this->streamFileVolumeNextUnk_0x48[0] / 2;
             }
-            if (flagsAndLoopCount.unknownFlag2 || flagsAndLoopCount.uninterruptable) {
-                flagsAndLoopCount = unkSoundFlagsAndLoopCount;
+            if (flagsAndLoopCount & (FLAG_SOUND_UNKNOWN_FLAG_2 | FLAG_SOUND_UNINTERRUPTABLE)) {
+                flagsAndLoopCount = 1;
             }
             AIL_set_stream_volume(this->stream_0xc[sndStreamIndex],
                 (this->streamVolume[sndStreamIndex] * this->streamFileVolumeNextUnk_0x48[sndStreamIndex]) / 100);
             this->streamFileVolumeCurrentUnk_0x5c[sndStreamIndex] = this->streamFileVolumeNextUnk_0x48[sndStreamIndex];
-            AIL_set_stream_loop_count(this->stream_0xc[sndStreamIndex], flagsAndLoopCount.loopCount);
+            AIL_set_stream_loop_count(
+                this->stream_0xc[sndStreamIndex], flagsAndLoopCount & FLAG_SOUND_LOOP_COUNT_FIELD);
             AIL_start_stream(this->stream_0xc[sndStreamIndex]);
         }
 
