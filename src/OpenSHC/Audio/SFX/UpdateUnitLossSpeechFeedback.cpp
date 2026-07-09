574eaa24403b55466e8780496324ffc54e685101
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-07-09 21:34:26 +0200

    reimplement: SHC_3BB0A8C1_0x0044B840 tried to reduce code

--- src/OpenSHC/Audio/SFX/UpdateUnitLossSpeechFeedback.cpp
+++ src/OpenSHC/Audio/SFX/UpdateUnitLossSpeechFeedback.cpp
@@ -51,10 +51,10 @@ namespace Audio {
             }
             ++DAT_GL_Counter::instance;
             for (int _offset = 8; _offset >= 0; --_offset) {
-                DAT_TotalUnitValueLossesEnemyHistory::instance[_offset + 1]
-                    = DAT_TotalUnitValueLossesEnemyHistory::instance[_offset];
                 DAT_CurrentPlayerUnitLossesHistory::instance[_offset + 1]
                     = DAT_CurrentPlayerUnitLossesHistory::instance[_offset];
+                DAT_TotalUnitValueLossesEnemyHistory::instance[_offset + 1]
+                    = DAT_TotalUnitValueLossesEnemyHistory::instance[_offset];
                 for (int i = 0; i < 9; ++i) {
                     Dat_UnitValueLossesHistoryPerEnemyPlayer::instance[i][_offset + 1]
                         = Dat_UnitValueLossesHistoryPerEnemyPlayer::instance[i][_offset];
@@ -67,114 +67,106 @@ namespace Audio {
                     = DAT_UnitValueLossesPerEnemyPlayer::instance[i];
                 DAT_TotalUnitValueLossesEnemyHistory::instance[0] += DAT_UnitValueLossesPerEnemyPlayer::instance[i];
             }
-            if (!(((DAT_GL_Counter::instance < 2)
-                      || (49
-                          < DAT_TotalUnitValueLossesEnemyHistory::instance[0] + DAT_CurrentPlayerUnitLosses::instance))
-                    || (DAT_TotalUnitValueLossesEnemyHistory::instance[2]
-                            + DAT_CurrentPlayerUnitLossesHistory::instance[2]
-                            + DAT_TotalUnitValueLossesEnemyHistory::instance[1]
-                            + DAT_CurrentPlayerUnitLossesHistory::instance[1]
-                        < 151))) {
-                _relative1 = MACRO_CALL(Game_Func::RelativeValueForGenie)(
-                    DAT_CurrentPlayerUnitLossesHistory::instance[1] + DAT_CurrentPlayerUnitLossesHistory::instance[2],
-                    DAT_TotalUnitValueLossesEnemyHistory::instance[2]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[1]);
-                _relative2 = MACRO_CALL(Game_Func::RelativeValueForGenie)(
-                    DAT_CurrentPlayerUnitLossesHistory::instance[3] + DAT_CurrentPlayerUnitLossesHistory::instance[4]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[5]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[6]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[7]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[8]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[9]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[2]
-                        + DAT_CurrentPlayerUnitLossesHistory::instance[1],
-                    DAT_TotalUnitValueLossesEnemyHistory::instance[5]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[4]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[6]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[7]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[8]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[9]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[3]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[2]
-                        + DAT_TotalUnitValueLossesEnemyHistory::instance[1]);
-                if (_relative1 == 0) {
-                    if (_relative2 == 0) {
-                        if (DAT_GameCore::instance.genieVoiceActive != FALSE) {
-                            /* "A Valliant Effort" */
-                            MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(s_Genie_29_wav_005a4ecc);
-                        }
-                        DAT_GL_Counter::instance = 0;
+            if (DAT_GL_Counter::instance >= 2
+                && 50 > DAT_TotalUnitValueLossesEnemyHistory::instance[0] + DAT_CurrentPlayerUnitLosses::instance) {
+                int playerUnitLosses1to2
+                    = DAT_CurrentPlayerUnitLossesHistory::instance[1] + DAT_CurrentPlayerUnitLossesHistory::instance[2];
+                int totalEnemyUnitLosses1to2 = DAT_TotalUnitValueLossesEnemyHistory::instance[1]
+                    + DAT_TotalUnitValueLossesEnemyHistory::instance[2];
+                if (playerUnitLosses1to2 + totalEnemyUnitLosses1to2 > 150) {
+                    _relative1
+                        = MACRO_CALL(Game_Func::RelativeValueForGenie)(playerUnitLosses1to2, totalEnemyUnitLosses1to2);
+                    int playerUnitLosses1to9 = 0;
+                    for (int i = 1; i < 10; ++i) {
+                        playerUnitLosses1to9 += DAT_CurrentPlayerUnitLossesHistory::instance[i];
                     }
-                } else {
-                    if ((0 < _relative1) && (0 < _relative2)) {
-                        _relative1 = (_relative2 + _relative1) / 2;
-                        if (DAT_GameCore::instance.genieVoiceActive) {
-                            if (_relative1 == 1) {
-                                /* "Majestic" */
+                    int totalEnemyUnitLosses1to9 = 0;
+                    for (int i = 1; i < 10; ++i) {
+                        totalEnemyUnitLosses1to9 += DAT_TotalUnitValueLossesEnemyHistory::instance[i];
+                    }
+                    _relative2
+                        = MACRO_CALL(Game_Func::RelativeValueForGenie)(playerUnitLosses1to9, totalEnemyUnitLosses1to9);
+                    if (_relative1 != 0 || _relative2 == 0) {
+                        if (_relative1 == 0 && _relative2 == 0) {
+                            if (DAT_GameCore::instance.genieVoiceActive != FALSE) {
+                                /* "A Valliant Effort" */
                                 MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_30_wav_005a4ebc);
-                            } else if (_relative1 == 2) {
-                                /* "Excellent" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_31_wav_005a4eac);
-                            } else if (_relative1 == 3) {
-                                /* "Impressive" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_32_wav_005a4e9c);
-                            } else if (_relative1 == 4) {
-                                /* "Glorious" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_34_wav_005a4e8c);
-                            } else if (_relative1 == 5) {
-                                /* "For the crown" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_37_wav_005a4e7c);
-                            } else if (_relative1 == 6) {
-                                /* "Heroic" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_38_wav_005a4e6c);
-                            } else if (_relative1 >= 7) {
-                                /* "It's a Rout" */
-                                MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                    s_Genie_39_wav_005a4e5c);
+                                    s_Genie_29_wav_005a4ecc);
                             }
-                        }
-                        DAT_GL_Counter::instance = 0;
-                    }
+                        } else {
+                            if (0 < _relative1 && 0 < _relative2) {
+                                _relative1 = (_relative2 + _relative1) / 2;
+                                if (DAT_GameCore::instance.genieVoiceActive) {
+                                    if (_relative1 == 1) {
+                                        /* "Majestic" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_30_wav_005a4ebc);
+                                    } else if (_relative1 == 2) {
+                                        /* "Excellent" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_31_wav_005a4eac);
+                                    } else if (_relative1 == 3) {
+                                        /* "Impressive" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_32_wav_005a4e9c);
+                                    } else if (_relative1 == 4) {
+                                        /* "Glorious" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_34_wav_005a4e8c);
+                                    } else if (_relative1 == 5) {
+                                        /* "For the crown" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_37_wav_005a4e7c);
+                                    } else if (_relative1 == 6) {
+                                        /* "Heroic" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_38_wav_005a4e6c);
+                                    } else if (_relative1 >= 7) {
+                                        /* "It's a Rout" */
+                                        MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                            s_Genie_39_wav_005a4e5c);
+                                    }
+                                }
+                                DAT_GL_Counter::instance = 0;
+                            }
 
-                    if (!((-1 < _relative1) || (-1 < _relative2))) {
-                        iVar3 = (_relative2 + _relative1) / 2;
-                        if (DAT_GameCore::instance.genieVoiceActive && iVar3 == -1) {
-                            /* "Vanquished" */
-                            MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(s_Genie_42_wav_005a4e4c);
-                        } else if (DAT_GameCore::instance.genieVoiceActive && iVar3 == -2) {
-                            /* "Crest falling" */
-                            MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(s_Genie_43_wav_005a4e3c);
-                        } else if (!DAT_GameCore::instance.genieVoiceActive || iVar3 != -2) {
-                            if (iVar3 <= -3) {
-                                if (DAT_GameCore::instance.genieVoiceActive != FALSE) {
-                                    /* "Humiliated" */
+                            if (-1 >= _relative1 && -1 >= _relative2) {
+                                iVar3 = (_relative2 + _relative1) / 2;
+                                if (DAT_GameCore::instance.genieVoiceActive && iVar3 == -1) {
+                                    /* "Vanquished" */
                                     MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
-                                        s_Genie_44_wav_005a4e2c);
-                                }
-                                iVar7 = -1;
-                                iVar6 = 0;
-                                for (int i = 1; i < 10; ++i) {
-                                    iVar4 = 0;
-                                    for (int j = 1; j < 10; ++j) {
-                                        iVar4 += Dat_UnitValueLossesHistoryPerEnemyPlayer::instance[i][j];
+                                        s_Genie_42_wav_005a4e4c);
+                                } else if (DAT_GameCore::instance.genieVoiceActive && iVar3 == -2) {
+                                    /* "Crest falling" */
+                                    MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                        s_Genie_43_wav_005a4e3c);
+                                } else if (!DAT_GameCore::instance.genieVoiceActive || iVar3 != -2) {
+                                    if (iVar3 <= -3) {
+                                        if (DAT_GameCore::instance.genieVoiceActive != FALSE) {
+                                            /* "Humiliated" */
+                                            MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(
+                                                s_Genie_44_wav_005a4e2c);
+                                        }
+                                        iVar7 = -1;
+                                        iVar6 = 0;
+                                        for (int i = 1; i < 9; ++i) {
+                                            iVar4 = 0;
+                                            for (int j = 1; j < 10; ++j) {
+                                                iVar4 += Dat_UnitValueLossesHistoryPerEnemyPlayer::instance[i][j];
+                                            }
+                                            if (iVar4 > iVar6) {
+                                                iVar6 = iVar4;
+                                                iVar7 = i;
+                                            }
+                                        }
+                                        if ((iVar7 != -1)
+                                            && (DAT_GameState::instance.mapAndTime.playerTeams[iVar7]
+                                                != DAT_GameState::instance.mapAndTime.playerTeams
+                                                    [DAT_GameSynchronyState::instance.currentPlayerSlotID])) {
+                                            MACRO_CALL_MEMBER(AI::AICState_Func::playFriendlyVictoryBikFromPlayer,
+                                                DAT_AICState::ptr)(iVar7);
+                                        }
                                     }
-                                    if (iVar4 > iVar6) {
-                                        iVar6 = iVar4;
-                                        iVar7 = i;
-                                    }
-                                }
-                                if ((iVar7 != -1)
-                                    && (DAT_GameState::instance.mapAndTime.playerTeams[iVar7]
-                                        != DAT_GameState::instance.mapAndTime
-                                            .playerTeams[DAT_GameSynchronyState::instance.currentPlayerSlotID])) {
-                                    MACRO_CALL_MEMBER(
-                                        AI::AICState_Func::playFriendlyVictoryBikFromPlayer, DAT_AICState::ptr)(iVar7);
                                 }
                             }
                         }
@@ -183,20 +175,13 @@ namespace Audio {
                 }
             }
             DAT_CurrentPlayerUnitLosses::instance = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[0] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[1] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[2] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[3] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[4] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[5] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[6] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[7] = 0;
-            DAT_UnitValueLossesPerEnemyPlayer::instance[8] = 0;
-            if ((1 < DAT_DestroyedBuildingsCount::instance)
-                && (5 < DAT_DestroyedBuildingsCountHistory::instance[0]
-                        + DAT_DestroyedBuildingsCountHistory::instance[1]
+            for (int i = 0; i < 9; ++i) {
+                DAT_UnitValueLossesPerEnemyPlayer::instance[i] = 0;
+            }
+            if (1 < DAT_DestroyedBuildingsCount::instance
+                && 5 < DAT_DestroyedBuildingsCountHistory::instance[0] + DAT_DestroyedBuildingsCountHistory::instance[1]
                         + DAT_DestroyedBuildingsCountHistory::instance[2]
-                        + DAT_DestroyedBuildingsCountHistory::instance[3] + DAT_DestroyedBuildingsCount::instance)) {
+                        + DAT_DestroyedBuildingsCountHistory::instance[3] + DAT_DestroyedBuildingsCount::instance) {
                 if (DAT_GameCore::instance.genieVoiceActive != FALSE) {
                     /* "Where is your Castle" */
                     MACRO_CALL_MEMBER(SFXState_Func::playWAVSFX, DAT_SFXState::ptr)(s_Genie_41_wav_005a4e1c);
@@ -213,7 +198,6 @@ namespace Audio {
             DAT_DestroyedBuildingsCount::instance = 0;
             return;
         }
-
     }
 }
 }
