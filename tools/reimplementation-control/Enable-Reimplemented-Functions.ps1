89c1bec8c96840f5754f59cf2d01e82a59405243
sourcehold <sourcehold@users.noreply.github.com>
2026-07-30 01:13:42 +0200

    [TOOLS] add tool

--- tools/reimplementation-control/Enable-Reimplemented-Functions.ps1
+++ tools/reimplementation-control/Enable-Reimplemented-Functions.ps1
@@ -1,9 +1,9 @@
 function Enable-Reimplemented-Functions {
     param(
-        [string]$AddressFilter = "",
         [string]$CppFilesFilter = "",
         [bool]$CppFilesFilterRecurse = $false,
-        [bool]$Enable = $true
+        [bool]$Enable = $true,
+        [string]$AddressFilter = ""
     )
     if ("" -eq $CppFilesFilter) {
         $CppFilesFilter = "src\OpenSHC\*.cpp"
@@ -17,13 +17,20 @@ function Enable-Reimplemented-Functions {
     if ($CppFilesFilterRecurse) {
         $cppFiles = Get-ChildItem $CppFilesFilter -Recurse
     }
+    $target = "$(-not $Enable)".ToLower()
+    $replacement = "$($Enable)".ToLower()
     $cppFiles | Select-String -Pattern "//\s*FUNCTION:\s* STRONGHOLDCRUSADER \s*$AddressFilter" | ForEach-Object {
         $address = $_.Matches[0].Groups[1].Value
         $funcFiles |
             Where-Object { $null -ne (Select-String -Path $_ -Pattern "$address") } |
             ForEach-Object {
-                (Get-Content -Path $_ -Raw) -replace "(?s)(MACRO_FUNCTION_RESOLVER\b(?:(?!MACRO_FUNCTION_RESOLVER).)*?),\s*$(-not $Enable)(\s*, Address::SHC_3BB0A8C1_$address)", "`$1, $($Enable)`$2" |
+                (Get-Content -Path $_ -Raw) -replace
+                    "(?s)(MACRO_FUNCTION_RESOLVER\b(?:(?!MACRO_FUNCTION_RESOLVER).)*?),\s*$target(\s*, Address::SHC_3BB0A8C1_$address)", "`$1, $replacement`$2" |
                     Set-Content -Path $_
             }
     }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+    & Enable-Reimplemented-Functions
 }
