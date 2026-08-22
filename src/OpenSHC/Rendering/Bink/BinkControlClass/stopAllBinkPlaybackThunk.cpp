10c442d257b44e35a5097d00443bc66e6f3790c6
sourcehold <sourcehold@users.noreply.github.com>
2026-08-23 01:21:50 +0200

    reimplement: SHC_3BB0A8C1_0x004092B0 100%

---
+++ src/OpenSHC/Rendering/Bink/BinkControlClass/stopAllBinkPlaybackThunk.cpp
@@ -0,0 +1,15 @@
+#include "OpenSHC/Rendering/Bink/BinkControlClass.func.hpp"
+
+namespace OpenSHC {
+namespace Rendering {
+    namespace Bink {
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x004092B0
+        void BinkControlClass::stopAllBinkPlaybackThunk()
+        {
+            MACRO_CALL_MEMBER(OpenSHC::Rendering::Bink::BinkControlClass_Func::stopAllBinkPlayback, this)();
+        }
+
+    }
+}
+}
