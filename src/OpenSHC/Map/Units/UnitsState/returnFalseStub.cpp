70587278decfe5d462992d251eaef689800951f6
sourcehold <sourcehold@users.noreply.github.com>
2026-08-23 01:49:09 +0200

    reimplement: SHC_3BB0A8C1_0x00533090 100%

---
+++ src/OpenSHC/Map/Units/UnitsState/returnFalseStub.cpp
@@ -0,0 +1,15 @@
+#include "OpenSHC/Map/Units/UnitsState.func.hpp"
+#include "OpenSHC/WindowsHelper/Enums/BOOLEnum.hpp"
+
+namespace OpenSHC {
+namespace Map {
+    namespace Units {
+
+        using OpenSHC::WindowsHelper::Enums::BOOLEnum;
+
+        // FUNCTION: STRONGHOLDCRUSADER 0x00533090
+        BOOLEnum UnitsState::returnFalseStub() { return FALSE; }
+
+    }
+}
+}
