6d74397005e9640981e5c2b3467a31e12b9f6055
sourcehold <sourcehold@users.noreply.github.com>
2026-08-23 01:57:22 +0200

    reimplement: SHC_3BB0A8C1_0x00480570 100%

---
+++ src/OpenSHC/Synchrony/GameSynchronyState/getGameCommandArrayIndex.cpp
@@ -0,0 +1,10 @@
+#include "OpenSHC/Synchrony/GameSynchronyState.func.hpp"
+
+namespace OpenSHC {
+namespace Synchrony {
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x00480570
+    undefined4 GameSynchronyState::getGameCommandArrayIndex() { return this->DAT_GameCommandArrayIndex; }
+
+}
+}
