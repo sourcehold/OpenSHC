50395a31aba97abf6edc89b5a4f7459449b6dadb
sourcehold <sourcehold@users.noreply.github.com>
2026-08-22 18:46:33 +0200

    [TOOLS]: add git tracked cpp files to cmake file list

--- tools/reimplementation-control/Add-All-Reimplementations-To-Cmake.ps1
+++ tools/reimplementation-control/Add-All-Reimplementations-To-Cmake.ps1
@@ -1,10 +1,18 @@
-function Add-All-Reimplementations-To-Cmake {
+function Add-All-Cpp-To-Cmake {
     param(
-        [string]$Destination = "cmake\openshc-sources.txt"
+        [string]$Destination = "cmake\openshc-sources.txt.local"
     )
     Get-ChildItem .\src\OpenSHC\*.cpp -Recurse |
         Resolve-Path -Relative |
         ForEach-Object { "$_".Substring(2).Replace("\", "/") } |
         Join-String -Separator "`n" |
         Set-Content -Path "$Destination"
+}
+
+function Add-All-Reimplementations-To-Cmake {
+	git ls-tree --full-tree --name-only -r main | Select-String -Pattern "src/OpenSHC/.*[.]cpp" > .\cmake\openshc-sources.txt.local
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+	 Add-All-Reimplementations-To-Cmake
 }
