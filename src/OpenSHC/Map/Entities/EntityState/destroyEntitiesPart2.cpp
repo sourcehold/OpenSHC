8455eecd9038f9a4b834d794201fe12a2aa55c3f
sourcehold <sourcehold@users.noreply.github.com>
2026-08-25 14:37:25 +0200

    reimplement: SHC_3BB0A8C1_0x00401B60 100%

--- src/OpenSHC/Map/Entities/EntityState/destroyEntitiesPart2.cpp
+++ src/OpenSHC/Map/Entities/EntityState/destroyEntitiesPart2.cpp
@@ -1,42 +1,19 @@
-#include "OpenSHC/Map/Entities/EntityState.func.hpp"
-#include "OpenSHC/Map/Entities/Entity.hpp"
-
-
-
-
+#include "../EntityState.func.hpp"
 
 namespace OpenSHC {
 namespace Map {
-namespace Entities {
+    namespace Entities {
 
-using OpenSHC::Map::Entities::Entity;
+        // FUNCTION: STRONGHOLDCRUSADER 0x00401B60
+        void EntityState::destroyEntitiesPart2(int param_1)
+        {
+            for (int iVar1 = 1; iVar1 < 3000; iVar1++) {
+                if ((this->entityArray[iVar1].logicalState == 2) && (this->entityArray[iVar1].owner == param_1)) {
+                    this->entityArray[iVar1].logicalState = 3;
+                }
+            }
+        }
 
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:56:35.138000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x00401B60
-void EntityState::destroyEntitiesPart2(int param_1)
-
-{
-Entity * psVar1;
-int iVar1;
-
-psVar1 = &this->entityArray[1];
-iVar1 = 2999;
-do {
-if ((psVar1->logicalState == 2) && (psVar1->owner == param_1)) {
-psVar1->logicalState = 3;
-}
-psVar1 = psVar1 + 0x74;
-iVar1 = iVar1 + -1;
-} while (iVar1 != 0);
-return;
-}
-
-
-}
+    }
 }
 }
