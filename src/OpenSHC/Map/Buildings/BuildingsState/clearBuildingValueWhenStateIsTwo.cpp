2f974739a96b378e9673e99ddbb1b4927a28c687
sourcehold <sourcehold@users.noreply.github.com>
2026-08-25 15:08:49 +0200

    reimplement: SHC_3BB0A8C1_0x0040F3F0 100%

--- src/OpenSHC/Map/Buildings/BuildingsState/clearBuildingValueWhenStateIsTwo.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/clearBuildingValueWhenStateIsTwo.cpp
@@ -1,46 +1,19 @@
-#include "OpenSHC/Map/Buildings/BuildingsState.func.hpp"
-#include "OpenSHC/Map/Buildings/Building.hpp"
-#include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
-
-
-
-
+#include "../BuildingsState.func.hpp"
 
 namespace OpenSHC {
 namespace Map {
-namespace Buildings {
+    namespace Buildings {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
+        // FUNCTION: STRONGHOLDCRUSADER 0x0040F3F0
+        void BuildingsState::clearBuildingValueWhenStateIsTwo()
+        {
+            for (int iVar1 = 1; iVar1 < (int)this->maxBuildingsCount; iVar1++) {
+                if (this->buildings[iVar1].logicalState == Buildings::BLS_NORMAL) {
+                    this->buildings[iVar1].animStateCounterTracker = 0;
+                }
+            }
+        }
 
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0040F3F0
-void BuildingsState::clearBuildingValueWhenStateIsTwo()
-
-{
-Building * piVar1;
-int iVar1;
-
-iVar1 = 1;
-if (1 < this->maxBuildingsCount) {
-piVar1 = &this->buildings[1];
-do {
-if (piVar1->logicalState == OpenSHC::Map::Buildings::BLS_NORMAL) {
-piVar1->animStateCounterTracker = 0;
-}
-iVar1 = iVar1 + 1;
-piVar1 = piVar1 + 0xcb;
-} while (iVar1 < this->maxBuildingsCount);
-}
-return;
-}
-
-
-}
+    }
 }
 }
