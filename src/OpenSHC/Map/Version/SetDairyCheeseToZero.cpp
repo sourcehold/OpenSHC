43e59b4e9380e6a066cf7c57384c2e96ac81bee0
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 20:35:49 +0200

    reimplement: SHC_3BB0A8C1_0x0041A2B0 100%

--- src/OpenSHC/Map/Version/SetDairyCheeseToZero.cpp
+++ src/OpenSHC/Map/Version/SetDairyCheeseToZero.cpp
@@ -3,39 +3,30 @@
 #include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
 #include "OpenSHC/Map/Buildings/BuildingType.hpp"
 
-
-
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
-using OpenSHC::Map::Buildings::BuildingType;
+    using OpenSHC::Map::Buildings::Building;
+    using OpenSHC::Map::Buildings::BuildingLogicalState;
+    using OpenSHC::Map::Buildings::BuildingType;
 
+    /*
+      decompilerscript: committed: 2025-01-30 21:57:43.216000
+     */
 
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A2B0
-void Version::SetDairyCheeseToZero()
-
-{
-Building * pBVar1;
-
-pBVar1 = &DAT_BuildingsState::instance.buildings[1];
-do {
-if ((pBVar1->logicalState != ((BuildingLogicalState)0)) && (pBVar1->buildingType == OpenSHC::Map::Buildings::BT_DAIRYFARM)) {
-pBVar1->flagonsOfAleOrCheeseOrReleaseDogs = 0;
-}
-pBVar1 = pBVar1 + 0x196;
-} while ((int)pBVar1 < 0x1124dc6);
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A2B0
+    void Version::SetDairyCheeseToZero()
+    {
+        for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
+            if ((DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0))
+                && (DAT_BuildingsState::instance.buildings[_buildingID].buildingType
+                    == OpenSHC::Map::Buildings::BT_DAIRYFARM)) {
+                DAT_BuildingsState::instance.buildings[_buildingID].flagonsOfAleOrCheeseOrReleaseDogs = 0;
+            }
+        }
+    }
 
 }
 }
