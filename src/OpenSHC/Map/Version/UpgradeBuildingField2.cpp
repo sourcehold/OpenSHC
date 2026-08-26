0a46334c820345310813c27318be556babbc5897
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 20:40:30 +0200

    reimplement: SHC_3BB0A8C1_0x0041A3C0 100%

--- src/OpenSHC/Map/Version/UpgradeBuildingField2.cpp
+++ src/OpenSHC/Map/Version/UpgradeBuildingField2.cpp
@@ -2,39 +2,24 @@
 #include "OpenSHC/Map/Buildings/Building.hpp"
 #include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
 
-
-
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
+    using OpenSHC::Map::Buildings::Building;
+    using OpenSHC::Map::Buildings::BuildingLogicalState;
 
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A3C0
-void Version::UpgradeBuildingField2()
-
-{
-Building * psVar1;
-
-psVar1 = &DAT_BuildingsState::instance.buildings[1];
-do {
-if (psVar1->logicalState != ((BuildingLogicalState)0)) {
-psVar1->field207_0x288 = 0;
-psVar1->field189_0x264 = 0;
-}
-psVar1 = psVar1 + 0x196;
-} while ((int)psVar1 < 0x1124f7c);
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A3C0
+    void Version::UpgradeBuildingField2()
+    {
+        for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
+            if (DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0)) {
+                DAT_BuildingsState::instance.buildings[_buildingID].field207_0x288 = 0;
+                DAT_BuildingsState::instance.buildings[_buildingID].field189_0x264 = 0;
+            }
+        }
+    }
 
 }
 }
