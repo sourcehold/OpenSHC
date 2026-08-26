bee1a24187268152e35f58efb5d3c3f94968e504
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 20:43:19 +0200

    progress

--- src/OpenSHC/Map/Version/UpgradeSolitaryMapBuildingField3.cpp
+++ src/OpenSHC/Map/Version/UpgradeSolitaryMapBuildingField3.cpp
@@ -1,9 +1,7 @@
 #include "OpenSHC/Map/Version.func.hpp"
-#include "OpenSHC/Map/Buildings/Building.hpp"
 #include "OpenSHC/Game/GameMode.hpp"
+#include "OpenSHC/Map/Buildings/Building.hpp"
 #include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
-
-
 
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 #include "OpenSHC/Globals/DAT_GameSynchronyState.hpp"
@@ -11,42 +9,21 @@
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Game::GameMode;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
+    using OpenSHC::Game::GameMode;
+    using OpenSHC::Map::Buildings::Building;
+    using OpenSHC::Map::Buildings::BuildingLogicalState;
 
-
-/* 
-  WARNING: Enum "DPSEND_EnumInt": Some values do not have unique names
- */
-
-/* 
-  WARNING: Enum "DPERRInt": Some values do not have unique names
- */
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A3F0
-void Version::UpgradeSolitaryMapBuildingField3()
-
-{
-Building * piVar1;
-
-if (DAT_GameSynchronyState::instance.currentGameMode == OpenSHC::Game::GM_SOLITARY) {
-piVar1 = &DAT_BuildingsState::instance.buildings[1];
-do {
-if (piVar1->logicalState != ((BuildingLogicalState)0)) {
-piVar1->playerColorUnk = 0;
-}
-piVar1 = piVar1 + 0xcb;
-} while ((int)piVar1 < 0x1124cfc);
-}
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A3F0
+    void Version::UpgradeSolitaryMapBuildingField3()
+    {
+        if (DAT_GameSynchronyState::instance.currentGameMode == OpenSHC::Game::GM_SOLITARY) {
+            for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
+                if (DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0)) {
+                    DAT_BuildingsState::instance.buildings[_buildingID].playerColorUnk = 0;
+                }
+            }
+        }
+    }
 
 }
 }
