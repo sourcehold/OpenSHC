c890b1325a4d1f870effcdc12f5ca5845da0c5c0
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 23:09:52 +0200

    reimplement: SHC_3BB0A8C1_0x0041A3C0 72% stuck

--- src/OpenSHC/UI/MenuView_IntroLogos_DoInitial.cpp
+++ src/OpenSHC/UI/MenuView_IntroLogos_DoInitial.cpp
@@ -9,23 +9,15 @@ namespace OpenSHC {
 
 using OpenSHC::UI::Menu;
 
-/*
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
 // FUNCTION: STRONGHOLDCRUSADER 0x00424770
 void UI::MenuView_IntroLogos_DoInitial()
-
 {
-    Menu* _menuPtr;
-
     DAT_MenuHandlerState::instance.y = DAT_WindowAndDirectDraw::instance.mainMenuBorderHeight;
     DAT_MenuHandlerState::instance.x = DAT_WindowAndDirectDraw::instance.mainMenuBorderWidth;
-    _menuPtr = DAT_MenuHandlerState::instance.currentMenu;
-    (DAT_MenuHandlerState::instance.currentMenu)->xPosition = DAT_WindowAndDirectDraw::instance.mainMenuBorderWidth;
-    _menuPtr->yPosition = DAT_MenuHandlerState::instance.y;
+    DAT_MenuHandlerState::instance.currentMenu->yPosition = DAT_MenuHandlerState::instance.y;
+    DAT_MenuHandlerState::instance.currentMenu->xPosition = DAT_MenuHandlerState::instance.x;
+
     DAT_IntroTimestamp::instance = timeGetTime();
-    return;
 }
 
 }
