a1bac7ccb7050a8c843c262a29cbebc9b8ee9632
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 19:43:47 +0200

    reimplement: SHC_3BB0A8C1_0x0041A060 100%

--- src/OpenSHC/Map/Version/SetUndamagedBuildingHealthToValue.cpp
+++ src/OpenSHC/Map/Version/SetUndamagedBuildingHealthToValue.cpp
@@ -2,43 +2,30 @@
 #include "OpenSHC/Map/Buildings/Building.hpp"
 #include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
 
-
-
-#include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 #include "OpenSHC/Globals/DAT_BuildingDefinedData.hpp"
+#include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
+    using OpenSHC::Map::Buildings::Building;
+    using OpenSHC::Map::Buildings::BuildingLogicalState;
 
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A060
-void Version::SetUndamagedBuildingHealthToValue()
-
-{
-short sVar1;
-Building * psVar2;
-
-psVar2 = &DAT_BuildingsState::instance.buildings[1];
-do {
-if ((psVar2->logicalState != ((BuildingLogicalState)0)) && (psVar2->currentHealth == psVar2->maxHealth))
-{
-sVar1 = DAT_BuildingDefinedData::instance.BuildingHP[(short)psVar2->buildingType].shortValue;
-psVar2->maxHealth = sVar1;
-psVar2->currentHealth = sVar1;
-}
-psVar2 = psVar2 + 0x196;
-} while ((int)psVar2 < 0x1124e02);
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A060
+    void Version::SetUndamagedBuildingHealthToValue()
+    {
+        for (int _buildingID = 1; _buildingID < 2000; _buildingID += 1) {
+            if ((DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0))
+                && (DAT_BuildingsState::instance.buildings[_buildingID].currentHealth
+                    == DAT_BuildingsState::instance.buildings[_buildingID].maxHealth)) {
+                uint health = DAT_BuildingDefinedData::instance
+                                  .BuildingHP[(short)DAT_BuildingsState::instance.buildings[_buildingID].buildingType]
+                                  .ushortValue;
+                DAT_BuildingsState::instance.buildings[_buildingID].maxHealth = health;
+                DAT_BuildingsState::instance.buildings[_buildingID].currentHealth = health;
+            }
+        }
+    }
 
 }
 }
