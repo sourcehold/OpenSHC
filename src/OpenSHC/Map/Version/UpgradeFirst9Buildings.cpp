61e5ba17d39393128dfa178445bab999278bf406
sourcehold <sourcehold@users.noreply.github.com>
2026-08-26 20:43:19 +0200

    progress

--- src/OpenSHC/Map/Version/UpgradeFirst9Buildings.cpp
+++ src/OpenSHC/Map/Version/UpgradeFirst9Buildings.cpp
@@ -1,37 +1,21 @@
-#include "OpenSHC/Map/Version.func.hpp"
 #include "OpenSHC/Map/Buildings/BuildingsState.func.hpp"
-
-
+#include "OpenSHC/Map/Version.func.hpp"
 
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-
-
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A0A0
-void Version::UpgradeFirst9Buildings()
-
-{
-int buildingID;
-
-buildingID = 1;
-do {
-MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupBarracksCampgroundPositions, DAT_BuildingsState::ptr)(buildingID);
-MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupMercenaryPostCampgroundPositions, DAT_BuildingsState::ptr)(buildingID)
-;
-buildingID = buildingID + 1;
-} while (buildingID < 9);
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A0A0
+    void Version::UpgradeFirst9Buildings()
+    {
+        for (int buildingID = 1; buildingID < 9; buildingID++) {
+            MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupBarracksCampgroundPositions,
+                DAT_BuildingsState::ptr)(buildingID);
+            MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupMercenaryPostCampgroundPositions,
+                DAT_BuildingsState::ptr)(buildingID);
+        }
+    }
 
 }
 }
--- src/OpenSHC/Map/Version/UpgradeKillingPitField.cpp
+++ src/OpenSHC/Map/Version/UpgradeKillingPitField.cpp
@@ -3,39 +3,26 @@
 #include "OpenSHC/Map/Buildings/BuildingLogicalState.hpp"
 #include "OpenSHC/Map/Buildings/BuildingType.hpp"
 
-
-
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Map::Buildings::Building;
-using OpenSHC::Map::Buildings::BuildingLogicalState;
-using OpenSHC::Map::Buildings::BuildingType;
+    using OpenSHC::Map::Buildings::Building;
+    using OpenSHC::Map::Buildings::BuildingLogicalState;
+    using OpenSHC::Map::Buildings::BuildingType;
 
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0041A5C0
-void Version::UpgradeKillingPitField()
-
-{
-Building * pBVar1;
-
-pBVar1 = &DAT_BuildingsState::instance.buildings[1];
-do {
-if ((pBVar1->logicalState != ((BuildingLogicalState)0)) && (pBVar1->buildingType == OpenSHC::Map::Buildings::BT_KILLINGPIT)) {
-pBVar1->killingPitField = 0;
-}
-pBVar1 = pBVar1 + 0x196;
-} while ((int)pBVar1 < 0x1124dc6);
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0041A5C0
+    void Version::UpgradeKillingPitField()
+    {
+        for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
+            if ((DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0))
+                && (DAT_BuildingsState::instance.buildings[_buildingID].buildingType
+                    == OpenSHC::Map::Buildings::BT_KILLINGPIT)) {
+                DAT_BuildingsState::instance.buildings[_buildingID].killingPitField = 0;
+            }
+        }
+    }
 
 }
 }
