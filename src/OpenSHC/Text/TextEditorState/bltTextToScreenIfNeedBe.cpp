9505221bd4a8d1cadd93903eec49473052973a4f
sourcehold <sourcehold@users.noreply.github.com>
2026-08-27 23:10:44 +0200

    reimplement: SHC_3BB0A8C1_0x0045D690 60%

--- src/OpenSHC/Text/TextEditorState/bltTextToScreenIfNeedBe.cpp
+++ src/OpenSHC/Text/TextEditorState/bltTextToScreenIfNeedBe.cpp
@@ -1,36 +1,25 @@
 #include "OpenSHC/Text/TextEditorState.func.hpp"
 #include "OpenSHC/UI/Rendering/WindowAndDirectDraw.func.hpp"
 
-
-
 #include "OpenSHC/Globals/DAT_WindowAndDirectDraw.hpp"
 
 namespace OpenSHC {
 namespace Text {
 
-
-
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0045D690
-void TextEditorState::bltTextToScreenIfNeedBe()
-
-{
-if (this->intArray1[0x14] != 0) {
-if (((this->savedMenuViewType == 0xc) ||
-(this->savedMenuViewType == 0xe)) ||
-(this->savedMenuViewType == 0x10)) {
-MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::WindowAndDirectDraw_Func::bltMapGameSurfaceToScreenMenuSurfaceComplete, DAT_WindowAndDirectDraw::ptr)();
-}
-this->intArray1[0x14] = 0;
-}
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x0045D690
+    void TextEditorState::bltTextToScreenIfNeedBe()
+    {
+        // fixme: wrong register usage, make part of 0x004403d0 ?
+        if (this->intArray1[20] != 0) {
+            if (((this->savedMenuViewType == 0xc) || (this->savedMenuViewType == 0xe))
+                || (this->savedMenuViewType == 0x10)) {
+                MACRO_CALL_MEMBER(
+                    OpenSHC::UI::Rendering::WindowAndDirectDraw_Func::bltMapGameSurfaceToScreenMenuSurfaceComplete,
+                    DAT_WindowAndDirectDraw::ptr)();
+            }
+            this->intArray1[20] = 0;
+        }
+    }
 
 }
 }
