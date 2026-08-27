1f1e7d2bf5339f925d70c9e5c643d0765ba95891
sourcehold <sourcehold@users.noreply.github.com>
2026-08-27 18:06:48 +0200

    reimplement: SHC_3BB0A8C1_0x004549C0 100% different register allocation

--- src/OpenSHC/UI/Rendering/TextureRenderCore/checkIfGfxTgxStartsWithTransparentPixels.cpp
+++ src/OpenSHC/UI/Rendering/TextureRenderCore/checkIfGfxTgxStartsWithTransparentPixels.cpp
@@ -1,10 +1,3 @@
-// ============================================================================
-// Program   : Stronghold Crusader.exe
-// Namespace : _HoldStrong::UI::Rendering::TextureRenderCore
-// Function  : checkIfGfxTgxStartsWithTransparentPixels
-// Address   : 004549c0
-// Signature : BOOLEnum __thiscall checkIfGfxTgxStartsWithTransparentPixels(TextureRenderCore * this, int gfxIndex)
-// ============================================================================
 
 #include "OpenSHC/IO/Graphics/GfxRef.hpp"
 #include "OpenSHC/IO/Graphics/TgxToken.hpp"
@@ -16,19 +9,13 @@ namespace OpenSHC {
 namespace UI {
     namespace Rendering {
 
-        /* decompilerscript: committed: 2025-01-30 21:57:43.216000 */
         // FUNCTION: STRONGHOLDCRUSADER 0x004549C0
         BOOLEnum TextureRenderCore::checkIfGfxTgxStartsWithTransparentPixels(int gfxIndex)
-
         {
-            // Match: 40% - Best achievable with current compiler
-            // Differences in register allocation (original uses edx/dl, recomp uses eax/cl)
-            // Disabling optimizations produces worse match (21.4%) due to frame pointer overhead
-            int index = gfxIndex;
-            GfxRef& ref = this->loadedGfxArray[index];
-            return (uint)((*(byte*)(ref.offsetInBuffer + 8 + (int)this->gmAndGfxImageDataBuffer)
-                              & OpenSHC::IO::Graphics::TT_TGX_PIXEL_HEADER)
-                == OpenSHC::IO::Graphics::TT_TRANSPARENT_PIXELS);
+            // fixme: compiler picks different registers
+            return (*(byte*)(this->loadedGfxArray[gfxIndex].offsetInBuffer + 8 + (int)this->gmAndGfxImageDataBuffer)
+                       & OpenSHC::IO::Graphics::TT_TGX_PIXEL_HEADER)
+                == OpenSHC::IO::Graphics::TT_TRANSPARENT_PIXELS;
         }
 
     }
