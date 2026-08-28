28c3ef9a8b4218cc80312fad058a5ceefebc0524
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-08-28 22:23:03 +0200

    reimplement: SHC_3BB0A8C1_0x004680B0 100%

---
+++ src/OpenSHC/Input/MouseState/updateMouseWheelStatus.cpp
@@ -0,0 +1,10 @@
+#include "../MouseState.func.hpp"
+
+namespace OpenSHC {
+namespace Input {
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x004680B0
+    void MouseState::updateMouseWheelStatus(int delta) { this->mouseWheelDelta = delta; }
+
+}
+}
