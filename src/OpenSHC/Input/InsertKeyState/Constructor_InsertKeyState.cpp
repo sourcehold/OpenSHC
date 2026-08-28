f1017d631d8ae16ab5536a975c66ee421a5eacd2
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-08-28 21:53:46 +0200

    reimplement: SHC_3BB0A8C1_0x00468C40 100%

---
+++ src/OpenSHC/Input/InsertKeyState/Constructor_InsertKeyState.cpp
@@ -0,0 +1,15 @@
+#include "../InsertKeyState.func.hpp"
+
+namespace OpenSHC {
+namespace Input {
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x00468C40
+    InsertKeyState* InsertKeyState::Constructor_InsertKeyState()
+    {
+        this->field1_0x4 = 0;
+        this->insert = 1;
+        return this;
+    }
+
+}
+}
