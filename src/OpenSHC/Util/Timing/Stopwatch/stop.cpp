954ae59e39bce855d4a79858946a5e047829480b
sourcehold <sourcehold@users.noreply.github.com>
2026-09-03 22:07:17 +0200

    reimplement: 100%

--- src/OpenSHC/Util/Timing/Stopwatch/stop.cpp
+++ src/OpenSHC/Util/Timing/Stopwatch/stop.cpp
@@ -1,38 +1,25 @@
 #include "OpenSHC/Util/Timing/Stopwatch.func.hpp"
 #include "OpenSHC/WindowsHelper/Enums/BOOLEnum.hpp"
 
-
-
-
-
 namespace OpenSHC {
 namespace Util {
-namespace Timing {
+    namespace Timing {
 
-using OpenSHC::WindowsHelper::Enums::BOOLEnum;
+        using OpenSHC::WindowsHelper::Enums::BOOLEnum;
 
+        // FUNCTION: STRONGHOLDCRUSADER 0x0046CEF0
+        void Stopwatch::stop()
+        {
+            DWORD _currentTime;
 
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
+            if (this->running_0x4 != FALSE) {
+                _currentTime = timeGetTime();
+                this->stopTime_0xc = _currentTime;
+                this->running_0x4 = FALSE;
+                this->duration_0x0 = _currentTime - this->startTime_0x8;
+            }
+        }
 
-
-// FUNCTION: STRONGHOLDCRUSADER 0x0046CEF0
-void Stopwatch::stop()
-
-{
-DWORD _currentTime;
-
-if (this->running_0x4 != FALSE) {
-_currentTime = timeGetTime();
-this->stopTime_0xc = _currentTime;
-this->running_0x4 = FALSE;
-this->duration_0x0 = _currentTime - this->startTime_0x8;
-}
-return;
-}
-
-
-}
+    }
 }
 }
