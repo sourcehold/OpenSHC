c413ed7ae09ff611e1c8e5484a574970a66035a6
sourcehold <sourcehold@users.noreply.github.com>
2026-09-03 22:34:56 +0200

    reimplement: 100%

--- src/OpenSHC/Synchrony/GameSynchronyState/clearGameCommandEntry.cpp
+++ src/OpenSHC/Synchrony/GameSynchronyState/clearGameCommandEntry.cpp
@@ -1,40 +1,19 @@
-#include "OpenSHC/Synchrony/GameSynchronyState.func.hpp"
 #include "OpenSHC/IO/LowLevelMemory.func.hpp"
-
-
+#include "OpenSHC/Synchrony/GameSynchronyState.func.hpp"
 
 #include "OpenSHC/Globals/DAT_LowLevelMemory.hpp"
 
 namespace OpenSHC {
 namespace Synchrony {
 
-
-
-
-/* 
-  WARNING: Enum "DPSEND_EnumInt": Some values do not have unique names
- */
-
-/* 
-  WARNING: Enum "DPERRInt": Some values do not have unique names
- */
-
-/* 
-  decompilerscript: committed: 2025-01-30 21:57:43.216000
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x004800B0
-void GameSynchronyState::clearGameCommandEntry(int gameCommandID)
-
-{
-if (gameCommandID < 200) {
-MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(0x4f8, '\0', (void *)((int)(
-this->DAT_GameCommandArray + gameCommandID)));
-}
-return;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x004800B0
+    void GameSynchronyState::clearGameCommandEntry(int gameCommandID)
+    {
+        if (gameCommandID < 200) {
+            MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
+                0x4f8, '\0', (void*)((int)(this->DAT_GameCommandArray + gameCommandID)));
+        }
+    }
 
 }
 }
--- src/OpenSHC/UI/MenuItemActionHandler_FindingNetworkSessions_TableRows.cpp
+++ src/OpenSHC/UI/MenuItemActionHandler_FindingNetworkSessions_TableRows.cpp
@@ -6,7 +6,6 @@ namespace OpenSHC {
 
 // FUNCTION: STRONGHOLDCRUSADER 0x0047D3F0
 void UI::MenuItemActionHandler_FindingNetworkSessions_TableRows(int param_1, ...)
-
 {
     if (DAT_GameSynchronyState::instance.scrollBarItemOffset + param_1
         < DAT_GameSynchronyState::instance.DPLAY_SessionsCount) {
