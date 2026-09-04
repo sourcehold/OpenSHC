ec8d6fbe901baa872e446439fcbcf5ce8b82d72b
sourcehold <sourcehold@users.noreply.github.com>
2026-09-04 23:56:47 +0200

    reimplement: 100%

--- src/OpenSHC/Map/TileMapState/recountTotalOwnedMoats.cpp
+++ src/OpenSHC/Map/TileMapState/recountTotalOwnedMoats.cpp
@@ -1,34 +1,18 @@
-#include "../TileMapState.func.hpp"
+#include "OpenSHC/Map/TileMapState.func.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-    /*
-      WARNING: Enum "MappersEnum": Some values do not have unique names
-     */
-
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000
-     */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x005000A0
     void TileMapState::recountTotalOwnedMoats()
-
     {
-        int iVar1;
-        byte* pbVar2;
-
         this->currentMoatCount = 0;
-        iVar1 = 1;
-        pbVar2 = &this->moats[1].owner;
-        do {
-            if (*pbVar2 != 0) {
-                this->currentMoatCount = iVar1 + 1;
+        for (int i = 1; i < 16000; i++) {
+            if (this->moats[i].owner != 0) {
+                // fixme: this must be wrong, but it is 100% Why not this->currentMoatCount += 1 ?
+                this->currentMoatCount = i + 1;
             }
-            iVar1 = iVar1 + 1;
-            pbVar2 = pbVar2 + 0x10;
-        } while (iVar1 < 16000);
-        return;
+        }
     }
 
 }
--- src/OpenSHC/Map/TileMapState/updateMoatCountdownTimers.cpp
+++ src/OpenSHC/Map/TileMapState/updateMoatCountdownTimers.cpp
@@ -1,5 +1,4 @@
-#include "../TileMapState.func.hpp"
-
+#include "OpenSHC/Map/TileMapState.func.hpp"
 #include "OpenSHC/Map/Moat.hpp"
 
 namespace OpenSHC {
@@ -7,42 +6,15 @@ namespace Map {
 
     using OpenSHC::Map::Moat;
 
-    /*
-      WARNING: Enum "MappersEnum": Some values do not have unique names
-     */
-
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000
-     */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x00500640
     void TileMapState::updateMoatCountdownTimers()
-
     {
-        Moat* _pMoats;
-        int iVar1;
-
         MACRO_CALL_MEMBER(OpenSHC::Map::TileMapState_Func::recountTotalOwnedMoats, this)();
-        iVar1 = 0;
-        /*
-          DAT_TotalOwnedMoats
-         */
-
-        if (0 < this->currentMoatCount) {
-            /*
-              SEC_MoatArray[0].field11_0xf
-             */
-
-            _pMoats = &this->moats[0];
-            do {
-                if ((_pMoats->owner != 0) && ('\0' < (char)_pMoats->someCountDown)) {
-                    _pMoats->someCountDown = _pMoats->someCountDown - 1;
-                }
-                iVar1 = iVar1 + 1;
-                _pMoats = _pMoats + 0x10;
-            } while (iVar1 < this->currentMoatCount);
+        for (int i = 0; i < this->currentMoatCount; i++) {
+            if (this->moats[i].owner != 0 && this->moats[i].someCountDown > 0) {
+                this->moats[i].someCountDown -= 1;
+            }
         }
-        return;
     }
 
 }
