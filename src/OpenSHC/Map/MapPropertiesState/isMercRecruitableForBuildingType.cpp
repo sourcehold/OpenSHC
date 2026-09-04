0fcfaf95f71e238f667656573b26e8d0ee3bcbe9
sourcehold <sourcehold@users.noreply.github.com>
2026-09-04 23:32:22 +0200

    reimplement: 100%

--- src/OpenSHC/Map/MapPropertiesState/isMercRecruitableForBuildingType.cpp
+++ src/OpenSHC/Map/MapPropertiesState/isMercRecruitableForBuildingType.cpp
@@ -1,38 +1,22 @@
 #include "OpenSHC/Map/MapPropertiesState.func.hpp"
 #include "OpenSHC/Game/GameMode2.hpp"
 
-
-
 #include "OpenSHC/Globals/DAT_GameCore.hpp"
 
 namespace OpenSHC {
 namespace Map {
 
-using OpenSHC::Game::GameMode2;
+    using OpenSHC::Game::GameMode2;
 
-
-/* 
-  Returns 1 always in editor mode. Otherwise returns SEC_MercRecruitable[param_1 - 0x38] from
-   MapPropertiesState, indicating whether the mercenary unit type associated with this building
-   offset is recruitable in the current map configuration.
-   
-   renamed by: Claude Sonnet 4.6
- */
-
-
-// FUNCTION: STRONGHOLDCRUSADER 0x004BB0B0
-int MapPropertiesState::isMercRecruitableForBuildingType(int param_1)
-
-{
-int iVar1;
-
-iVar1 = 1;
-if (DAT_GameCore::instance.gameMode_2 != OpenSHC::Game::GM_EDITOR) {
-iVar1 = (int)this->SEC_MercRecruitable[param_1 + -0x38];
-}
-return iVar1;
-}
-
+    // FUNCTION: STRONGHOLDCRUSADER 0x004BB0B0
+    int MapPropertiesState::isMercRecruitableForBuildingType(int param_1)
+    {
+        int iVar1 = 1;
+        if (DAT_GameCore::instance.gameMode_2 != OpenSHC::Game::GM_EDITOR) {
+            iVar1 = ((short*)&this->field10_0x520)[param_1];
+        }
+        return iVar1;
+    }
 
 }
 }
