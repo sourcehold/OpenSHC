f4823e6f03981804e3de9fee8a7a745d2c4c4c78
sourcehold <sourcehold@users.noreply.github.com>
2026-09-10 22:30:24 +0200

    restore proper versions

--- src/OpenSHC/AI/AICState/getAIBuildInterval.cpp
+++ src/OpenSHC/AI/AICState/getAIBuildInterval.cpp
@@ -1,24 +1,22 @@
 #include "../AICState.hpp"
-"
 
 #include "OpenSHC/AI/AITypeA.hpp"
 
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 
-    namespace OpenSHC
-{
-    namespace AI {
+namespace OpenSHC {
+namespace AI {
 
-        // FUNCTION: STRONGHOLDCRUSADER 0x004CB120
-        int AICState::getAIBuildInterval(int playerID)
+    // FUNCTION: STRONGHOLDCRUSADER 0x004CB120
+    int AICState::getAIBuildInterval(int playerID)
 
-        {
-            int const aiType = DAT_GameState::ptr->playerDataArray[playerID].aiType;
-            if (aiType == AITA_NULL) {
-                return 0;
-            }
-
-            return this->aics[(aiType - 1)].buildInterval;
+    {
+        int const aiType = DAT_GameState::ptr->playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL) {
+            return 0;
         }
+
+        return this->aics[(aiType - 1)].buildInterval;
     }
+}
 }
