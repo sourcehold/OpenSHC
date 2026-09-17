b4fe87b25b675d058148ccc94fa03f779fd2eaf5
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-09-17 20:11:51 +0200

    reimplement: SHC_3BB0A8C1_0x00462190 100%

--- src/OpenSHC/Text/TextEditorState.hpp
+++ src/OpenSHC/Text/TextEditorState.hpp
@@ -176,7 +176,7 @@ namespace Text {
 
         void popHelpDialogStack();
 
-        void openUnusedHelpTextEditorDialog(int param_1);
+        void openUnusedHelpTextEditorDialog(int sectionId);
     };
 
     static_assert_cpp98_obj(sizeof(TextEditorState) == 145780, TextEditorState);
---
+++ src/OpenSHC/Text/TextEditorState/openUnusedHelpTextEditorDialog.cpp
@@ -0,0 +1,95 @@
+#include "../TextEditorState.func.hpp"
+
+#include "OpenSHC/Game/GameCore.func.hpp"
+
+#include "OpenSHC/Globals/DAT_GameCore.hpp"
+#include "OpenSHC/Globals/DAT_UserTextHandlerState.hpp"
+#include "OpenSHC/Globals/DAT_WindowAndDirectDraw.hpp"
+#include "OpenSHC/Globals/Menu_UnusedHelpTextEditor.hpp"
+
+namespace OpenSHC {
+namespace Text {
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x00462190
+    void TextEditorState::openUnusedHelpTextEditorDialog(int sectionId)
+    {
+        if (this->helpDialogVariant) {
+            if (this->helpDialogVariant == 2) {
+                MACRO_CALL_MEMBER(TextEditorState_Func::openScenarioHelpDialog, this)(sectionId);
+                return;
+            }
+            if (this->useInGameHelpHandler) {
+                MACRO_CALL_MEMBER(TextEditorState_Func::openInGameHelpDialog, this)(sectionId);
+                return;
+            }
+            MACRO_CALL_MEMBER(TextEditorState_Func::openBuildingHelpDialog, this)(sectionId);
+            return;
+        }
+
+        if (!this->isDialogStateInitialized) {
+            this->savedMenuViewType = DAT_GameCore::instance.currentMenuViewType;
+            this->savedActiveMenuTab = DAT_GameCore::instance.activeMenuTab.tabType;
+            this->savedMenuFlag = DAT_GameCore::instance.unknownAlwaysZero01;
+            this->unknown_0x23960 = 1;
+            this->isDialogStateInitialized = TRUE;
+            this->useInGameHelpHandler = FALSE;
+            this->helpDialogVariant = 0;
+            this->helpDialogSubMode = 0;
+            this->useAlternateHelpTab = FALSE;
+            this->isCustomTextMode = FALSE;
+            this->helpContentScrollX = 0;
+            this->helpContentScrollY = 0;
+            this->useWideHelpLayout = FALSE;
+            DAT_UserTextHandlerState::instance.inputBufferIndex = 0;
+        }
+        this->activeHelpHotspotIndex = 0;
+        if (sectionId == -1) {
+            MACRO_CALL_MEMBER(TextEditorState_Func::closeHelpDialogAndReturnToMenu, this)();
+            return;
+        }
+        if (!this->useAlternateHelpTab) {
+            this->isTextHelpDialogMode = TRUE;
+            DAT_GameCore::instance.menuTabToSwitchTo.tabType = 0;
+            MACRO_CALL_MEMBER(Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
+                UI::Enums::MVT_UNUSED_HELP_TEXT_EDITOR, 0);
+        } else {
+            this->isTextHelpDialogMode = TRUE;
+            DAT_GameCore::instance.menuTabToSwitchTo.tabType = 1;
+            MACRO_CALL_MEMBER(Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
+                UI::Enums::MVT_UNUSED_HELP_TEXT_EDITOR, 0);
+        }
+        Menu_UnusedHelpTextEditor::instance.thousand = 0;
+
+        int const dialogX = DAT_WindowAndDirectDraw::instance.mainMenuBorderWidth + 80;
+        int const dialogY = DAT_WindowAndDirectDraw::instance.mainMenuBorderHeight + 60;
+        int const dialogWidth = 640;
+        int const dialogHeight = 480;
+        this->dialogX = dialogX;
+        this->dialogY = dialogY;
+        this->dialogWidth = dialogWidth;
+        this->dialogHeight = dialogHeight;
+
+        if (!this->useWideHelpLayout) {
+            this->dialogContentX = this->dialogX + 10;
+            this->dialogContentY = this->dialogY + 10;
+            this->dialogContentHeight = 600;
+            this->dialogContentWidth = 435;
+        } else {
+            this->dialogContentX = this->dialogX + 10;
+            this->dialogContentY = this->dialogY + 10;
+            this->dialogContentHeight = 664;
+            this->dialogContentWidth = 291;
+        }
+
+        for (int i = 28; i >= 0; --i) {
+            this->helpSectionHistoryStack[i + 1] = this->helpSectionHistoryStack[i];
+        }
+        this->helpSectionHistoryStack[0] = this->currentHelpSectionID;
+
+        this->currentHelpSectionID = sectionId;
+        this->helpContentScrollOffsetY = 0;
+        MACRO_CALL_MEMBER(TextEditorState_Func::loadAndLayoutHelpContent, this)();
+    }
+
+}
+}
