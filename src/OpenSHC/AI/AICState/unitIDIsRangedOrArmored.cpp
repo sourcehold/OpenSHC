63c9ce9a4281d6bb869c47e5624b449db12aa0c9
sourcehold <sourcehold@users.noreply.github.com>
2026-09-17 22:44:06 +0200

    reimplement: SHC_3BB0A8C1_0x004CC8D0 100%

--- src/OpenSHC/AI/AICState/unitIDIsRangedOrArmored.cpp
+++ src/OpenSHC/AI/AICState/unitIDIsRangedOrArmored.cpp
@@ -1,4 +1,5 @@
-#include "OpenSHC/AI/AICState.func.hpp"
+#include "../AICState.func.hpp"
+
 #include "OpenSHC/Game/Skirmish/SkirmishDefinedData.hpp"
 
 #include "OpenSHC/Globals/DAT_SkirmishDefinedData.hpp"
@@ -10,23 +11,17 @@ namespace AI {
     using OpenSHC::Game::Skirmish::SkirmishDefinedData;
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CC8D0
-    undefined4 AICState ::unitIDIsRangedOrArmored(int unitID)
-
+    undefined4 AICState::unitIDIsRangedOrArmored(int unitID)
     {
-        int _unitType = (int)(short)DAT_UnitsState::instance.units[unitID].unitType;
+        int _unitType = DAT_UnitsState::instance.units[unitID].unitType;
 
-        SkirmishDefinedData* pSVar1 = DAT_SkirmishDefinedData::ptr;
+        for (int i = 0; i < 7; i++) {
+            if (DAT_SkirmishDefinedData::instance.RangedAndArmoredUnits[i] == _unitType) {
+                return TRUE;
+            }
+        }
 
-        do {
-            if (pSVar1->RangedAndArmoredUnits[0] == _unitType)
-                return (undefined4)(1);
-
-            pSVar1 = (SkirmishDefinedData*)(pSVar1->RangedAndArmoredUnits + 1);
-
-        } while ((int)pSVar1 < 0xb3eb50);
-
-        return (undefined4)(0);
+        return FALSE;
     }
-
 }
 }
