9c02d179232e0bf779bed9dfaa6a6e417ea088bc
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2026-09-17 19:05:09 +0200

    reimplement: SHC_3BB0A8C1_0x00461CF0 100%

--- src/OpenSHC/Text/TextEditorState.hpp
+++ src/OpenSHC/Text/TextEditorState.hpp
@@ -164,9 +164,9 @@ namespace Text {
 
         void openBuildingHelpDialog(int sectionId);
 
-        void openInGameHelpDialog(int param_1);
+        void openInGameHelpDialog(int sectionId);
 
-        void openScenarioHelpDialog(int param_1);
+        void openScenarioHelpDialog(int sectionId);
 
         void openCreditsScrollDialog(int param_1);
 
