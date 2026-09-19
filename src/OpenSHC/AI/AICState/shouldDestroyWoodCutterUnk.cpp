11aaf6a42beaa343f6db6103d800f4f4e74b0225
sourcehold <sourcehold@users.noreply.github.com>
2026-09-19 15:01:21 +0200

    reimplement: SHC_3BB0A8C1_0x004CB890 100%

--- src/OpenSHC/AI/AICState/shouldDestroyWoodCutterUnk.cpp
+++ src/OpenSHC/AI/AICState/shouldDestroyWoodCutterUnk.cpp
@@ -3,6 +3,7 @@
 #include "OpenSHC/AI/AICState.hpp"
 #include "OpenSHC/AI/AITypeA.hpp"
 #include "OpenSHC/Map/Buildings/BuildingType.hpp"
+#include "OpenSHC/Game/Resources/ResourceType.hpp"
 
 #include "OpenSHC/Globals/DAT_AIVState.hpp"
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
@@ -14,33 +15,38 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB890
     BOOLEnum AICState::shouldDestroyWoodCutterUnk(int playerID)
     {
-        int _hut;
-        if (DAT_GameState::instance.playerDataArray[playerID].aiType == AITA_NULL)
+        if (DAT_GameState::instance.playerDataArray[playerID].aiType == AITA_NULL) {
             return FALSE;
-
-        if (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 0) {
-            _hut = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::findFirstBuildingIDForPlayerAndType,
-                DAT_BuildingsState::ptr)(playerID, Map::Buildings::BT_WOODCUTTERSHUT);
-        } else {
-            if (DAT_GameState::instance.playerDataArray[playerID].currentResources[2] <= 0x14)
-                return FALSE;
-            if (DAT_GameState::instance.playerDataArray[playerID].noLabourerBuildingCount < 2)
-                return FALSE;
-            if (DAT_GameState::instance.playerDataArray[playerID].someCount10 < 25)
-                return FALSE;
-            _hut = DAT_GameState::instance.playerDataArray[playerID].someCount11;
         }
 
-        if (_hut == 0)
-            return FALSE;
+        int buildingID;
+        if (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 0) {
+            buildingID = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::findFirstBuildingIDForPlayerAndType,
+                DAT_BuildingsState::ptr)(playerID, Map::Buildings::BT_WOODCUTTERSHUT);
+        } else {
+            if (DAT_GameState::instance.playerDataArray[playerID].currentResources[Game::Resources::RT_WOOD] <= 20) {
+                return FALSE;
+            }
+            if (DAT_GameState::instance.playerDataArray[playerID].noLabourerBuildingCount < 2) {
+                return FALSE;
+            }
+            if (DAT_GameState::instance.playerDataArray[playerID].someCount10 < 25) {
+                return FALSE;
+            }
+            buildingID = DAT_GameState::instance.playerDataArray[playerID].someCount11;
+        }
 
-        MACRO_CALL_MEMBER(AIVState_Func::resetCountdownInHeatMap, DAT_AIVState::ptr)(
-            (int)(short)DAT_BuildingsState::instance.buildings[_hut].x,
-            (int)(short)DAT_BuildingsState::instance.buildings[_hut].y);
-        MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::giveBackResourceForDestroyedBuilding,
-            DAT_BuildingsState::ptr)(_hut, playerID, 50);
-        MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::destroyBuilding, DAT_BuildingsState::ptr)(_hut);
-        return TRUE;
+        if (buildingID != 0) {
+            MACRO_CALL_MEMBER(AIVState_Func::resetCountdownInHeatMap, DAT_AIVState::ptr)(
+                (short)DAT_BuildingsState::instance.buildings[buildingID].x,
+                (short)DAT_BuildingsState::instance.buildings[buildingID].y);
+            MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::giveBackResourceForDestroyedBuilding,
+                DAT_BuildingsState::ptr)(buildingID, playerID, 50);
+            MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::destroyBuilding, DAT_BuildingsState::ptr)(
+                buildingID);
+            return TRUE;
+        }
+        return FALSE;
     }
 }
 }
