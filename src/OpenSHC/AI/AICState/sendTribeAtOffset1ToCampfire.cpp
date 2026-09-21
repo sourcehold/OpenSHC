ff0a34d676c491489ed10b2245f3c12292d4c1b1
Gynt <Gynt@users.noreply.github.com>
2026-09-21 20:01:30 +0200

    reimplement: SHC_3BB0A8C1_0x004D2870 100%

--- src/OpenSHC/AI/AICState/sendTribeAtOffset1ToCampfire.cpp
+++ src/OpenSHC/AI/AICState/sendTribeAtOffset1ToCampfire.cpp
@@ -9,12 +9,13 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D2870
     void AICState::sendTribeAtOffset1ToCampfire(int playerID)
     {
-        int _tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[1];
-        if ((0 < _tribeID)
-            && (DAT_TribesState::instance.tribes[_tribeID].uid
-                == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[1])) {
-            MACRO_CALL_MEMBER(AICState_Func::sendUnitsToCampfire, this)(_tribeID, playerID);
-        }
+        int _tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[1];
+        if (_tribeID <= 0)
+            return;
+        if (DAT_TribesState::instance.tribes[_tribeID].uid
+            != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[1])
+            return;
+        MACRO_CALL_MEMBER(AICState_Func::sendUnitsToCampfire, this)(_tribeID, playerID);
     }
 }
 }
