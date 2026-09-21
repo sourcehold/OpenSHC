ebcf32e626a7ce2fd53c7c8876b77c92ac3df83c
Gynt <Gynt@users.noreply.github.com>
2026-09-21 20:48:43 +0200

    reimplement: SHC_3BB0A8C1_0x004D1F00 100%

--- src/OpenSHC/AI/AICState/requestGoods.cpp
+++ src/OpenSHC/AI/AICState/requestGoods.cpp
@@ -15,56 +15,55 @@ namespace AI {
     using OpenSHC::Game::Resources::ResourceType;
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004D1F00
-    void AICState ::requestGoods(int playerID, ResourceType resourceType, int amount)
-
+    void AICState::requestGoods(int playerID, ResourceType resourceType, int amount)
     {
-
-        if ((((((DAT_GameState::instance.playerDataArray[playerID].aiType != OpenSHC::AI::AIT_NULL)
-                   && (DAT_GameState::instance.playerDataArray[playerID].aiRequestDelay == 0))
-                  && (resourceType != OpenSHC::Game::Resources::RT_WHEAT))
-                 && ((resourceType != OpenSHC::Game::Resources::RT_HOPS
-                     && (resourceType != OpenSHC::Game::Resources::RT_ALE))))
-                && ((((resourceType != OpenSHC::Game::Resources::RT_BREAD
-                          && ((resourceType != OpenSHC::Game::Resources::RT_CHEESE
-                              && (resourceType != OpenSHC::Game::Resources::RT_MEAT))))
-                         && (resourceType != OpenSHC::Game::Resources::RT_APPLE))
-                    || (DAT_GameState::instance.playerDataArray[playerID].totalFood < 5))))
-            && (((((resourceType != OpenSHC::Game::Resources::RT_BREAD
-                       && (resourceType != OpenSHC::Game::Resources::RT_CHEESE))
-                      && (resourceType != OpenSHC::Game::Resources::RT_MEAT))
-                     && (resourceType != OpenSHC::Game::Resources::RT_APPLE))
-                || (DAT_GameState::instance.playerDataArray[playerID].totalFood < 5)))) {
-
-            int iVar1;
-            int _playerID = 1;
-
-            short* psVar2 = DAT_GameState::instance.playerDataArray[1].requestedGoodsArray1Unk + playerID;
-
-            do {
-
-                if (((DAT_GameSynchronyState::instance.currentPlayerFullIDArray[_playerID] != -1)
-                        && (DAT_GameState::instance.mapAndTime.playerTeams[_playerID]
-                            == DAT_GameState::instance.mapAndTime.playerTeams[playerID]))
-                    && ((iVar1 = MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer,
-                             DAT_UnitsState::ptr)(_playerID),
-                        iVar1 != 0 && (*psVar2 == 0)))) {
-
-                    *psVar2 = (short)resourceType;
-
-                    psVar2[10] = (short)amount;
-
-                    MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::playGoodsRequestBikFromPlayerToPlayer, this)(
-                        playerID, _playerID);
-                }
-
-                _playerID = _playerID + 1;
-
-                psVar2 = psVar2 + 0x1cfa;
-
-            } while (_playerID < 9);
+        if (DAT_GameState::instance.playerDataArray[playerID].aiType == OpenSHC::AI::AIT_NULL) {
+            return;
+        }
+        if (DAT_GameState::instance.playerDataArray[playerID].aiRequestDelay != 0) {
+            return;
+        }
+        // Never request raw farm goods or ale
+        if (resourceType == OpenSHC::Game::Resources::RT_WHEAT || resourceType == OpenSHC::Game::Resources::RT_HOPS
+            || resourceType == OpenSHC::Game::Resources::RT_ALE) {
+            return;
+        }
+        // Only request food when almost out of it
+        if ((resourceType == OpenSHC::Game::Resources::RT_BREAD || resourceType == OpenSHC::Game::Resources::RT_CHEESE
+                || resourceType == OpenSHC::Game::Resources::RT_MEAT
+                || resourceType == OpenSHC::Game::Resources::RT_APPLE)
+            && DAT_GameState::instance.playerDataArray[playerID].totalFood >= 5) {
+            return;
+        }
+        // fixme: NOTE: the original performs the exact same food check a second time, it cannot be removed
+        if ((resourceType == OpenSHC::Game::Resources::RT_BREAD || resourceType == OpenSHC::Game::Resources::RT_CHEESE
+                || resourceType == OpenSHC::Game::Resources::RT_MEAT
+                || resourceType == OpenSHC::Game::Resources::RT_APPLE)
+            && DAT_GameState::instance.playerDataArray[playerID].totalFood >= 5) {
+            return;
         }
 
-        return;
+        // Ask every human teammate that has no pending request from this player
+        for (int i = 1; i < 9; i++) {
+            if (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[i] == -1) {
+                continue;
+            }
+            if (DAT_GameState::instance.mapAndTime.playerTeams[i]
+                != DAT_GameState::instance.mapAndTime.playerTeams[playerID]) {
+                continue;
+            }
+            if (MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(i)
+                == 0) {
+                continue;
+            }
+            if (DAT_GameState::instance.playerDataArray[i].requestedGoodsArray1Unk[playerID] != 0) {
+                continue;
+            }
+
+            DAT_GameState::instance.playerDataArray[i].requestedGoodsArray1Unk[playerID] = resourceType;
+            DAT_GameState::instance.playerDataArray[i].requestedGoodsArray2Unk[playerID] = amount;
+            MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::playGoodsRequestBikFromPlayerToPlayer, this)(playerID, i);
+        }
     }
 
 }
