b56a91b46df6657b6692971f6bb8561ca4b4aa6e
Gynt <Gynt@users.noreply.github.com>
2026-09-21 08:12:23 +0200

    improve style

--- src/OpenSHC/AI/AICState/setNextFarmTypeToBuild.cpp
+++ src/OpenSHC/AI/AICState/setNextFarmTypeToBuild.cpp
@@ -8,15 +8,13 @@ namespace AI {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB1B0
     int AICState::setNextFarmTypeToBuild(PlayerID playerID)
-
     {
-        unsigned int _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        int _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
         if (_aiType == AITA_NULL) {
             return 0;
         }
         _aiType += -1;
-        int* _ptrAiFarmIndex = &DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex;
-        *_ptrAiFarmIndex = *_ptrAiFarmIndex + 1;
+        DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex += 1;
         if (8 <= DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex) {
             DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex = 0;
         }
