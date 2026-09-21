6d5169f8da5dd477585ee64fd45483b30fb865d5
Gynt <Gynt@users.noreply.github.com>
2026-09-21 19:31:56 +0200

    reimplement: SHC_3BB0A8C1_0x004CD110 100%

--- src/OpenSHC/AI/AICState/sendUnitsToCampfire.cpp
+++ src/OpenSHC/AI/AICState/sendUnitsToCampfire.cpp
@@ -11,12 +11,14 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CD110
     void AICState::sendUnitsToCampfire(int tribeID, int playerID)
     {
-        if (0 < DAT_GameState::instance.playerDataArray[playerID].campground.id) {
-            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::commandUnitsToLocation, DAT_TribesState::ptr)(tribeID,
-                DAT_GameState::instance.playerDataArray[playerID].campground.xEntry,
-                DAT_GameState::instance.playerDataArray[playerID].campground.yEntry, 0);
-            DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
+        if (DAT_GameState::instance.playerDataArray[playerID].campground.id <= 0) {
+            return;
         }
+
+        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::commandUnitsToLocation, DAT_TribesState::ptr)(tribeID,
+            DAT_GameState::instance.playerDataArray[playerID].campground.xEntry,
+            DAT_GameState::instance.playerDataArray[playerID].campground.yEntry, 0);
+        DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
     }
 }
 }
