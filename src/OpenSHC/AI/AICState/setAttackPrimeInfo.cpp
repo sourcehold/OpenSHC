a58ef148e8535a4e99e2604fdf0c44f4e595ec25
Gynt <Gynt@users.noreply.github.com>
2026-09-21 17:39:29 +0200

    reimplement: SHC_3BB0A8C1_0x004CDFD0 100%

--- src/OpenSHC/AI/AICState/setAttackPrimeInfo.cpp
+++ src/OpenSHC/AI/AICState/setAttackPrimeInfo.cpp
@@ -11,17 +11,16 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CDFD0
     void AICState::setAttackPrimeInfo(int playerID, int attackedPlayerID)
     {
-        int _tile = DAT_GameState::instance.playerDataArray[playerID].shortestDistanceTile;
-        int _zone = (int)(char)DAT_TileMapState::instance.AIZoneLayer[_tile];
-        int _pathCon = (int)(short)DAT_TileMapState::instance.PathConnectionLayer[_tile];
-        int _zoneSize = DAT_PathFindingState::instance.zoneSizesArray[_pathCon];
-        DAT_TroopValueState::instance.attackInfo.startCon = _pathCon;
-        int _attackedTile = DAT_GameState::instance.playerDataArray[attackedPlayerID].campground.tileEntry;
-        DAT_TroopValueState::instance.attackInfo.startZone = _zone;
-        DAT_TroopValueState::instance.attackInfo.zoneSize = _zoneSize;
+        int tile = DAT_GameState::instance.playerDataArray[playerID].shortestDistanceTile;
+        DAT_TroopValueState::instance.attackInfo.startZone = (char)DAT_TileMapState::instance.AIZoneLayer[tile];
+        DAT_TroopValueState::instance.attackInfo.startCon = (short)DAT_TileMapState::instance.PathConnectionLayer[tile];
+        DAT_TroopValueState::instance.attackInfo.zoneSize
+            = DAT_PathFindingState::instance.zoneSizesArray[DAT_TroopValueState::instance.attackInfo.startCon];
         DAT_TroopValueState::instance.attackInfo.unknownOne_0x20f90 = 1;
-        DAT_TroopValueState::instance.attackInfo.keepCon
-            = (int)(short)DAT_TileMapState::instance.PathConnectionLayer[_attackedTile];
+        DAT_TroopValueState::instance.attackInfo.keepCon = (short)DAT_TileMapState::instance
+                                                               .PathConnectionLayer[DAT_GameState::instance
+                                                                       .playerDataArray[attackedPlayerID]
+                                                                       .campground.tileEntry];
     }
 }
 }
