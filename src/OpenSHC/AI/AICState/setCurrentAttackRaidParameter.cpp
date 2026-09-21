d1102caad3bbd31c450010e104238e3f16d2d64a
Gynt <Gynt@users.noreply.github.com>
2026-09-21 17:27:17 +0200

    reimplement: SHC_3BB0A8C1_0x004CDF10 72.00% stuck

--- src/OpenSHC/AI/AICState/setCurrentAttackRaidParameter.cpp
+++ src/OpenSHC/AI/AICState/setCurrentAttackRaidParameter.cpp
@@ -11,27 +11,30 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CDF10
     void AICState::setCurrentAttackRaidParameter(int playerID)
     {
-        int const _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (_aiType != AITA_NULL) {
-            int _rng = (int)SEC_RNG::instance.currentNumber2;
-            MACRO_CALL_MEMBER(Random::RNG_Func::nextRandomNumber2, SEC_RNG::ptr)();
-            AICSpecification const* spec = &this->aics[_aiType - 1];
-            int _gold = DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf];
-            int _raidUnitsRandom = (int)spec->RaidUnitsRandom;
-            if (_gold < 1000) {
-                _raidUnitsRandom = 0;
-            } else if (4999 < _gold) {
-                _raidUnitsRandom = _raidUnitsRandom * 2;
-            }
-            if (DAT_GameState::instance
-                    .playerDataArray[DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk]
-                    .currentResources[0xf]
-                < 500) {
-                _raidUnitsRandom = -((int)spec->RaidUnitsBase / 2);
-            }
-            DAT_GameState::instance.playerDataArray[playerID].currentAttackRaidParameter
-                = (_raidUnitsRandom * (_rng % 100)) / 100;
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL) {
+            return;
         }
+
+        int randomPercent = SEC_RNG::instance.currentNumber2 % 100;
+        MACRO_CALL_MEMBER(Random::RNG_Func::nextRandomNumber2, SEC_RNG::ptr)();
+
+        int raidUnits = this->aics[aiType - 1].RaidUnitsRandom;
+        if (DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf] < 1000) {
+            raidUnits = 0;
+        } else if (DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf] >= 5000) {
+            raidUnits *= 2;
+        }
+
+        if (DAT_GameState::instance
+                .playerDataArray[DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk]
+                .currentResources[0xf]
+            < 500) {
+            raidUnits = -(this->aics[aiType - 1].RaidUnitsBase / 2);
+        }
+
+        DAT_GameState::instance.playerDataArray[playerID].currentAttackRaidParameter
+            = (raidUnits * randomPercent) / 100;
     }
 }
 }
