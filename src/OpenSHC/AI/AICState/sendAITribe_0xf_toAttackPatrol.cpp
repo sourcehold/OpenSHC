7898e1c8ad90cb070ae2ceb95d8e112c90c767c5
Gynt <Gynt@users.noreply.github.com>
2026-09-21 20:16:08 +0200

    reimplement: SHC_3BB0A8C1_0x004CF4F0 57.10% stuck

--- src/OpenSHC/AI/AICState/sendAITribe_0xf_toAttackPatrol.cpp
+++ src/OpenSHC/AI/AICState/sendAITribe_0xf_toAttackPatrol.cpp
@@ -13,19 +13,26 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CF4F0
     void AICState::sendAITribe_0xf_toAttackPatrol(int playerID)
     {
-        int _tribeID;
-        /* bug: only sends 0xf, while 0xf+1 could also be send if defined by 'AIC: AttUnitPatrolGroupsCount' */
-        if ((DAT_GameState::instance.playerDataArray[playerID].aiType != AITA_NULL)
-            && (_tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xf], _tribeID != 0)) {
-            if ((DAT_TribesState::instance.tribes[_tribeID].uid
-                    == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xf])
-                && (DAT_TribesState::instance.tribes[_tribeID].selectionTargetUnitID != 0)) {
-                DAT_TribesState::instance.tribes[_tribeID].unitStance = Map::Units::Behavior::USE_DEFENSIVE;
-                MACRO_CALL_MEMBER(Map::Units::TroopValueState_Func::sendAttackingPatrolTribeToComputedDestination,
-                    DAT_TroopValueState::ptr)(_tribeID);
-                return;
-            }
+        if (DAT_GameState::instance.playerDataArray[playerID].aiType == AITA_NULL) {
+            return;
         }
+
+        // Bug: only tribe 15 is sent, although tribe 16 can also exist if 'AIC: AttUnitPatrolGroupsCount' is 2
+        int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[15];
+        if (tribeID == 0) {
+            return;
+        }
+        if (DAT_TribesState::instance.tribes[tribeID].uid
+            != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[15]) {
+            return;
+        }
+        if (DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID == 0) {
+            return;
+        }
+
+        DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_DEFENSIVE;
+        MACRO_CALL_MEMBER(Map::Units::TroopValueState_Func::sendAttackingPatrolTribeToComputedDestination,
+            DAT_TroopValueState::ptr)(tribeID);
     }
 }
 }
