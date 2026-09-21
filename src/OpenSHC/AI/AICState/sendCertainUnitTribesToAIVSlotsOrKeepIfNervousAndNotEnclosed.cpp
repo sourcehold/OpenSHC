7f8c96413f50cdc638c2426eb3f9a1ace2b62e46
Gynt <Gynt@users.noreply.github.com>
2026-09-21 20:09:56 +0200

    reimplement: SHC_3BB0A8C1_0x004D4220 100%

--- src/OpenSHC/AI/AICState/sendCertainUnitTribesToAIVSlotsOrKeepIfNervousAndNotEnclosed.cpp
+++ src/OpenSHC/AI/AICState/sendCertainUnitTribesToAIVSlotsOrKeepIfNervousAndNotEnclosed.cpp
@@ -20,90 +20,47 @@ namespace AI {
     using OpenSHC::Map::Units::Behavior::UnitStanceEnum;
     using OpenSHC::WindowsHelper::Enums::BOOLEnum;
 
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004D4220
-    void AICState ::sendCertainUnitTribesToAIVSlotsOrKeepIfNervousAndNotEnclosed(int playerID)
-
+    void AICState::sendCertainUnitTribesToAIVSlotsOrKeepIfNervousAndNotEnclosed(int playerID)
     {
+        BOOLEnum enclosed = MACRO_CALL_MEMBER(
+            OpenSHC::Game::GameStateStructures_Func::checkKeepEnclosed, DAT_GameState::ptr)(playerID);
 
-        BOOLEnum _enclosed;
-
-        AIVUnitTypeInt* _ptr;
-
-        int _tribe;
-
-        int _index;
-
-        int _limit;
-
-        int* _nervous;
-
-        _enclosed = MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::checkKeepEnclosed, DAT_GameState::ptr)(
-            playerID);
-
-        _ptr = &DAT_SkirmishDefinedData::instance.MaxAIVLocationForGroundUnits[0].aivUnitType;
-
-        do {
-
-            _limit = DAT_GameState::instance.playerDataArray[playerID].aivUnitLocationSlotLocationCount[*_ptr];
-
-            _index = 0;
-
-            if (0 < _limit) {
-
-                _nervous = &DAT_GameState::instance.playerDataArray[playerID].aiNervousActionsTracker;
-
-                do {
-
-                    _tribe = (int)DAT_GameState::instance.playerDataArray[playerID]
-                                 .aiTribeIDs[((AIVUnitTypeMaxLocationPair*)(_ptr + -1))->tribeArrayOffset + _index];
-
-                    if ((_tribe != 0)
-                        && (DAT_TribesState::instance.tribes[_tribe].uid
-                            == DAT_GameState::instance.playerDataArray[playerID]
-                                .aiTribeUIDs[((AIVUnitTypeMaxLocationPair*)(_ptr + -1))->tribeArrayOffset + _index])) {
-
-                        if ((*_nervous < 1) || (_enclosed != FALSE)) {
-
-                            MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::sendTribeToAIVLocationSlot, this)(
-                                _tribe, (AIVUnitType)((int)(*_ptr)), _index);
-
-                        }
-
-                        else {
-
-                            MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::sendUnitsToKeep, this)(_tribe, playerID);
-                        }
+        for (int i = 0; i < 6; i++) {
+            int count = DAT_GameState::instance.playerDataArray[playerID].aivUnitLocationSlotLocationCount
+                            [DAT_SkirmishDefinedData::instance.MaxAIVLocationForGroundUnits[i].aivUnitType];
+            for (int j = 0; j < count; j++) {
+                int tribeID = DAT_GameState::instance.playerDataArray[playerID]
+                                  .aiTribeIDs[DAT_SkirmishDefinedData::instance.MaxAIVLocationForGroundUnits[i]
+                                                  .tribeArrayOffset
+                                      + j];
+                if (tribeID != 0
+                    && DAT_TribesState::instance.tribes[tribeID].uid
+                        == DAT_GameState::instance.playerDataArray[playerID]
+                               .aiTribeUIDs[DAT_SkirmishDefinedData::instance.MaxAIVLocationForGroundUnits[i]
+                                                .tribeArrayOffset
+                                   + j]) {
+                    if (DAT_GameState::instance.playerDataArray[playerID].aiNervousActionsTracker <= 0
+                        || enclosed != FALSE) {
+                        MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::sendTribeToAIVLocationSlot, this)(tribeID,
+                            (AIVUnitType)DAT_SkirmishDefinedData::instance.MaxAIVLocationForGroundUnits[i].aivUnitType,
+                            j);
+                    } else {
+                        MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::sendUnitsToKeep, this)(tribeID, playerID);
                     }
+                }
 
-                    /*
-                              bug:fixme: _tribe could be 0 */
-
-                    if ((*_nervous < 1) || (_enclosed != FALSE)) {
-
-                        DAT_TribesState::instance.tribes[_tribe].unitStance
-                            = OpenSHC::Map::Units::Behavior::USE_AGGRESSIVE;
-
-                    }
-
-                    else {
-
-                        DAT_TribesState::instance.tribes[_tribe].unitStance
-                            = OpenSHC::Map::Units::Behavior::USE_DEFENSIVE;
-                    }
-
-                    _index = _index + 1;
-
-                } while (_index < _limit);
+                // NOTE: the original sets the stance even when the tribe is invalid (tribeID may be 0)
+                if (DAT_GameState::instance.playerDataArray[playerID].aiNervousActionsTracker <= 0
+                    || enclosed != FALSE) {
+                    DAT_TribesState::instance.tribes[tribeID].unitStance
+                        = OpenSHC::Map::Units::Behavior::USE_AGGRESSIVE;
+                } else {
+                    DAT_TribesState::instance.tribes[tribeID].unitStance
+                        = OpenSHC::Map::Units::Behavior::USE_DEFENSIVE;
+                }
             }
-
-            _ptr = _ptr + 2;
-
-        } while ((int)_ptr < 0xb4277c);
-
-        return;
+        }
     }
 
 }
