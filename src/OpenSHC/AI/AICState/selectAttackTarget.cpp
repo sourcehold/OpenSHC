482fa2cd20b31f078dc33f8750ad10abc905d90e
Gynt <Gynt@users.noreply.github.com>
2026-09-21 20:44:23 +0200

    reimplement: SHC_3BB0A8C1_0x004D4680 64.80%

--- src/OpenSHC/AI/AICState/selectAttackTarget.cpp
+++ src/OpenSHC/AI/AICState/selectAttackTarget.cpp
@@ -23,130 +23,121 @@ namespace AI {
     using OpenSHC::Game::Player::PlayerData;
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004D4680
-    void AICState ::selectAttackTarget(int playerID)
+    void AICState::selectAttackTarget(int playerID)
     {
-        AITypeInt _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        int _selectedPlayer = 0;
+        int selectedPlayerID = 0;
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == OpenSHC::AI::AIT_NULL) {
+            return;
+        }
 
-        if (_aiType != OpenSHC::AI::AIT_NULL) {
-            int fromXPosition = DAT_GameState::instance.playerDataArray[playerID].campground.xEntry;
-            int fromYPosition = DAT_GameState::instance.playerDataArray[playerID].campground.yEntry;
-            int _lowestDistance = 10000;
-            int _highestGold = 0;
-            int _lowestPoints = 1000000;
+        int aicIndex = aiType - 1;
+        int fromX = DAT_GameState::instance.playerDataArray[playerID].campground.xEntry;
+        int fromY = DAT_GameState::instance.playerDataArray[playerID].campground.yEntry;
+        int lowestDistance = 10000;
+        int highestGold = 0;
+        int lowestPoints = 1000000;
 
-            MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::computeNervousness, this)(playerID);
+        MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::computeNervousness, this)(playerID);
 
-            int _reqState = DAT_GameState::instance.playerDataArray[playerID].requestStateUnk;
-            if (((_reqState == 2) || (_reqState == 1))
-                && MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
-                       DAT_GameState::instance.playerDataArray[playerID].playerID_askerUnk)
-                    == 0) {
-                DAT_GameState::instance.playerDataArray[playerID].requestStateUnk = 0;
-            }
+        // Drop a pending request if the asking player's lord is dead
+        if ((DAT_GameState::instance.playerDataArray[playerID].requestStateUnk == 2
+                || DAT_GameState::instance.playerDataArray[playerID].requestStateUnk == 1)
+            && MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
+                   DAT_GameState::instance.playerDataArray[playerID].playerID_askerUnk)
+                == 0) {
+            DAT_GameState::instance.playerDataArray[playerID].requestStateUnk = 0;
+        }
 
-            int _reqState2 = DAT_GameState::instance.playerDataArray[playerID].requestStateUnk;
-            if (_reqState2 == 2) {
-                DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID = 0;
+        if (DAT_GameState::instance.playerDataArray[playerID].requestStateUnk == 2) {
+            DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID = 0;
+            return;
+        }
+        if (DAT_GameState::instance.playerDataArray[playerID].requestStateUnk == 1) {
+            if (MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
+                    DAT_GameState::instance.playerDataArray[playerID].requestedAttackTargetUnk)
+                != 0) {
+                DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID
+                    = DAT_GameState::instance.playerDataArray[playerID].requestedAttackTargetUnk;
                 return;
             }
-            if (_reqState2 == 1) {
-                if (MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
-                        DAT_GameState::instance.playerDataArray[playerID].requestedAttackTargetUnk)
-                    != 0) {
-                    DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID
-                        = DAT_GameState::instance.playerDataArray[playerID].requestedAttackTargetUnk;
-                    return;
-                }
-                DAT_GameState::instance.playerDataArray[playerID].requestStateUnk = 0;
-            }
-
-            PlayerData* _pPlayerData2 = (PlayerData*)(&DAT_GameState::instance.playerDataArray[1]);
-            int _iPlayer = 1;
-
-            do {
-                if ((DAT_GameState::instance.mapAndTime.playerTeams[_iPlayer]
-                        != DAT_GameState::instance.mapAndTime.playerTeams[playerID])
-                    && MACRO_CALL_MEMBER(
-                           OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(_iPlayer)
-                        != 0) {
-
-                    MACRO_CALL_MEMBER(
-                        OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::setAxisBasedDistanceResult,
-                        DAT_DirectionAlgorithmState::ptr)((_pPlayerData2->campground).xEntry,
-                        (_pPlayerData2->campground).yEntry, fromXPosition, fromYPosition);
-
-                    AITargetChoiceInt _targetChoiceType = this->aics[_aiType + ~OpenSHC::AI::AIT_NULL].TargetChoice;
-                    int _currentGoldValue = _highestGold;
-                    int _points = _lowestPoints;
-
-                    if (_targetChoiceType == OpenSHC::AI::AIC::AITC_PLAYER) {
-                        if (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[_iPlayer] != -1) {
-                            if (DAT_DirectionAlgorithmState::instance.distanceHigh <= _lowestDistance) {
-                                _lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
-                                _lowestPoints = _points;
-                                _highestGold = _currentGoldValue;
-                                _selectedPlayer = _iPlayer;
-                            }
-                        }
-                    } else if (_targetChoiceType == OpenSHC::AI::AIC::AITC_CLOSEST) {
-                        if (DAT_DirectionAlgorithmState::instance.distanceHigh <= _lowestDistance) {
-                            _lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
-                            _lowestPoints = _points;
-                            _highestGold = _currentGoldValue;
-                            _selectedPlayer = _iPlayer;
-                        }
-                    } else if (_targetChoiceType == OpenSHC::AI::AIC::AITC_GOLD) {
-                        _currentGoldValue = _pPlayerData2->currentResources[0xf];
-                        if (_highestGold <= _pPlayerData2->currentResources[0xf]) {
-                            _lowestPoints = _points;
-                            _highestGold = _currentGoldValue;
-                            _selectedPlayer = _iPlayer;
-                        }
-                    } else if (_targetChoiceType == OpenSHC::AI::AIC::AITC_BALANCED) {
-                        _points = _pPlayerData2->currentPopulation * 5 + _pPlayerData2->totalTroopValue
-                            + _pPlayerData2->currentResources[0xf] / 100
-                            + DAT_DirectionAlgorithmState::instance.distanceHigh * 2;
-                        if (_points <= _lowestPoints) {
-                            _lowestPoints = _points;
-                            _highestGold = _currentGoldValue;
-                            _selectedPlayer = _iPlayer;
-                        }
-                    }
-                }
-
-                _iPlayer = _iPlayer + 1;
-                _pPlayerData2 = _pPlayerData2 + 0xe7d;
-            } while (_iPlayer < 9);
-
-            if (_selectedPlayer == 0) {
-                int _iPlayer2 = 1;
-                PlayerData* _ptrPlayerData = &DAT_GameState::instance.playerDataArray[1];
-                do {
-                    if (((DAT_GameState::instance.mapAndTime.playerTeams[_iPlayer2]
-                             != DAT_GameState::instance.mapAndTime.playerTeams[playerID])
-                            && MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer,
-                                   DAT_UnitsState::ptr)(_iPlayer2)
-                                != 0)
-                        && (MACRO_CALL_MEMBER(
-                                OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::setAxisBasedDistanceResult,
-                                DAT_DirectionAlgorithmState::ptr)((_ptrPlayerData->campground).xEntry,
-                                (_ptrPlayerData->campground).yEntry, fromXPosition, fromYPosition),
-                            DAT_DirectionAlgorithmState::instance.distanceHigh <= _lowestDistance)) {
-                        _lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
-                        _selectedPlayer = _iPlayer2;
-                    }
-                    _iPlayer2 = _iPlayer2 + 1;
-                    _ptrPlayerData = _ptrPlayerData + 0xe7d;
-                } while (_iPlayer2 < 9);
-
-                if (_selectedPlayer == 0)
-                    return;
-            }
-
-            DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk = _selectedPlayer;
-            DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID = _selectedPlayer;
+            DAT_GameState::instance.playerDataArray[playerID].requestStateUnk = 0;
         }
+
+        for (int i = 1; i < 9; i++) {
+            if (DAT_GameState::instance.mapAndTime.playerTeams[i]
+                == DAT_GameState::instance.mapAndTime.playerTeams[playerID]) {
+                continue;
+            }
+            if (MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(i)
+                == 0) {
+                continue;
+            }
+
+            MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::setAxisBasedDistanceResult,
+                DAT_DirectionAlgorithmState::ptr)(DAT_GameState::instance.playerDataArray[i].campground.xEntry,
+                DAT_GameState::instance.playerDataArray[i].campground.yEntry, fromX, fromY);
+
+            int targetChoice = this->aics[aicIndex].TargetChoice;
+            if (targetChoice == OpenSHC::AI::AIC::AITC_PLAYER) {
+                // Closest human player
+                if (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[i] != -1) {
+                    if (DAT_DirectionAlgorithmState::instance.distanceHigh <= lowestDistance) {
+                        lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
+                        selectedPlayerID = i;
+                    }
+                }
+            } else if (targetChoice == OpenSHC::AI::AIC::AITC_CLOSEST) {
+                if (DAT_DirectionAlgorithmState::instance.distanceHigh <= lowestDistance) {
+                    lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
+                    selectedPlayerID = i;
+                }
+            } else if (targetChoice == OpenSHC::AI::AIC::AITC_GOLD) {
+                if (DAT_GameState::instance.playerDataArray[i].currentResources[0xf] >= highestGold) {
+                    highestGold = DAT_GameState::instance.playerDataArray[i].currentResources[0xf];
+                    selectedPlayerID = i;
+                }
+            } else if (targetChoice == OpenSHC::AI::AIC::AITC_BALANCED) {
+                int points = DAT_GameState::instance.playerDataArray[i].currentPopulation * 5
+                    + DAT_GameState::instance.playerDataArray[i].totalTroopValue
+                    + DAT_GameState::instance.playerDataArray[i].currentResources[0xf] / 100
+                    + DAT_DirectionAlgorithmState::instance.distanceHigh * 2;
+                if (points <= lowestPoints) {
+                    lowestPoints = points;
+                    selectedPlayerID = i;
+                }
+            }
+        }
+
+        // Fall back to the closest enemy
+        if (selectedPlayerID == 0) {
+            for (int i = 1; i < 9; i++) {
+                if (DAT_GameState::instance.mapAndTime.playerTeams[i]
+                    == DAT_GameState::instance.mapAndTime.playerTeams[playerID]) {
+                    continue;
+                }
+                if (MACRO_CALL_MEMBER(
+                        OpenSHC::Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(i)
+                    == 0) {
+                    continue;
+                }
+
+                MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::setAxisBasedDistanceResult,
+                    DAT_DirectionAlgorithmState::ptr)(DAT_GameState::instance.playerDataArray[i].campground.xEntry,
+                    DAT_GameState::instance.playerDataArray[i].campground.yEntry, fromX, fromY);
+                if (DAT_DirectionAlgorithmState::instance.distanceHigh <= lowestDistance) {
+                    lowestDistance = DAT_DirectionAlgorithmState::instance.distanceHigh;
+                    selectedPlayerID = i;
+                }
+            }
+
+            if (selectedPlayerID == 0) {
+                return;
+            }
+        }
+
+        DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk = selectedPlayerID;
+        DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID = selectedPlayerID;
     }
 }
 }
