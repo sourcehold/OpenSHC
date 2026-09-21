e524977ad1120a1c8ff37784090452199bf70bd7
Gynt <Gynt@users.noreply.github.com>
2026-09-21 08:23:07 +0200

    reimplement: SHC_3BB0A8C1_0x004CF5E0 100%

--- src/OpenSHC/AI/AICState/setEightTribe0xc0TribesToAggressiveAndAttack.cpp
+++ src/OpenSHC/AI/AICState/setEightTribe0xc0TribesToAggressiveAndAttack.cpp
@@ -13,22 +13,25 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CF5E0
     void AICState::setEightTribe0xc0TribesToAggressiveAndAttack(int playerID)
     {
-        if (DAT_GameState::instance.playerDataArray[playerID].aiType != AITA_NULL) {
-            int iVar1 = 0;
-            short* psVar2 = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs + 0xc0;
-            do {
-                int _tribeID = (int)*psVar2;
-                if (((_tribeID != 0)
-                        && (DAT_TribesState::instance.tribes[_tribeID].uid
-                            == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[iVar1 + 0xc0]))
-                    && (DAT_TribesState::instance.tribes[_tribeID].selectionTargetUnitID != 0)) {
-                    DAT_TribesState::instance.tribes[_tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
-                    MACRO_CALL_MEMBER(Map::Units::TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
-                        _tribeID, Map::Units::STBT_0x3f2);
-                }
-                iVar1 = iVar1 + 1;
-                psVar2 = psVar2 + 1;
-            } while (iVar1 < 8);
+        if (DAT_GameState::instance.playerDataArray[playerID].aiType == AITA_NULL) {
+            return;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[i + 0xc0];
+            if (tribeID == 0) {
+                continue;
+            }
+            if (DAT_TribesState::instance.tribes[tribeID].uid
+                != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[i + 0xc0]) {
+                continue;
+            }
+            if (DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID == 0) {
+                continue;
+            }
+            DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
+                tribeID, Map::Units::STBT_0x3f2);
         }
     }
 }
