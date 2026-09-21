68648d5cdac115e3d540e2f4109387fb9f51bc29
Gynt <Gynt@users.noreply.github.com>
2026-09-21 19:33:08 +0200

    reimplement: SHC_3BB0A8C1_0x004CF750 100%

--- src/OpenSHC/AI/AICState/sendTribeToAttack.cpp
+++ src/OpenSHC/AI/AICState/sendTribeToAttack.cpp
@@ -13,17 +13,25 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CF750
     void AICState::sendTribeToAttack(int playerID)
     {
-        int tribeID;
-        if ((DAT_GameState::instance.playerDataArray[playerID].aiType != AITA_NULL)
-            && (tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xbd], tribeID != 0)) {
-            if ((DAT_TribesState::instance.tribes[tribeID].uid
-                    == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xbd])
-                && (DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID != 0)) {
-                DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
-                MACRO_CALL_MEMBER(Map::Units::TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
-                    tribeID, Map::Units::STBT_0x3fb);
-            }
+        if (DAT_GameState::instance.playerDataArray[playerID].aiType == AITA_NULL) {
+            return;
         }
+
+        int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xbd];
+        if (tribeID == 0) {
+            return;
+        }
+        if (DAT_TribesState::instance.tribes[tribeID].uid
+            != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xbd]) {
+            return;
+        }
+        if (DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID == 0) {
+            return;
+        }
+
+        DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_AGGRESSIVE;
+        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
+            tribeID, Map::Units::STBT_0x3fb);
     }
 }
 }
