8569de1baf278591190d3ac090ace0a78dc5a4de
Gynt <Gynt@users.noreply.github.com>
2026-09-22 19:26:29 +0200

    reimplement: SHC_3BB0A8C1_0x004CDF10 98.15% Reimplemented

--- src/OpenSHC/AI/AICState/setCurrentAttackRaidParameter.cpp
+++ src/OpenSHC/AI/AICState/setCurrentAttackRaidParameter.cpp
@@ -12,25 +12,24 @@ namespace AI {
     void AICState::setCurrentAttackRaidParameter(int playerID)
     {
         int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (aiType == AITA_NULL) {
+        if (aiType == AITA_NULL)
             return;
-        }
+        int aicIndex = aiType - 1;
 
         int randomPercent = SEC_RNG::instance.currentNumber2 % 100;
         MACRO_CALL_MEMBER(Random::RNG_Func::nextRandomNumber2, SEC_RNG::ptr)();
 
-        int raidUnits = this->aics[aiType - 1].RaidUnitsRandom;
-        if (DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf] < 1000) {
+        int attackedPlayerID = DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk;
+        int raidUnits = this->aics[aicIndex].RaidUnitsRandom;
+        if (DAT_GameState::instance.playerDataArray[playerID].currentResources[Game::Resources::RT_GOLD] < 1000) {
             raidUnits = 0;
-        } else if (DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf] >= 5000) {
+        } else if (DAT_GameState::instance.playerDataArray[playerID].currentResources[Game::Resources::RT_GOLD] >= 5000) {
             raidUnits *= 2;
         }
 
-        if (DAT_GameState::instance
-                .playerDataArray[DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID_2Unk]
-                .currentResources[0xf]
+        if (DAT_GameState::instance.playerDataArray[attackedPlayerID].currentResources[Game::Resources::RT_GOLD]
             < 500) {
-            raidUnits = -(this->aics[aiType - 1].RaidUnitsBase / 2);
+            raidUnits = -(this->aics[aicIndex].RaidUnitsBase / 2);
         }
 
         DAT_GameState::instance.playerDataArray[playerID].currentAttackRaidParameter
