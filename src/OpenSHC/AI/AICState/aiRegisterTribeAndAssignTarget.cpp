bf7afe8cda76dd3eb1b1d8e1aad1289d982dc41c
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:51:21 +0200

    reimplement: SHC_3BB0A8C1_0x004D1950 98.00% Reimplemented

--- src/OpenSHC/AI/AICState/aiRegisterTribeAndAssignTarget.cpp
+++ src/OpenSHC/AI/AICState/aiRegisterTribeAndAssignTarget.cpp
@@ -8,62 +8,29 @@
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::AI::AIType;
-
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004D1950
-    void AICState ::aiRegisterTribeAndAssignTarget(int tribeID, int tribeUID)
-
+    void AICState::aiRegisterTribeAndAssignTarget(int tribeID, int tribeUID)
     {
+        if (DAT_TribesState::instance.tribes[tribeID].size > 0) {
+            if (DAT_GameState::instance.playerDataArray[DAT_TribesState::instance.tribes[tribeID].owner].aiType
+                == OpenSHC::AI::AIT_NULL)
+                return;
 
-        int _index;
+            for (int i = 0; i < 1000; i++) {
+                int existingTribeID = this->tribeIDArray[i];
+                if (existingTribeID != 0
+                    && DAT_TribesState::instance.tribes[existingTribeID].uid == this->tribeUIDArray[i])
+                    continue;
 
-        short* _tribe;
-
-        int* _uid;
-
-        if (DAT_TribesState::instance.tribes[tribeID].size < 1) {
-
-            DAT_TribesState::instance.tribes[tribeID].tribeState = 3;
-
+                this->tribeIDArray[i] = tribeID;
+                this->tribeUIDArray[i] = tribeUID;
+                MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::setTargetForTribeBasedOnTargetChoiceLogic, this)(
+                    DAT_TribesState::instance.tribes[tribeID].owner, tribeID);
+                return;
+            }
             return;
         }
-
-        if (DAT_GameState::instance.playerDataArray[DAT_TribesState::instance.tribes[tribeID].owner].aiType
-            != OpenSHC::AI::AIT_NULL) {
-
-            _index = 0;
-
-            _uid = this->tribeUIDArray;
-
-            _tribe = this->tribeIDArray;
-
-            do {
-
-                if ((*_tribe == 0) || (DAT_TribesState::instance.tribes[*_tribe].uid != *_uid)) {
-
-                    this->tribeIDArray[_index] = (short)tribeID;
-
-                    this->tribeUIDArray[_index] = tribeUID;
-
-                    MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::setTargetForTribeBasedOnTargetChoiceLogic, this)(
-                        DAT_TribesState::instance.tribes[tribeID].owner, tribeID);
-
-                    return;
-                }
-
-                _index = _index + 1;
-
-                _tribe = _tribe + 1;
-
-                _uid = _uid + 1;
-
-            } while (_index < 1000);
-        }
-
-        return;
+        DAT_TribesState::instance.tribes[tribeID].tribeState = 3;
     }
 
 }
