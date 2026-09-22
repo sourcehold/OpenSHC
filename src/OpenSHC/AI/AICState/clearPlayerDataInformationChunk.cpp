503df75e3b2b6e2f803d7e84dcdd3135c3d08260
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:13:52 +0200

    reimplement: SHC_3BB0A8C1_0x004CAC70 95.24% Reimplemented

--- src/OpenSHC/AI/AICState/clearPlayerDataInformationChunk.cpp
+++ src/OpenSHC/AI/AICState/clearPlayerDataInformationChunk.cpp
@@ -1,7 +1,6 @@
 #include "../AICState.func.hpp"
 
 #include "OpenSHC/OS.func.hpp"
-#include "OpenSHC/Synchrony/GameSynchronyState.hpp"
 
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 #include "OpenSHC/Globals/DAT_GameSynchronyState.hpp"
@@ -9,28 +8,14 @@
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::Synchrony::GameSynchronyState;
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CAC70
-    void AICState ::clearPlayerDataInformationChunk()
-
+    void AICState::clearPlayerDataInformationChunk()
     {
-        int* _Dst = &DAT_GameState::instance.playerDataArray[0].aiFarmIndex;
-        int* _pAI = DAT_GameSynchronyState::instance.currentAIArray;
-        int* _pFullID = DAT_GameSynchronyState::instance.currentPlayerFullIDArray;
-
-        do {
-            if (*_pFullID != -1 || *_pAI == 0) {
-                MACRO_CALL(OpenSHC::OS_Func::_memset)(_Dst, 0, (size_t)((int)(3748)));
-            }
-
-            _pAI = _pAI + 1;
-            _pFullID = _pFullID + 1;
-            _Dst = _Dst + 3709;
-
-        } while ((int)_pAI < 0x191dea0);
-
-        return;
+        for (int i = 0; i < 9; i++) {
+            if (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[i] != -1
+                || DAT_GameSynchronyState::instance.currentAIArray[i] == 0)
+                MACRO_CALL(OpenSHC::OS_Func::_memset)(&DAT_GameState::instance.playerDataArray[i].aiFarmIndex, 0, 3748);
+        }
     }
 
 }
