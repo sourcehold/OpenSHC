77d9f2426e4dbd11cecf73c6bd09e01a6d46c898
Gynt <Gynt@users.noreply.github.com>
2026-09-22 09:06:25 +0200

    reimplement: SHC_3BB0A8C1_0x004CE1A0 100%

--- src/OpenSHC/AI/AICState/recomputeAIZonerLayer.cpp
+++ src/OpenSHC/AI/AICState/recomputeAIZonerLayer.cpp
@@ -12,31 +12,19 @@
 namespace OpenSHC {
 namespace AI {
 
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CE1A0
-    void AICState ::recomputeAIZonerLayer()
-
+    void AICState::recomputeAIZonerLayer()
     {
-
         DAT_TroopValueState::instance.attackInfo.playerInfo[0].currentPlayerID_OR_attackedPlayerID_OR_one = 1;
-
         MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
-            80400, '\0', (void*)((int)(DAT_TileMapState::instance.AIZoneLayer)));
-
+            80400, '\0', DAT_TileMapState::instance.AIZoneLayer);
         MACRO_CALL_MEMBER(
-            OpenSHC::Map::Navigation::PathFindingState_Func::computeAIZoneLayer, DAT_PathFindingState::ptr)(1, 2);
-
+            OpenSHC::Map::Navigation::PathFindingState_Func::computeAIZoneLayer, DAT_PathFindingState::ptr)(1, 2, 1);
         MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::updateWalkLayerAndAIPathCostLayer,
             DAT_PathFindingState::ptr)(252, 0, 1, 1);
-
         DAT_GameState::instance.playerDataArray[1].unknownPathBorderDistanceRelated
             = DAT_PathFindingState::instance.distance;
-
         DAT_TroopValueState::instance.attackInfo.playerInfo[0].currentPlayerID_OR_attackedPlayerID_OR_one = 1;
-
-        return;
     }
 
 }
