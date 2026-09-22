1c7cdd3c7a5f2d714dd18d38e31e68747f1f67ed
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:55:06 +0200

    reimplement: SHC_3BB0A8C1_0x004CD160 91.67% Register allocation differs

--- src/OpenSHC/AI/AICState/digNearestMoat.cpp
+++ src/OpenSHC/AI/AICState/digNearestMoat.cpp
@@ -16,36 +16,37 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CD160
     void AICState::digNearestMoat(int playerID)
     {
-        int iVar1;
-        BOOLEnum tile;
-        int tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[10];
-        if ((((tribeID != 0)
-                 && (DAT_TribesState::instance.tribes[tribeID].uid
-                     == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[10]))
-                && (0 < DAT_TribesState::instance.tribes[tribeID].size))
-            && (DAT_TribesState::instance.tribes[tribeID].percentageSomething < 0x51)) {
-            iVar1 = MACRO_CALL_MEMBER(Map::TileMapState_Func::countUnfinishedMoatTilesForPlayer, DAT_TileMapState::ptr)(
-                playerID);
-            if (iVar1 < 1) {
-                MACRO_CALL_MEMBER(AICState_Func::sendUnitsToKeep, this)(tribeID, playerID);
-                return;
-            }
-            iVar1 = (int)DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID;
-            tile = MACRO_CALL_MEMBER(Map::TileMapState_Func::findNearestFriendlyMoatTileForDigging,
-                DAT_TileMapState::ptr)(playerID, iVar1, 1);
-            if (-1 < (int)tile) {
-                MACRO_CALL_MEMBER(Map::TileMapState_Func::setXYBasedOnMoatID, DAT_TileMapState::ptr)(tile, 1,
-                    (int)DAT_UnitsState::instance.units[iVar1].x, (int)DAT_UnitsState::instance.units[iVar1].y);
-                iVar1 = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::updateTribeRallyFlags, DAT_TribesState::ptr)(
-                    tribeID);
-                if (iVar1 == 0) {
-                    MACRO_CALL_MEMBER(Map::Units::TribesState_Func::giveTribeAnInstruction, DAT_TribesState::ptr)(
-                        tribeID, Map::Units::UIT_DIG_MOAT, DAT_TileMapState::instance.ALG_MoatXResult,
-                        DAT_TileMapState::instance.ALG_MoatYResult, 0);
-                    DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_STAND_GROUND;
-                }
-            }
+        int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[10];
+        if (tribeID == 0)
+            return;
+        if (DAT_TribesState::instance.tribes[tribeID].uid != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[10])
+            return;
+        if (DAT_TribesState::instance.tribes[tribeID].size <= 0)
+            return;
+        if (DAT_TribesState::instance.tribes[tribeID].percentageSomething > 80)
+            return;
+
+        if (MACRO_CALL_MEMBER(Map::TileMapState_Func::countUnfinishedMoatTilesForPlayer, DAT_TileMapState::ptr)(playerID)
+            <= 0) {
+            MACRO_CALL_MEMBER(AICState_Func::sendUnitsToKeep, this)(tribeID, playerID);
+            return;
         }
+
+        int targetUnitID = DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID;
+        int tile = MACRO_CALL_MEMBER(Map::TileMapState_Func::findNearestFriendlyMoatTileForDigging, DAT_TileMapState::ptr)(
+            playerID, targetUnitID, 1);
+        if (tile <= -1)
+            return;
+
+        MACRO_CALL_MEMBER(Map::TileMapState_Func::setXYBasedOnMoatID, DAT_TileMapState::ptr)(
+            tile, 1, DAT_UnitsState::instance.units[targetUnitID].x, DAT_UnitsState::instance.units[targetUnitID].y);
+        if (MACRO_CALL_MEMBER(Map::Units::TribesState_Func::updateTribeRallyFlags, DAT_TribesState::ptr)(tribeID) != 0)
+            return;
+
+        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::giveTribeAnInstruction, DAT_TribesState::ptr)(tribeID,
+            Map::Units::UIT_DIG_MOAT, DAT_TileMapState::instance.ALG_MoatXResult, DAT_TileMapState::instance.ALG_MoatYResult,
+            0);
+        DAT_TribesState::instance.tribes[tribeID].unitStance = Map::Units::Behavior::USE_STAND_GROUND;
     }
 }
 }
