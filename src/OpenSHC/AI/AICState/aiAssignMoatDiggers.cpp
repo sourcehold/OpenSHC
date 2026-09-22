4a5694b962ff4629bfd03d61efcd5865499b2468
Gynt <Gynt@users.noreply.github.com>
2026-09-22 17:07:03 +0200

    reimplement: SHC_3BB0A8C1_0x004D3F20 97.40% Reimplemented

--- src/OpenSHC/AI/AICState/aiAssignMoatDiggers.cpp
+++ src/OpenSHC/AI/AICState/aiAssignMoatDiggers.cpp
@@ -10,40 +10,41 @@
 namespace OpenSHC {
 namespace AI {
 
-    // FUNCTION: STRONGHOLDCRUSADER 0x004D3F70
+    using namespace OpenSHC::Map::Units;
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x004D3F20
     void AICState::aiAssignMoatDiggers(int playerID)
     {
-        int _unitID;
-        int _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        Map::Units::UnitTypeShort _unitType;
-        if (((_aiType != AITA_NULL) && (0 < DAT_GameState::instance.playerDataArray[playerID].totalTroopsType0))
-            && (_unitID = 1, 1 < (int)DAT_UnitsState::instance.maxUnitCount)) {
-            Map::Units::Unit* _ptrUnit = &DAT_UnitsState::instance.units[1];
-            do {
-                if ((((((_ptrUnit->logicalState != Map::Units::ULS_INVISIBLE) && (_ptrUnit->owner == playerID))
-                          && ((_ptrUnit->isSelectable_OR_matchTime != 0
-                              && ((_ptrUnit->aiUnitBehaviourType == 0 && (_ptrUnit->dying == 0))))))
-                         && (_unitType = _ptrUnit->unitType, _unitType != Map::Units::UT_LORD))
-                        && ((((_unitType != Map::Units::UT_E_ENGINEER && (_unitType != Map::Units::UT_TUNNELER))
-                                 && (_unitType != Map::Units::UT_S_CATAPULT))
-                            && ((
-                                _unitType != Map::Units::UT_S_TREBUCHET && (_unitType != Map::Units::UT_S_MANGONEL))))))
-                    && ((
-                        (_unitType != Map::Units::UT_S_TOWER
-                            && ((_unitType != Map::Units::UT_S_BATTERINGRAM && (_unitType != Map::Units::UT_S_SHIELD))))
-                        && ((_unitType != Map::Units::UT_S_BALLISTA && (_unitType != Map::Units::UT_S_FBALLISTA)))))) {
-                    if (((((int)this->aics[_aiType - 1].DefDiggingUnitMax == 0)
-                             || (_unitType == Map::Units::UT_E_ARCHER))
-                            || (_unitType == Map::Units::UT_E_XBOW))
-                        || (_ptrUnit->someUnitStat4 == 0)) {
-                        MACRO_CALL_MEMBER(AICState_Func::assignUnitToATribe, this)(_unitID);
-                    } else {
-                        MACRO_CALL_MEMBER(AICState_Func::aiAddUnitToMoatDiggerTribe, this)(_unitID);
-                    }
-                }
-                _unitID = _unitID + 1;
-                _ptrUnit = _ptrUnit + 1;
-            } while (_unitID < (int)DAT_UnitsState::instance.maxUnitCount);
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
+            return;
+        int aicIndex = aiType - 1;
+        if (DAT_GameState::instance.playerDataArray[playerID].totalTroopsType0 <= 0)
+            return;
+
+        for (int unitID = 1; unitID < (int)DAT_UnitsState::instance.maxUnitCount; unitID++) {
+            if (DAT_UnitsState::instance.units[unitID].logicalState == ULS_INVISIBLE)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].owner != playerID)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].isSelectable_OR_matchTime == 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].dying != 0)
+                continue;
+
+            if (DAT_UnitsState::instance.units[unitID].unitType == UT_LORD || DAT_UnitsState::instance.units[unitID].unitType == UT_E_ENGINEER || DAT_UnitsState::instance.units[unitID].unitType == UT_TUNNELER || DAT_UnitsState::instance.units[unitID].unitType == UT_S_CATAPULT
+                || DAT_UnitsState::instance.units[unitID].unitType == UT_S_TREBUCHET || DAT_UnitsState::instance.units[unitID].unitType == UT_S_MANGONEL || DAT_UnitsState::instance.units[unitID].unitType == UT_S_TOWER
+                || DAT_UnitsState::instance.units[unitID].unitType == UT_S_BATTERINGRAM || DAT_UnitsState::instance.units[unitID].unitType == UT_S_SHIELD || DAT_UnitsState::instance.units[unitID].unitType == UT_S_BALLISTA
+                || DAT_UnitsState::instance.units[unitID].unitType == UT_S_FBALLISTA)
+                continue;
+
+            if (this->aics[aicIndex].DefDiggingUnitMax == 0 || DAT_UnitsState::instance.units[unitID].unitType == UT_E_ARCHER || DAT_UnitsState::instance.units[unitID].unitType == UT_E_XBOW
+                || DAT_UnitsState::instance.units[unitID].someUnitStat4 == 0)
+                MACRO_CALL_MEMBER(AICState_Func::assignUnitToATribe, this)(unitID);
+            else
+                MACRO_CALL_MEMBER(AICState_Func::aiAddUnitToMoatDiggerTribe, this)(unitID);
         }
     }
 }
