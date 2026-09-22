012735a3dcdd3546a15144d6e1b414fc0dffe497
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:35:33 +0200

    reimplement: SHC_3BB0A8C1_0x004CF920 96.88% Reimplemented

--- src/OpenSHC/AI/AICState/giveSomeRaidCommand.cpp
+++ src/OpenSHC/AI/AICState/giveSomeRaidCommand.cpp
@@ -11,51 +11,32 @@
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::Map::Units::Behavior::UnitStanceEnum;
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CF920
-    undefined4 AICState ::giveSomeRaidCommand(int tribeIndex, int tribeType)
-
+    undefined4 AICState::giveSomeRaidCommand(int tribeIndex, int tribeType)
     {
-        int _tribeOffset = tribeIndex * 0x334;
-        int _attacked = DAT_TribesState::instance.tribes[tribeIndex].owner;
-        int _targetBuildingID = (int)DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID;
+        int owner = DAT_TribesState::instance.tribes[tribeIndex].owner;
+        int targetBuildingID = DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID;
+        if (tribeType == 186 || tribeType == 18 || tribeType == 14 || tribeType == 13)
+            return 0;
 
-        if (tribeType != 0xba && tribeType != 0x12 && tribeType != 0xe && tribeType != 0xd) {
+        if (targetBuildingID != 0
+            && DAT_BuildingsState::instance.buildings[targetBuildingID].uid
+                == DAT_TribesState::instance.tribes[tribeIndex].targetBuildingUID
+            && DAT_BuildingsState::instance.buildings[targetBuildingID].fireDuration == 0)
+            return 1;
 
-            if (_targetBuildingID != 0
-                && DAT_BuildingsState::instance.buildings[_targetBuildingID].uid
-                    == DAT_TribesState::instance.tribes[tribeIndex].targetBuildingUID
-                && DAT_BuildingsState::instance.buildings[_targetBuildingID].fireDuration == 0) {
-                return (undefined4)(1);
-            }
+        DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID = 0;
+        int buildingID = MACRO_CALL_MEMBER(OpenSHC::AI::AICState_Func::chooseRandomBuildingIDFromPlayersArrayOfBuildings,
+            this)(DAT_GameState::instance.playerDataArray[owner].attackedPlayerID);
+        if (buildingID == 0)
+            return 0;
 
-            int _attackedPlayerID = DAT_GameState::instance.playerDataArray[_attacked].attackedPlayerID;
-
-            DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID = 0;
-
-            int _chosenBuildingID = MACRO_CALL_MEMBER(
-                OpenSHC::AI::AICState_Func::chooseRandomBuildingIDFromPlayersArrayOfBuildings, this)(_attackedPlayerID);
-
-            if (_chosenBuildingID != 0) {
-
-                MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::queueDisbandAndAttackCommand4Params,
-                    DAT_UnitsState::ptr)(tribeIndex, 9, (undefined4)((int)(_chosenBuildingID)),
-                    (undefined4)((int)(DAT_BuildingsState::instance.buildings[_chosenBuildingID].uid)));
-
-                // Store UID before targetBuildingID (matching orig order)
-                DAT_TribesState::instance.tribes[tribeIndex].targetBuildingUID
-                    = DAT_BuildingsState::instance.buildings[_chosenBuildingID].uid;
-
-                DAT_TribesState::instance.tribes[tribeIndex].unitStance = OpenSHC::Map::Units::Behavior::USE_DEFENSIVE;
-
-                DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID = (short)_chosenBuildingID;
-
-                return (undefined4)(1);
-            }
-        }
-
-        return (undefined4)(0);
+        MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::queueDisbandAndAttackCommand4Params, DAT_UnitsState::ptr)(
+            tribeIndex, 9, buildingID, DAT_BuildingsState::instance.buildings[buildingID].uid);
+        DAT_TribesState::instance.tribes[tribeIndex].targetBuildingUID = DAT_BuildingsState::instance.buildings[buildingID].uid;
+        DAT_TribesState::instance.tribes[tribeIndex].unitStance = OpenSHC::Map::Units::Behavior::USE_DEFENSIVE;
+        DAT_TribesState::instance.tribes[tribeIndex].targetBuildingID = buildingID;
+        return 1;
     }
 
 }
