eb7abcb08dc073f2928413963188e5592b946ef6
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:57:59 +0200

    reimplement: SHC_3BB0A8C1_0x004CB180 100.00% Reimplemented

--- src/OpenSHC/AI/AICState/aiFarmChoice.cpp
+++ src/OpenSHC/AI/AICState/aiFarmChoice.cpp
@@ -8,15 +8,11 @@ namespace AI {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB180
     int AICState::aiFarmChoice(PlayerID playerID)
-
     {
-        int const aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (aiType == AITA_NULL) {
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
             return 0;
-        }
-
-        /* ugly because EAX is always 1 or higher */
-        return (&this->aics[(aiType - 1)].farm1)[DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex];
+        return (&this->aics[aiType - 1].farm1)[DAT_GameState::instance.playerDataArray[playerID].aiFarmIndex];
     }
 }
 }
