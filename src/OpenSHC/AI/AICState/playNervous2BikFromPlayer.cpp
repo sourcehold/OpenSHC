d6f29536e26ee2a685bd87a3284ebb585186a948
Gynt <Gynt@users.noreply.github.com>
2026-09-22 10:16:31 +0200

    reimplement: SHC_3BB0A8C1_0x004D0B10 68% Reimplemented functionally, but aiType substraction is an issue

--- src/OpenSHC/AI/AICState/playNervous2BikFromPlayer.cpp
+++ src/OpenSHC/AI/AICState/playNervous2BikFromPlayer.cpp
@@ -13,16 +13,15 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D0B10
     void AICState::playNervous2BikFromPlayer(int playerID)
     {
-        int iVar3
-            = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID);
-        int iVar2 = DAT_GameSynchronyState::instance.currentPlayerSlotID;
-        if ((iVar3 != 0) && (DAT_GameState::instance.playerDataArray[playerID].nervousBikCountdown == 0)) {
-            int AVar1 = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        int lord = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID);
+        if ((lord != 0) && (DAT_GameState::instance.playerDataArray[playerID].nervousBikCountdown == 0)) {
+            int aiIndex = (int)DAT_GameState::instance.playerDataArray[playerID].aiType - 1;
+            int curPlayer = DAT_GameSynchronyState::instance.currentPlayerSlotID;
             DAT_GameState::instance.playerDataArray[playerID].nervousBikCountdown = 48;
             if (DAT_GameState::instance.mapAndTime.playerTeams[playerID]
-                != DAT_GameState::instance.mapAndTime.playerTeams[iVar2]) {
+                != DAT_GameState::instance.mapAndTime.playerTeams[curPlayer]) {
                 MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
-                    playerID, AVar1 - 1, 9);
+                    playerID, aiIndex, 9);
             }
         }
     }
