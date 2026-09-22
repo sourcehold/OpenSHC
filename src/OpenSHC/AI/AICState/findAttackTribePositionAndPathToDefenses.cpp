9c0e89caca138b4accf6c4ce706ec81c8a0bb2da
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:55:06 +0200

    reimplement: SHC_3BB0A8C1_0x004CE930 86.96% Register allocation differs

--- src/OpenSHC/AI/AICState/findAttackTribePositionAndPathToDefenses.cpp
+++ src/OpenSHC/AI/AICState/findAttackTribePositionAndPathToDefenses.cpp
@@ -13,56 +13,46 @@ namespace OpenSHC {
 namespace AI {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CE930
-    void AICState ::findAttackTribePositionAndPathToDefenses(int playerID)
+    void AICState::findAttackTribePositionAndPathToDefenses(int playerID)
     {
-        uint local_8 = 0;
-        uint local_4 = 0;
-        int* local_c = &DAT_SkirmishDefinedData::instance.MaxAttackTribes1[0].tribeCount;
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 11; i++) {
+            int targetUnitID = 0;
+            for (int j = 0; j < DAT_SkirmishDefinedData::instance.MaxAttackTribes1[i].tribeCount; j++) {
+                int tribeID = DAT_GameState::instance.playerDataArray[playerID]
+                                  .aiTribeIDs[DAT_SkirmishDefinedData::instance.MaxAttackTribes1[i].tribeType + j];
+                if (tribeID == 0)
+                    continue;
+                if (DAT_TribesState::instance.tribes[tribeID].uid
+                    != DAT_GameState::instance.playerDataArray[playerID]
+                           .aiTribeUIDs[j + DAT_SkirmishDefinedData::instance.MaxAttackTribes1[i].tribeType])
+                    continue;
 
-        bool _found = false;
-        do {
-            int iVar2 = 0;
-            if (0 < *local_c) {
-                short* psVar3
-                    = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs + (*(int (*)[2])(local_c + -1))[0];
-                do {
-                    int iVar1 = (int)*psVar3;
-                    if ((iVar1 != 0)
-                        && (DAT_TribesState::instance.tribes[iVar1].uid
-                            == DAT_GameState::instance.playerDataArray[playerID]
-                                .aiTribeUIDs[iVar2 + (*(int (*)[2])(local_c + -1))[0]])) {
-                        iVar2 = (int)DAT_TribesState::instance.tribes[iVar1].selectionTargetUnitID;
-                        local_8 = (uint)DAT_UnitsState::instance.units[iVar2].x;
-                        local_4 = (uint)DAT_UnitsState::instance.units[iVar2].y;
-                        if (iVar2 != 0) {
-                            _found = true;
-                            break;
-                        }
-                        break;
-                    }
-                    iVar2 = iVar2 + 1;
-                    psVar3 = psVar3 + 1;
-                } while (iVar2 < *local_c);
-            }
-            if (_found)
+                targetUnitID = DAT_TribesState::instance.tribes[tribeID].selectionTargetUnitID;
+                x = DAT_UnitsState::instance.units[targetUnitID].x;
+                y = DAT_UnitsState::instance.units[targetUnitID].y;
                 break;
-            local_c = local_c + 2;
-        } while ((int)local_c <= 0xb42a2b);
+            }
+            if (targetUnitID != 0)
+                break;
+        }
 
         DAT_GameState::instance.playerDataArray[playerID].someTile = 0;
+        if (x == 0)
+            return;
 
-        if (local_8 != 0) {
-            int _tile = MACRO_CALL_MEMBER(
-                OpenSHC::Map::Navigation::PathFindingState_Func::tracePathToFriendlyDefensesAndReturnTileUnk,
-                DAT_PathFindingState::ptr)(playerID, local_8, local_4);
-            if (0 < _tile) {
-                short _y = DAT_ViewportRenderState::instance.tileTranslationMatrix_YComponent[_tile];
-                DAT_GameState::instance.playerDataArray[playerID].someTile = _tile;
-                int iVar2 = DAT_ViewportRenderState::instance.translationMatrix[_y].addXgetTile;
-                DAT_GameState::instance.playerDataArray[playerID].someY = (int)_y;
-                DAT_GameState::instance.playerDataArray[playerID].someX = _tile - iVar2;
-            }
-        }
+        int tile = MACRO_CALL_MEMBER(
+            OpenSHC::Map::Navigation::PathFindingState_Func::tracePathToFriendlyDefensesAndReturnTileUnk,
+            DAT_PathFindingState::ptr)(playerID, x, y);
+        if (tile <= 0)
+            return;
+
+        short tileY = DAT_ViewportRenderState::instance.tileTranslationMatrix_YComponent[tile];
+        DAT_GameState::instance.playerDataArray[playerID].someTile = tile;
+        DAT_GameState::instance.playerDataArray[playerID].someY = tileY;
+        DAT_GameState::instance.playerDataArray[playerID].someX
+            = tile - DAT_ViewportRenderState::instance.translationMatrix[tileY].addXgetTile;
     }
 }
 }
