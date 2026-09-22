e6fecea1ebf9ee893b916923d835cafd3e9b7137
Gynt <Gynt@users.noreply.github.com>
2026-09-22 22:36:22 +0200

    reimplement: SHC_3BB0A8C1_0x0046B2F0 100.00% Reimplemented

---
+++ src/OpenSHC/Game/GameCore/setViewOnExitUnk.cpp
@@ -0,0 +1,13 @@
+#include "../GameCore.func.hpp"
+
+namespace OpenSHC {
+namespace Game {
+
+    // FUNCTION: STRONGHOLDCRUSADER 0x0046B2F0
+    void GameCore::setViewOnExitUnk()
+    {
+        this->currentMenuViewType = 2;
+    }
+
+}
+}
