c434c4b9405ad299227cf7fcab0343468337bbac
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:44:40 +0200

    reimplement: SHC_3BB0A8C1_0x004CB540 97.50% Reimplemented

--- src/OpenSHC/AI/AICState/aiShouldBuildPitchrig.cpp
+++ src/OpenSHC/AI/AICState/aiShouldBuildPitchrig.cpp
@@ -10,27 +10,23 @@ namespace OpenSHC {
 namespace AI {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB540
-    uint AICState::aiShouldBuildPitchrig(PlayerID param_1)
+    uint AICState::aiShouldBuildPitchrig(PlayerID playerID)
     {
-        int const aiType = DAT_GameState::instance.playerDataArray[param_1].aiType;
-        if (aiType == AITA_NULL) {
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
             return 0;
-        }
-        AICSpecification const* spec = &this->aics[aiType - 1];
-        if ((int)spec->populationPerPitchrig <= 0) {
+        int aicIndex = aiType - 1;
+        if (this->aics[aicIndex].populationPerPitchrig <= 0)
             return 0;
-        }
-        int iVar3 = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer,
-            DAT_BuildingsState::ptr)(param_1, Map::Buildings::BT_PITCHRIG, 1);
-        if (iVar3 < 1) {
-            iVar3 = 1;
-        } else {
-            if (iVar3 >= (int)spec->maxPitchrigs) {
-                return 0;
-            }
-        }
-        return DAT_GameState::instance.playerDataArray[param_1].currentPopulation / iVar3
-            >= (int)spec->populationPerPitchrig;
+
+        int count = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer, DAT_BuildingsState::ptr)(
+            playerID, Map::Buildings::BT_PITCHRIG, 1);
+        if (count <= 0)
+            count = 1;
+        else if (count >= this->aics[aicIndex].maxPitchrigs)
+            return 0;
+        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / count
+            >= this->aics[aicIndex].populationPerPitchrig;
     }
 }
 }
