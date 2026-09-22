01be9ea0f2d29a4996993a427e06cf5e8ec3b294
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:44:40 +0200

    reimplement: SHC_3BB0A8C1_0x004CB4D0 97.50% Reimplemented

--- src/OpenSHC/AI/AICState/aiShouldBuildIronMine.cpp
+++ src/OpenSHC/AI/AICState/aiShouldBuildIronMine.cpp
@@ -12,23 +12,21 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB4D0
     uint AICState::aiShouldBuildIronMine(PlayerID playerID)
     {
-        int const aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (aiType == AITA_NULL) {
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
             return 0;
-        }
-        AICSpecification const* spec = &this->aics[aiType - 1];
-        if ((int)spec->populationPerIronmine <= 0) {
+        int aicIndex = aiType - 1;
+        if (this->aics[aicIndex].populationPerIronmine <= 0)
             return 0;
-        }
-        int iVar3 = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer,
-            DAT_BuildingsState::ptr)(playerID, Map::Buildings::BT_IRONMINE, 1);
-        if (iVar3 < 1) {
-            iVar3 = 1;
-        } else if (iVar3 >= (int)spec->maxIronmines) {
+
+        int count = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer, DAT_BuildingsState::ptr)(
+            playerID, Map::Buildings::BT_IRONMINE, 1);
+        if (count <= 0)
+            count = 1;
+        else if (count >= this->aics[aicIndex].maxIronmines)
             return 0;
-        }
-        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / iVar3
-            >= (int)spec->populationPerIronmine;
+        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / count
+            >= this->aics[aicIndex].populationPerIronmine;
     }
 }
 }
