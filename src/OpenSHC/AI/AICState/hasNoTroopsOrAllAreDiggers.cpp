21d7e03ee29dcc21568c9aaa08b53cb6bc8ef13f
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:12:32 +0200

    reimplement: SHC_3BB0A8C1_0x004CFFD0 100% Reimplemented

--- src/OpenSHC/AI/AICState/hasNoTroopsOrAllAreDiggers.cpp
+++ src/OpenSHC/AI/AICState/hasNoTroopsOrAllAreDiggers.cpp
@@ -8,51 +8,36 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CFFD0
     BOOLEnum AICState::hasNoTroopsOrAllAreDiggers(int playerID)
     {
-        int _totalMisc = DAT_GameState::instance.playerDataArray[playerID].totalAssassinTroops
+        int totalTroops = DAT_GameState::instance.playerDataArray[playerID].totalAssassinTroops
             + DAT_GameState::instance.playerDataArray[playerID].totalUnit2Troops
             + DAT_GameState::instance.playerDataArray[playerID].totalUnitPatrolTroops;
-        int _halfOfDiggingTroops = 0;
-        int _totalTroops;
+        int halfOfDiggingTroops = 0;
 
         if (DAT_GameState::instance.playerDataArray[playerID].aiPlayerState == 6) {
-            _totalMisc += DAT_GameState::instance.playerDataArray[playerID].totalDiggingUnitTroops;
-            int _attackTicker = DAT_GameState::instance.playerDataArray[playerID].attackTicker;
-            int _backup;
-            if (_attackTicker > 0x10) {
-                int _bp = DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
-                _backup = (_bp + (_bp >> 0x1f & 7)) >> 3;
-            } else {
-                _backup = DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
-            }
-            _totalMisc += _backup;
-            _totalTroops = _totalMisc
-                + DAT_GameState::instance.playerDataArray[playerID].totalUnitEngageTroops
+            totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalDiggingUnitTroops;
+            if (DAT_GameState::instance.playerDataArray[playerID].attackTicker > 16)
+                totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops / 8;
+            else
+                totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
+            totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitEngageTroops
                 + DAT_GameState::instance.playerDataArray[playerID].totalUnitSiegeDefTroops
                 + DAT_GameState::instance.playerDataArray[playerID].totalMaxDefaultTroops;
         } else {
-            int _digging = DAT_GameState::instance.playerDataArray[playerID].totalDiggingUnitTroops;
-            _halfOfDiggingTroops = _digging / 2;
-            int _attackTicker = DAT_GameState::instance.playerDataArray[playerID].attackTicker;
-            int _backup;
-            if (_attackTicker > 0xc) {
-                int _bp2 = DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
-                _backup = (_bp2 + (_bp2 >> 0x1f & 7)) >> 3;
-            } else {
-                _backup = DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
-            }
-            _totalMisc += _backup;
-            _totalTroops = _totalMisc
-                + DAT_GameState::instance.playerDataArray[playerID].totalUnitEngageTroops
+            halfOfDiggingTroops = DAT_GameState::instance.playerDataArray[playerID].totalDiggingUnitTroops / 2;
+            if (DAT_GameState::instance.playerDataArray[playerID].attackTicker > 12)
+                totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops / 8;
+            else
+                totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitBackupTroops;
+            totalTroops += DAT_GameState::instance.playerDataArray[playerID].totalUnitEngageTroops
                 + DAT_GameState::instance.playerDataArray[playerID].totalUnitSiegeDefTroops
                 + DAT_GameState::instance.playerDataArray[playerID].totalMaxDefaultTroops;
         }
 
-        if (1 < _totalTroops) {
-            if (4 < _totalTroops || _halfOfDiggingTroops <= _totalTroops + _totalTroops) {
-                return FALSE;
-            }
-        }
-        return TRUE;
+        if (totalTroops <= 1)
+            return TRUE;
+        if (totalTroops <= 4 && halfOfDiggingTroops > totalTroops * 2)
+            return TRUE;
+        return FALSE;
     }
 }
 }
