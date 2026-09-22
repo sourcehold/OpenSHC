3fa8fb68fabd53cf7fd9a6b7ac02d301425367a5
Gynt <Gynt@users.noreply.github.com>
2026-09-22 10:32:02 +0200

    reimplement: SHC_3BB0A8C1_0x004D0F90 100% Reimplemented

--- src/OpenSHC/AI/AICState/playJokeBikFromAIToHuman.cpp
+++ src/OpenSHC/AI/AICState/playJokeBikFromAIToHuman.cpp
@@ -11,12 +11,12 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D0F90
     void AICState::playJokeBikFromAIToHuman(int playerID, int aiType)
     {
-        int iVar1
-            = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID);
-        if (iVar1 != 0) {
-            MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
-                playerID, aiType, 0x14);
+        if (MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID) == 0) {
+            return;
         }
+
+        MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
+            playerID, aiType, 20);
     }
 }
 }
