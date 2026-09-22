a2fe0aa457bf8e685780efaf36afe8f84ce63e58
Gynt <Gynt@users.noreply.github.com>
2026-09-22 09:05:56 +0200

    reimplement: SHC_3BB0A8C1_0x004CE040 100%

--- src/OpenSHC/AI/AICState/recomputeAIPathCostToAttackedKeep.cpp
+++ src/OpenSHC/AI/AICState/recomputeAIPathCostToAttackedKeep.cpp
@@ -16,78 +16,45 @@ namespace AI {
 
     using OpenSHC::WindowsHelper::Enums::BOOLEnum;
 
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CE040
-    void AICState ::recomputeAIPathCostToAttackedKeep(int playerID, int useBorderDistance)
-
+    void AICState::recomputeAIPathCostToAttackedKeep(int playerID, int useBorderDistance)
     {
+        int attackedPlayerID = DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID;
+        int keepArea = (short)DAT_TileMapState::instance
+                           .PathConnectionLayer[DAT_GameState::instance.playerDataArray[playerID].campground.tileEntry];
 
-        BOOLEnum BVar1;
-
-        uint _borderDistance;
-
-        bool _cannotNavigateToKeep;
-
-        int _attackedPlayerID;
-
-        int _bd;
-
-        ushort _keepArea;
-
-        _attackedPlayerID = DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID;
-
-        _keepArea = DAT_TileMapState::instance
-                        .PathConnectionLayer[DAT_GameState::instance.playerDataArray[playerID].campground.tileEntry];
-
-        _bd = DAT_GameState::instance.playerDataArray[_attackedPlayerID].unknownPathBorderDistanceRelated;
-
-        if (_bd < 120) {
-
-            _borderDistance = ((_bd < 90) - 1 & 20) + 50;
-
+        int borderDistance;
+        if (DAT_GameState::instance.playerDataArray[attackedPlayerID].unknownPathBorderDistanceRelated > 120) {
+            borderDistance = 98;
         }
-
         else {
-
-            _borderDistance = 98;
+            borderDistance
+                = DAT_GameState::instance.playerDataArray[attackedPlayerID].unknownPathBorderDistanceRelated > 90 ? 70
+                                                                                                                   : 50;
         }
-
         if (useBorderDistance == 0) {
-
-            _borderDistance = 0;
+            borderDistance = 0;
         }
 
-        *(int*)((int)DAT_TroopValueState::instance.attackInfo.hackValuesArray + playerID * 0x177bc + -0x10)
-            = _attackedPlayerID;
+        DAT_TroopValueState::instance.attackInfo.playerInfo[playerID - 1].currentPlayerID_OR_attackedPlayerID_OR_one
+            = attackedPlayerID;
 
         MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
-            80400, '\0', (void*)((int)(DAT_TileMapState::instance.AIZoneLayer)));
+            80400, '\0', DAT_TileMapState::instance.AIZoneLayer);
 
-        BVar1 = MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::canNavigateFromKeepToKeep,
-            DAT_GameState::ptr)(playerID, DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID);
-
-        _cannotNavigateToKeep = BVar1 == FALSE;
-
-        if (_cannotNavigateToKeep) {
-
-            _attackedPlayerID = DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID;
-
+        if (MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::canNavigateFromKeepToKeep, DAT_GameState::ptr)(
+                playerID, DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID)
+            != FALSE) {
+            MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::computeAIZoneLayer,
+                DAT_PathFindingState::ptr)(DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID, 1, 1);
         }
-
         else {
-
-            _attackedPlayerID = DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID;
+            MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::computeAIZoneLayer,
+                DAT_PathFindingState::ptr)(DAT_GameState::instance.playerDataArray[playerID].attackedPlayerID, 0, 1);
         }
-
-        MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::computeAIZoneLayer,
-            DAT_PathFindingState::ptr)(_attackedPlayerID, (int)((int)((uint)!_cannotNavigateToKeep)));
 
         MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::updateWalkLayerAndAIPathCostLayer,
-            DAT_PathFindingState::ptr)(110, _borderDistance, (dword)((int)((int)(short)_keepArea)), playerID);
-
-        return;
+            DAT_PathFindingState::ptr)(110, borderDistance, keepArea, playerID);
     }
 
 }
