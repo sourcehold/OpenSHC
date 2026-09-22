d7f17660eb14f706eba51b94fff966ffb6e29c1f
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:34:22 +0200

    reimplement: SHC_3BB0A8C1_0x004CDA50 100.00% Reimplemented

--- src/OpenSHC/AI/AICState/addBuildingToTargetableBuildings.cpp
+++ src/OpenSHC/AI/AICState/addBuildingToTargetableBuildings.cpp
@@ -1,5 +1,7 @@
 #include "../AICState.func.hpp"
 
+#include "OpenSHC/Game/GameMode.hpp"
+
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 #include "OpenSHC/Globals/DAT_GameSynchronyState.hpp"
@@ -10,14 +12,18 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CDA50
     void AICState::addBuildingToTargetableBuildings(int buildingID)
     {
-        int _player = DAT_BuildingsState::instance.buildings[buildingID].owner;
-        if (DAT_GameSynchronyState::instance.DAT_CurrentGameMode != Game::GM_SOLITARY) {
-            int* piVar1 = &DAT_GameState::instance.playerDataArray[_player].top100TargetableBuildingsTracker;
-            if ((*piVar1 < 100) && (DAT_BuildingsState::instance.buildings[buildingID].fireDuration == 0)) {
-                DAT_GameState::instance.playerDataArray[_player].top100TargetableBuildings[*piVar1] = (short)buildingID;
-                *piVar1 = *piVar1 + 1;
-            }
-        }
+        int owner = DAT_BuildingsState::instance.buildings[buildingID].owner;
+        if (DAT_GameSynchronyState::instance.currentGameMode == Game::GM_SOLITARY)
+            return;
+        if (DAT_GameState::instance.playerDataArray[owner].top100TargetableBuildingsTracker >= 100)
+            return;
+        if (DAT_BuildingsState::instance.buildings[buildingID].fireDuration != 0)
+            return;
+
+        DAT_GameState::instance.playerDataArray[owner]
+            .top100TargetableBuildings[DAT_GameState::instance.playerDataArray[owner].top100TargetableBuildingsTracker]
+            = buildingID;
+        DAT_GameState::instance.playerDataArray[owner].top100TargetableBuildingsTracker++;
     }
 
 }
