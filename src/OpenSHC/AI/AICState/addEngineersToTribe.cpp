f28b0157c984e4eb83d4c9f3ea52729a552d2028
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:34:22 +0200

    reimplement: SHC_3BB0A8C1_0x004D4040 96.97% Reimplemented

--- src/OpenSHC/AI/AICState/addEngineersToTribe.cpp
+++ src/OpenSHC/AI/AICState/addEngineersToTribe.cpp
@@ -1,6 +1,7 @@
 #include "../AICState.func.hpp"
 
 #include "OpenSHC/Map/Units/UnitLogicState.hpp"
+#include "OpenSHC/Map/Units/UnitType.hpp"
 
 #include "OpenSHC/Globals/DAT_UnitsState.hpp"
 
@@ -10,21 +11,21 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D4040
     void AICState::addEngineersToTribe(int playerID)
     {
-        int _unitID = 1;
-        if (1 < (int)DAT_UnitsState::instance.maxUnitCount) {
-            Map::Units::Unit* _pUnit = &DAT_UnitsState::instance.units[1];
-            do {
-                /* if logicalState == normal && owner == playerID && unitType == 0x1e (engineer)
-                   && aiUnitBehaviourType == 10 */
-                if ((((_pUnit->logicalState == Map::Units::ULS_NORMAL) && (_pUnit->owner == playerID))
-                        && (_pUnit->dying == 0))
-                    && (((_pUnit->unitType == Map::Units::UT_E_ENGINEER) && (_pUnit->aiUnitBehaviourType == 10))
-                        && (_pUnit->tribeID == 0))) {
-                    MACRO_CALL_MEMBER(AICState_Func::addUnitToItsTribe, this)(_unitID, 10);
-                }
-                _unitID = _unitID + 1;
-                _pUnit = _pUnit + 1;
-            } while (_unitID < (int)DAT_UnitsState::instance.maxUnitCount);
+        for (int unitID = 1; unitID < (int)DAT_UnitsState::instance.maxUnitCount; unitID++) {
+            if (DAT_UnitsState::instance.units[unitID].logicalState != Map::Units::ULS_NORMAL)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].owner != playerID)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].dying != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].unitType != Map::Units::UT_E_ENGINEER)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType != 10)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].tribeID != 0)
+                continue;
+
+            MACRO_CALL_MEMBER(AICState_Func::addUnitToItsTribe, this)(unitID, 10);
         }
     }
 
