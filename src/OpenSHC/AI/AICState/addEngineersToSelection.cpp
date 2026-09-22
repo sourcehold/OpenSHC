0890d4ad97e2af6e6145e720bea9d8c1cbc16684
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:34:22 +0200

    reimplement: SHC_3BB0A8C1_0x004CC680 83.33% Register push placement diff

--- src/OpenSHC/AI/AICState/addEngineersToSelection.cpp
+++ src/OpenSHC/AI/AICState/addEngineersToSelection.cpp
@@ -3,6 +3,7 @@
 #include "OpenSHC/Map/Units/TribesState.func.hpp"
 #include "OpenSHC/Map/Units/States/UnitState.hpp"
 #include "OpenSHC/Map/Units/UnitLogicState.hpp"
+#include "OpenSHC/Map/Units/UnitType.hpp"
 
 #include "OpenSHC/Globals/DAT_TribesState.hpp"
 #include "OpenSHC/Globals/DAT_UnitsState.hpp"
@@ -13,30 +14,27 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CC680
     int AICState::addEngineersToSelection(int playerID, int unitCount)
     {
-        int tribeID;
-        uint unitID;
+        int tribeID = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
+        for (int unitID = 1; unitID < (int)DAT_UnitsState::instance.maxUnitCount; unitID++) {
+            if (unitCount <= 0)
+                return tribeID;
+            if (DAT_UnitsState::instance.units[unitID].logicalState != Map::Units::ULS_NORMAL)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].owner != playerID)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].dying != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].unitType != Map::Units::UT_E_ENGINEER)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType != 10)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].tribeID != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].state.generic == Map::Units::States::US_STAND_UPUnk)
+                continue;
 
-        tribeID = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
-        unitID = 1;
-        if (1 < (int)DAT_UnitsState::instance.maxUnitCount) {
-            /* Unit offset 150 */
-            Map::Units::Unit* _ptrUnit = &DAT_UnitsState::instance.units[1];
-            do {
-                if (unitCount <= 0) {
-                    return tribeID;
-                }
-                if ((((_ptrUnit->logicalState == Map::Units::ULS_NORMAL) && (_ptrUnit->owner == playerID))
-                        && (_ptrUnit->dying == 0))
-                    && (((_ptrUnit->unitType == Map::Units::UT_E_ENGINEER) && (_ptrUnit->aiUnitBehaviourType == 10))
-                        && ((_ptrUnit->tribeID == 0)
-                            && ((_ptrUnit->state).generic != Map::Units::States::US_STAND_UPUnk)))) {
-                    MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
-                        unitID, tribeID);
-                    unitCount = unitCount + -1;
-                }
-                unitID = unitID + 1;
-                _ptrUnit = _ptrUnit + 1;
-            } while ((int)unitID < (int)DAT_UnitsState::instance.maxUnitCount);
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, tribeID);
+            unitCount--;
         }
         return tribeID;
     }
