2fe05a4ef34645c62d10999cd358e8021355ffaa
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:12:49 +0200

    reimplement: SHC_3BB0A8C1_0x004D2830 100% Reimplemented

--- src/OpenSHC/AI/AICState/ifHasTribe0SendUnitsToKeep.cpp
+++ src/OpenSHC/AI/AICState/ifHasTribe0SendUnitsToKeep.cpp
@@ -9,12 +9,13 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D2830
     void AICState::ifHasTribe0SendUnitsToKeep(int playerID)
     {
-        int tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0];
-        if ((0 < tribeID)
-            && (DAT_TribesState::instance.tribes[tribeID].uid
-                == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0])) {
-            MACRO_CALL_MEMBER(AICState_Func::sendUnitsToKeep, this)(tribeID, playerID);
-        }
+        int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0];
+        if (tribeID <= 0)
+            return;
+        if (DAT_TribesState::instance.tribes[tribeID].uid != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0])
+            return;
+
+        MACRO_CALL_MEMBER(AICState_Func::sendUnitsToKeep, this)(tribeID, playerID);
     }
 }
 }
