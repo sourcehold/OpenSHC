0b1598b594efb4f63ddfa0f66a0b9317cb290301
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:58:00 +0200

    reimplement: SHC_3BB0A8C1_0x004CB290 98.11% Reimplemented

--- src/OpenSHC/AI/AICState/aiShouldBuildWoodcutter.cpp
+++ src/OpenSHC/AI/AICState/aiShouldBuildWoodcutter.cpp
@@ -13,35 +13,29 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB290
     BOOLEnum AICState::aiShouldBuildWoodcutter(int playerID)
     {
-        if (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 0) {
+        if (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 0)
             return FALSE;
-        }
         int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (aiType == AITA_NULL) {
+        if (aiType == AITA_NULL)
             return FALSE;
-        }
-        aiType -= 1;
-        // AICSpecification const* spec = &this->aics[aiType];
-        if ((int)this->aics[aiType].populationPerWoodcutter <= 0) {
+        int aicIndex = aiType - 1;
+        if (this->aics[aicIndex].populationPerWoodcutter <= 0)
             return FALSE;
-        }
-        int iVar3 = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer,
-            DAT_BuildingsState::ptr)(playerID, Map::Buildings::BT_WOODCUTTERSHUT, 1);
-        if (iVar3 <= 0) {
-            iVar3 = 1;
+
+        int count = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countBuildingsForPlayer, DAT_BuildingsState::ptr)(
+            playerID, Map::Buildings::BT_WOODCUTTERSHUT, 1);
+        if (count <= 0) {
+            count = 1;
         } else {
-            if (iVar3 >= (int)this->aics[aiType].maxWoodcutters) {
+            if (count >= this->aics[aicIndex].maxWoodcutters)
                 return FALSE;
-            }
-            if ((6 <= iVar3) && (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 90)) {
+            if (count >= 6 && DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 90)
                 return FALSE;
-            }
-            if ((3 <= iVar3) && (DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 30)) {
+            if (count >= 3 && DAT_AIVState::instance.mapExtraInfo.totalWoodAvailable <= 30)
                 return FALSE;
-            }
         }
-        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / iVar3
-            >= (int)this->aics[aiType].populationPerWoodcutter;
+        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / count
+            >= this->aics[aicIndex].populationPerWoodcutter;
     }
 }
 }
