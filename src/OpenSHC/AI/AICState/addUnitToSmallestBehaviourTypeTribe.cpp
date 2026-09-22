1b124776aeeaf6f2527c7a06a839b3bff4cba438
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:00:06 +0200

    reimplement: SHC_3BB0A8C1_0x004CCD20 63.91% Loop register allocation diff

--- src/OpenSHC/AI/AICState/addUnitToSmallestBehaviourTypeTribe.cpp
+++ src/OpenSHC/AI/AICState/addUnitToSmallestBehaviourTypeTribe.cpp
@@ -11,81 +11,69 @@ namespace AI {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004CCD20
     int AICState::addUnitToSmallestBehaviourTypeTribe(
-        undefined4 playerID, undefined4 param_2, undefined4 aiUnitBehaviourType)
+        undefined4 playerID, undefined4 unitID, undefined4 aiUnitBehaviourType)
     {
-        int _playerID = (int)playerID;
-        int AVar1 = DAT_GameState::instance.playerDataArray[_playerID].aiType;
-        if (AVar1 == AITA_NULL) {
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
             return 0;
-        }
-        int _tribeTypeOffset
-            = DAT_SkirmishDefinedData::instance.AIUnitBehaviourTypeMapping[(int)aiUnitBehaviourType + -10];
-        int _smallestTribe = 0;
-        int _smallestTribeOffset = 0;
-        short _smallestTribeSize = 1000;
-        dword _groupsCount;
 
-        if (_tribeTypeOffset == 0xf) {
-            _groupsCount = this->aics[AVar1 - 1].AttUnitPatrolGroupsCount;
-        } else if (_tribeTypeOffset == 0xba) {
-            _groupsCount = this->aics[AVar1 - 1].AttUnitBackupGroupsCount;
-        } else if (_tribeTypeOffset == 0xbe) {
-            _groupsCount = this->aics[AVar1 - 1].AttUnitSiegeDefGroupsCount;
-        } else if (_tribeTypeOffset == 0xc0) {
-            _groupsCount = this->aics[AVar1 - 1].AttMainGroupsCount;
-        } else {
-            _groupsCount = 1;
+        int aicIndex = aiType - 1;
+        int tribeType = DAT_SkirmishDefinedData::instance.AIUnitBehaviourTypeMapping[aiUnitBehaviourType - 10];
+        int groupsCount;
+        switch (tribeType) {
+        case 15:
+            groupsCount = this->aics[aicIndex].AttUnitPatrolGroupsCount;
+            break;
+        case 186:
+            groupsCount = this->aics[aicIndex].AttUnitBackupGroupsCount;
+            break;
+        case 190:
+            groupsCount = this->aics[aicIndex].AttUnitSiegeDefGroupsCount;
+            break;
+        case 192:
+            groupsCount = this->aics[aicIndex].AttMainGroupsCount;
+            break;
+        default:
+            groupsCount = 1;
+            break;
         }
 
-        int _selectedTribe;
-        int _selectedSlot;
-        if ((int)_groupsCount < 1 || _groupsCount == 1) {
-            int _tribeID_groupsCount1
-                = (int)DAT_GameState::instance.playerDataArray[_playerID].aiTribeIDs[_tribeTypeOffset];
-            if ((_tribeID_groupsCount1 != 0)
-                && (DAT_TribesState::instance.tribes[_tribeID_groupsCount1].uid
-                    == DAT_GameState::instance.playerDataArray[_playerID].aiTribeUIDs[_tribeTypeOffset])) {
-                return _tribeID_groupsCount1;
-            }
-            _selectedTribe = MACRO_CALL_MEMBER(
-                Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(_playerID);
-            _selectedSlot = _tribeTypeOffset;
+        int selectedTribeID = 0;
+        int selectedSlot = 0;
+        if (groupsCount <= 1) {
+            int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[tribeType];
+            if (tribeID != 0
+                && DAT_TribesState::instance.tribes[tribeID].uid
+                    == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[tribeType])
+                return tribeID;
+            selectedTribeID
+                = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
+            selectedSlot = tribeType;
         } else {
-            int _maxOffset = _groupsCount + _tribeTypeOffset;
-            _selectedTribe = 0;
-            _selectedSlot = 0;
-            short* _pTribeIDs = DAT_GameState::instance.playerDataArray[_playerID].aiTribeIDs + _tribeTypeOffset;
-            if (_tribeTypeOffset < _maxOffset) {
-                do {
-                    int _tribeID_2 = (int)*_pTribeIDs;
-                    if ((_tribeID_2 == 0)
-                        || (DAT_TribesState::instance.tribes[_tribeID_2].uid
-                            != DAT_GameState::instance.playerDataArray[_playerID].aiTribeUIDs[_tribeTypeOffset])) {
-                        _selectedTribe = MACRO_CALL_MEMBER(
-                            Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(_playerID);
-                        _selectedSlot = _tribeTypeOffset;
-                        break;
-                    }
-                    short _tribeSize = DAT_TribesState::instance.tribes[_tribeID_2].size;
-                    if (_tribeSize < _smallestTribeSize) {
-                        _smallestTribeOffset = _tribeTypeOffset;
-                        _smallestTribe = _tribeID_2;
-                        _smallestTribeSize = _tribeSize;
-                    }
-                    _pTribeIDs = _pTribeIDs + 1;
-                    _tribeTypeOffset = _tribeTypeOffset + 1;
-                    _selectedTribe = _smallestTribe;
-                    _selectedSlot = _smallestTribeOffset;
-                } while (_tribeTypeOffset < _maxOffset);
+            int smallestSize = 1000;
+            for (int slot = tribeType; slot < tribeType + groupsCount; slot++) {
+                int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[slot];
+                if (tribeID == 0
+                    || DAT_TribesState::instance.tribes[tribeID].uid
+                        != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[slot]) {
+                    selectedTribeID = MACRO_CALL_MEMBER(
+                        Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
+                    selectedSlot = slot;
+                    break;
+                }
+                if (DAT_TribesState::instance.tribes[tribeID].size < smallestSize) {
+                    smallestSize = DAT_TribesState::instance.tribes[tribeID].size;
+                    selectedTribeID = tribeID;
+                    selectedSlot = slot;
+                }
             }
         }
-        if (0 < _selectedTribe) {
-            DAT_GameState::instance.playerDataArray[_playerID].aiTribeIDs[_selectedSlot] = (short)_selectedTribe;
-            DAT_GameState::instance.playerDataArray[_playerID].aiTribeUIDs[_selectedSlot]
-                = DAT_TribesState::instance.tribes[_selectedTribe].uid;
-            return _selectedTribe;
-        }
-        return 0;
+        if (selectedTribeID <= 0)
+            return 0;
+        DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[selectedSlot] = selectedTribeID;
+        DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[selectedSlot]
+            = DAT_TribesState::instance.tribes[selectedTribeID].uid;
+        return selectedTribeID;
     }
 }
 }
