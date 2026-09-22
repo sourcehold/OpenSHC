a8623042e08f0c4c316a086c404332e183b36c2f
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:39:55 +0200

    reimplement: SHC_3BB0A8C1_0x004CC520 93.75% Reimplemented

--- src/OpenSHC/AI/AICState/aiRecruitEngineers.cpp
+++ src/OpenSHC/AI/AICState/aiRecruitEngineers.cpp
@@ -14,20 +14,26 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CC520
     void AICState::aiRecruitEngineers(int playerID)
     {
-        int recruitmentBuilding;
-        int _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if ((((_aiType != AITA_NULL) && (DAT_GameState::instance.playerDataArray[playerID].isEngineerRequired != FALSE))
-                && (DAT_GameState::instance.playerDataArray[playerID].canStartSpending != 0))
-            && (((0x1d < (int)this->aics[_aiType - 1].recruitGoldThreshold
-                             + DAT_GameState::instance.playerDataArray[playerID].currentResources[0xf]
-                     && (recruitmentBuilding = DAT_GameState::instance.playerDataArray[playerID].engineersGuild.id,
-                         0 < recruitmentBuilding))
-                && (DAT_BuildingsState::instance.buildings[recruitmentBuilding].buildingType
-                    == Map::Buildings::BT_ENGINEERSGUILD)))) {
-            MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::nonEuroRecruit, DAT_UnitsState::ptr)(
-                Map::Units::UT_E_ENGINEER, recruitmentBuilding, playerID, 0);
-            DAT_GameState::instance.playerDataArray[playerID].isEngineerRequired = FALSE;
-        }
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
+            return;
+        if (DAT_GameState::instance.playerDataArray[playerID].isEngineerRequired == FALSE)
+            return;
+        if (DAT_GameState::instance.playerDataArray[playerID].canStartSpending == 0)
+            return;
+        if (this->aics[aiType - 1].recruitGoldThreshold
+                + DAT_GameState::instance.playerDataArray[playerID].currentResources[Game::Resources::RT_GOLD]
+            < 30)
+            return;
+        int guildID = DAT_GameState::instance.playerDataArray[playerID].engineersGuild.id;
+        if (guildID <= 0)
+            return;
+        if (DAT_BuildingsState::instance.buildings[guildID].buildingType != Map::Buildings::BT_ENGINEERSGUILD)
+            return;
+
+        MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::nonEuroRecruit, DAT_UnitsState::ptr)(
+            Map::Units::UT_E_ENGINEER, guildID, playerID, 0);
+        DAT_GameState::instance.playerDataArray[playerID].isEngineerRequired = FALSE;
     }
 }
 }
