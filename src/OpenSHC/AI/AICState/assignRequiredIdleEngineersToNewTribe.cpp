201352c7877d09074c0c8d039a5f008ff29507df
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:44:41 +0200

    reimplement: SHC_3BB0A8C1_0x004CC5A0 85.71% Register allocation differs

--- src/OpenSHC/AI/AICState/assignRequiredIdleEngineersToNewTribe.cpp
+++ src/OpenSHC/AI/AICState/assignRequiredIdleEngineersToNewTribe.cpp
@@ -4,7 +4,6 @@
 #include "OpenSHC/Map/Units/UnitLogicState.hpp"
 #include "OpenSHC/Map/Units/UnitType.hpp"
 
-#include "OpenSHC/Globals/DAT_GameState.hpp"
 #include "OpenSHC/Globals/DAT_TribesState.hpp"
 #include "OpenSHC/Globals/DAT_UnitsState.hpp"
 
@@ -14,33 +13,34 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CC5A0
     int AICState::assignRequiredIdleEngineersToNewTribe(int playerID, int count)
     {
-        Map::Units::States::UnitStateShort UVar1;
-        int tribeID
-            = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
-        uint unitID = 1;
-        if (1 < (int)DAT_UnitsState::instance.maxUnitCount) {
-            Map::Units::Unit* psVar2 = &DAT_UnitsState::instance.units[1];
-            do {
-                if (count < 1) {
-                    return tribeID;
-                }
-                if (((((psVar2->logicalState == Map::Units::ULS_NORMAL) && (psVar2->owner == playerID))
-                         && (psVar2->dying == 0))
-                        && ((psVar2->unitType == Map::Units::UT_E_ENGINEER && (psVar2->aiUnitBehaviourType != 0x16))))
-                    && ((psVar2->aiUnitBehaviourType != 10 && (psVar2->engineerRelatedUnk == 0)))
-                    && (((UVar1 = (psVar2->state).generic, UVar1 == 0x69 || (UVar1 == 2))
-                        || (UVar1 == Map::Units::States::US_IDLEUnk)))) {
-                    if (psVar2->tribeID != 0) {
-                        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::removeUnitFromTribe, DAT_TribesState::ptr)(
-                            unitID, (int)psVar2->tribeID);
-                    }
-                    MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
-                        unitID, tribeID);
-                    count = count + -1;
-                }
-                unitID = unitID + 1;
-                psVar2 = psVar2 + 1;
-            } while ((int)unitID < (int)DAT_UnitsState::instance.maxUnitCount);
+        int tribeID = MACRO_CALL_MEMBER(Map::Units::TribesState_Func::createTribeForPlayer, DAT_TribesState::ptr)(playerID);
+        for (int unitID = 1; unitID < (int)DAT_UnitsState::instance.maxUnitCount; unitID++) {
+            if (count <= 0)
+                return tribeID;
+            if (DAT_UnitsState::instance.units[unitID].logicalState != Map::Units::ULS_NORMAL)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].owner != playerID)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].dying != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].unitType != Map::Units::UT_E_ENGINEER)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType == 22)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType == 10)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].engineerRelatedUnk != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].state.generic != 105
+                && DAT_UnitsState::instance.units[unitID].state.generic != 2
+                && DAT_UnitsState::instance.units[unitID].state.generic != Map::Units::States::US_IDLEUnk)
+                continue;
+
+            if (DAT_UnitsState::instance.units[unitID].tribeID != 0)
+                MACRO_CALL_MEMBER(Map::Units::TribesState_Func::removeUnitFromTribe, DAT_TribesState::ptr)(
+                    unitID, DAT_UnitsState::instance.units[unitID].tribeID);
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, tribeID);
+            count--;
         }
         return tribeID;
     }
