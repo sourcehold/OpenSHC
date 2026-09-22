e22e8ad32269a67661e2c90f3a5d83ab16666b0b
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:57:59 +0200

    reimplement: SHC_3BB0A8C1_0x004CB220 97.83% Reimplemented

--- src/OpenSHC/AI/AICState/aiShouldBuildFarm.cpp
+++ src/OpenSHC/AI/AICState/aiShouldBuildFarm.cpp
@@ -11,21 +11,19 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CB220
     BOOLEnum AICState::aiShouldBuildFarm(int playerID)
     {
-        unsigned int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-        if (aiType == AITA_NULL) {
-            return (BOOLEnum)aiType;
-        }
-        aiType -= 1;
-        int const _farmCount
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == AITA_NULL)
+            return FALSE;
+        int aicIndex = aiType - 1;
+
+        int farmCount
             = MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::countFarms, DAT_BuildingsState::ptr)(playerID, 1);
-        if (_farmCount <= 0) {
+        if (farmCount <= 0)
             return TRUE;
-        }
-        if (_farmCount < (int)this->aics[aiType].maxFarms) {
-            return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / _farmCount
-                >= (int)this->aics[aiType].populationPerFarm;
-        }
-        return FALSE;
+        if (farmCount >= this->aics[aicIndex].maxFarms)
+            return FALSE;
+        return DAT_GameState::instance.playerDataArray[playerID].currentPopulation / farmCount
+            >= this->aics[aicIndex].populationPerFarm;
     }
 }
 }
