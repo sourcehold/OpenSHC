ba5fa9d2e900f25f554f11ccdb373fe222a58319
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:39:54 +0200

    reimplement: SHC_3BB0A8C1_0x004D2790 91.30% Reimplemented

--- src/OpenSHC/AI/AICState/aiAssignUnitToDefensiveTribe.cpp
+++ src/OpenSHC/AI/AICState/aiAssignUnitToDefensiveTribe.cpp
@@ -13,14 +13,14 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D2790
     void AICState::aiAssignUnitToDefensiveTribe(int unitID)
     {
-        int _playerID = (int)DAT_UnitsState::instance.units[unitID].owner;
-        if (DAT_GameState::instance.playerDataArray[_playerID].aiType != AITA_NULL) {
-            DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType = 2;
-            int _tribeID = MACRO_CALL_MEMBER(AICState_Func::getDefensiveTribeForUnit, this)(_playerID, unitID);
-            if (_tribeID != 0) {
-                MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, _tribeID);
-            }
-        }
+        int owner = DAT_UnitsState::instance.units[unitID].owner;
+        if (DAT_GameState::instance.playerDataArray[owner].aiType == AITA_NULL)
+            return;
+
+        DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType = 2;
+        int tribeID = MACRO_CALL_MEMBER(AICState_Func::getDefensiveTribeForUnit, this)(owner, unitID);
+        if (tribeID != 0)
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, tribeID);
     }
 }
 }
