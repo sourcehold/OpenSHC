57da4dcb26c2cf06c4b96ea3f233f4fec9d002be
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:23:56 +0200

    reimplement: SHC_3BB0A8C1_0x004CFCD0 55.97% Unresolved attackInfo offsets

--- src/OpenSHC/AI/AICState/aiCommandSiegeEngineTribes.cpp
+++ src/OpenSHC/AI/AICState/aiCommandSiegeEngineTribes.cpp
@@ -4,10 +4,8 @@
 #include "OpenSHC/Map/Units/TribesState.func.hpp"
 #include "OpenSHC/Map/Units/Instructions/UnitMatchSpeedEnum.hpp"
 #include "OpenSHC/Map/Units/SomeTribeBehaviorType.hpp"
-#include "OpenSHC/Map/Units/Unit.hpp"
 #include "OpenSHC/Map/Units/UnitLogicState.hpp"
 #include "OpenSHC/Map/Units/UnitType.hpp"
-#include "OpenSHC/Map/Units/UnitTypeShort.hpp"
 
 #include "OpenSHC/Globals/DAT_DirectionAlgorithmState.hpp"
 #include "OpenSHC/Globals/DAT_GameState.hpp"
@@ -15,108 +13,83 @@
 #include "OpenSHC/Globals/DAT_TroopValueState.hpp"
 #include "OpenSHC/Globals/DAT_UnitsState.hpp"
 #include "OpenSHC/Globals/DAT_ViewportRenderState.hpp"
+
 namespace OpenSHC {
 namespace AI {
-    using OpenSHC::Map::Units::SomeTribeBehaviorType;
-    using OpenSHC::Map::Units::Unit;
-    using OpenSHC::Map::Units::UnitLogicState;
-    using OpenSHC::Map::Units::UnitType;
-    using OpenSHC::Map::Units::UnitTypeShort;
-    using OpenSHC::Map::Units::Instructions::UnitMatchSpeedEnum;
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
+
+    using namespace OpenSHC::Map::Units;
+
     // FUNCTION: STRONGHOLDCRUSADER 0x004CFCD0
-    void AICState ::aiCommandSiegeEngineTribes(int playerID)
+    void AICState::aiCommandSiegeEngineTribes(int playerID)
     {
-        int _targetTribeIDs[3];
-        _targetTribeIDs[0] = 0;
-        _targetTribeIDs[1] = 0;
-        _targetTribeIDs[2] = 0;
-        int _validCount = 0;
-        int _tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xba];
-        if ((_tribeID != 0)
-            && (DAT_TribesState::instance.tribes[_tribeID].uid
-                == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xba])) {
-            _targetTribeIDs[0] = _tribeID;
-            _validCount = 1;
+        int shieldTargetTribeIDs[3];
+        shieldTargetTribeIDs[0] = 0;
+        shieldTargetTribeIDs[1] = 0;
+        shieldTargetTribeIDs[2] = 0;
+        int targetCount = 0;
+        for (int i = 0; i < 3; i++) {
+            int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[186 + i];
+            if (tribeID == 0)
+                continue;
+            if (DAT_TribesState::instance.tribes[tribeID].uid
+                != DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[186 + i])
+                continue;
+            shieldTargetTribeIDs[targetCount] = tribeID;
+            targetCount++;
         }
-        _tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xbb];
-        if ((_tribeID != 0)
-            && (DAT_TribesState::instance.tribes[_tribeID].uid
-                == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xbb])) {
-            _targetTribeIDs[_validCount] = _tribeID;
-            _validCount += 1;
-        }
-        _tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[0xbc];
-        if ((_tribeID != 0)
-            && (DAT_TribesState::instance.tribes[_tribeID].uid
-                == DAT_GameState::instance.playerDataArray[playerID].aiTribeUIDs[0xbc])) {
-            _targetTribeIDs[_validCount] = _tribeID;
-        }
-        int _shieldCount = 0;
-        for (int _unitID = 1; _unitID < 2500; _unitID++) {
-            UnitTypeShort _unitType;
-            if ((DAT_UnitsState::instance.units[_unitID].logicalState != OpenSHC::Map::Units::ULS_INVISIBLE)
-                && ((((_unitType = DAT_UnitsState::instance.units[_unitID].unitType,
-                          _unitType == OpenSHC::Map::Units::UT_S_TOWER
-                              || (_unitType == OpenSHC::Map::Units::UT_S_BATTERINGRAM))
-                         || (_unitType == OpenSHC::Map::Units::UT_S_SHIELD))
-                    && ((DAT_UnitsState::instance.units[_unitID].unknownSiegeTentRelated02 == 3
-                        && (DAT_UnitsState::instance.units[_unitID].aiUnitBehaviourType != 0x15)))))) {
-                if (_unitType == OpenSHC::Map::Units::UT_S_TOWER) {
-                    int _engineTribeID = (int)DAT_UnitsState::instance.units[_unitID].tribeID;
-                    if ((_engineTribeID != 0)
-                        && (*(int*)((int)DAT_TroopValueState::instance.attackInfo.wideValuesArray + playerID * 0x177bc
-                                + -4)
-                            != 0)) {
-                        MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::assignAttackTargetsForTribe,
-                            DAT_TribesState::ptr)(_engineTribeID, OpenSHC::Map::Units::STBT_0x414);
-                    }
-                } else if (_unitType == OpenSHC::Map::Units::UT_S_BATTERINGRAM) {
-                    int _engineTribeID = (int)DAT_UnitsState::instance.units[_unitID].tribeID;
-                    if (_engineTribeID != 0) {
-                        if (*(int*)((int)DAT_TroopValueState::instance.attackInfo.gateValuesArray + playerID * 0x177bc
-                                + -4)
-                            != 0) {
-                            MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::assignAttackTargetsForTribe,
-                                DAT_TribesState::ptr)(_engineTribeID, OpenSHC::Map::Units::STBT_0x3f6);
-                        } else {
-                            if (*(int*)((int)DAT_TroopValueState::instance.attackInfo.wideValuesArray
-                                    + playerID * 0x177bc + -4)
-                                != 0) {
-                                MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::assignAttackTargetsForTribe,
-                                    DAT_TribesState::ptr)(_engineTribeID, OpenSHC::Map::Units::STBT_0x413);
-                            }
-                        }
-                    }
-                } else if (_unitType == OpenSHC::Map::Units::UT_S_SHIELD) {
-                    int _shieldTribeID = (int)DAT_UnitsState::instance.units[_unitID].tribeID;
-                    if ((_shieldTribeID != 0) && (_shieldCount < 3)) {
-                        int _targetTribeID = _targetTribeIDs[_shieldCount];
-                        DAT_TribesState::instance.tribes[_shieldTribeID].field56_0x1f2 = (short)_targetTribeID;
-                        DAT_TribesState::instance.tribes[_shieldTribeID].uid2
-                            = DAT_TribesState::instance.tribes[_targetTribeID].uid;
-                        int _targetUnitID = (int)DAT_TribesState::instance.tribes[_targetTribeID].selectionTargetUnitID;
-                        DAT_TribesState::instance.tribes[_targetTribeID].tribeID = (short)_shieldTribeID;
-                        int _targetTile = DAT_UnitsState::instance.units[_targetUnitID].tile;
-                        _shieldCount = _shieldCount + 1;
-                        if (0 < _targetTile) {
-                            uint y1
-                                = (uint)DAT_ViewportRenderState::instance.tileTranslationMatrix_YComponent[_targetTile];
-                            uint x1 = _targetTile - DAT_ViewportRenderState::instance.translationMatrix[y1].addXgetTile;
-                            MACRO_CALL_MEMBER(
-                                OpenSHC::Map::Units::TribesState_Func::giveTribeMoveInstruction, DAT_TribesState::ptr)(
-                                _shieldTribeID, x1, y1, 0, 0, OpenSHC::Map::Units::Instructions::UMSE_0);
-                            MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::
-                                                  calculatePreferredRelativeOrientation,
-                                DAT_DirectionAlgorithmState::ptr)((int)DAT_UnitsState::instance.units[_targetUnitID].x,
-                                (int)((int)(DAT_UnitsState::instance.units[_targetUnitID].y)), (int)((int)(x1)),
-                                (int)((int)(y1)));
-                            DAT_TribesState::instance.tribes[_shieldTribeID].orientation
-                                = (short)DAT_DirectionAlgorithmState::instance.orientation;
-                        }
-                    }
-                }
+
+        int shieldCount = 0;
+        for (int unitID = 1; unitID < 2500; unitID++) {
+            if (DAT_UnitsState::instance.units[unitID].logicalState == ULS_INVISIBLE)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].unitType != UT_S_TOWER
+                && DAT_UnitsState::instance.units[unitID].unitType != UT_S_BATTERINGRAM
+                && DAT_UnitsState::instance.units[unitID].unitType != UT_S_SHIELD)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].unknownSiegeTentRelated02 != 3)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType == 21)
+                continue;
+
+            int tribeID = DAT_UnitsState::instance.units[unitID].tribeID;
+            if (tribeID == 0)
+                continue;
+
+            if (DAT_UnitsState::instance.units[unitID].unitType == UT_S_TOWER) {
+                if (*(int*)((int)DAT_TroopValueState::instance.attackInfo.wideValuesArray + playerID * 0x177bc - 4) != 0)
+                    MACRO_CALL_MEMBER(TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
+                        tribeID, STBT_0x414);
+            } else if (DAT_UnitsState::instance.units[unitID].unitType == UT_S_BATTERINGRAM) {
+                if (*(int*)((int)DAT_TroopValueState::instance.attackInfo.gateValuesArray + playerID * 0x177bc - 4) != 0)
+                    MACRO_CALL_MEMBER(TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
+                        tribeID, STBT_0x3f6);
+                else if (*(int*)((int)DAT_TroopValueState::instance.attackInfo.wideValuesArray + playerID * 0x177bc - 4)
+                    != 0)
+                    MACRO_CALL_MEMBER(TribesState_Func::assignAttackTargetsForTribe, DAT_TribesState::ptr)(
+                        tribeID, STBT_0x413);
+            } else {
+                if (shieldCount >= 3)
+                    continue;
+
+                int targetTribeID = shieldTargetTribeIDs[shieldCount];
+                DAT_TribesState::instance.tribes[tribeID].field56_0x1f2 = targetTribeID;
+                DAT_TribesState::instance.tribes[tribeID].uid2 = DAT_TribesState::instance.tribes[targetTribeID].uid;
+                int targetUnitID = DAT_TribesState::instance.tribes[targetTribeID].selectionTargetUnitID;
+                DAT_TribesState::instance.tribes[targetTribeID].tribeID = tribeID;
+                int tile = DAT_UnitsState::instance.units[targetUnitID].tile;
+                shieldCount++;
+                if (tile <= 0)
+                    continue;
+
+                int y = DAT_ViewportRenderState::instance.tileTranslationMatrix_YComponent[tile];
+                int x = tile - DAT_ViewportRenderState::instance.translationMatrix[y].addXgetTile;
+                MACRO_CALL_MEMBER(TribesState_Func::giveTribeMoveInstruction, DAT_TribesState::ptr)(
+                    tribeID, x, y, 0, 0, Instructions::UMSE_0);
+                MACRO_CALL_MEMBER(
+                    OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::calculatePreferredRelativeOrientation,
+                    DAT_DirectionAlgorithmState::ptr)(
+                    DAT_UnitsState::instance.units[targetUnitID].x, DAT_UnitsState::instance.units[targetUnitID].y, x, y);
+                DAT_TribesState::instance.tribes[tribeID].orientation = DAT_DirectionAlgorithmState::instance.orientation;
             }
         }
     }
