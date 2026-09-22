fe43623551c2752bf9d29a559e3fcb88004a5c37
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:13:53 +0200

    reimplement: SHC_3BB0A8C1_0x004D3850 82.35% AIC index folding differs

--- src/OpenSHC/AI/AICState/determineAIPlayerHelp.cpp
+++ src/OpenSHC/AI/AICState/determineAIPlayerHelp.cpp
@@ -1,53 +1,33 @@
 #include "../AICState.func.hpp"
 
-#include "OpenSHC/Global.func.hpp"
 #include "OpenSHC/AI/AIType.hpp"
-#include "OpenSHC/AI/AITypeInt.hpp"
-#include "OpenSHC/WindowsHelper/Enums/BOOLEnum.hpp"
 
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::AI::AIType;
-    using OpenSHC::AI::AITypeInt;
-    using OpenSHC::WindowsHelper::Enums::BOOLEnum;
-
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004D3850
-    BOOLEnum AICState ::determineAIPlayerHelp(int playerID, int requestedByPlayerID)
-
+    BOOLEnum AICState::determineAIPlayerHelp(int playerID, int requestedByPlayerID)
     {
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == OpenSHC::AI::AIT_NULL)
+            return FALSE;
+        int aicIndex = aiType - 1;
 
-        AITypeInt AVar1;
-
-        AVar1 = DAT_GameState::instance.playerDataArray[playerID].aiType;
-
-        if (AVar1 != OpenSHC::AI::AIT_NULL) {
-
-            if (DAT_GameState::instance.playerDataArray[playerID].isNotNervousByEnemyTroopValue == 0) {
-
-                if (DAT_GameState::instance.playerDataArray[playerID].totalAttackTroops
-                    < *(int*)((int)this + (AVar1 + ~OpenSHC::AI::AIT_NULL) * 0x2a4 + 0x1fc)) {
-
-                    MACRO_CALL(OpenSHC::Global_Func::PlayWillNotHelp2BikFromPlayerToPlayer)(
-                        playerID, requestedByPlayerID);
-
-                    return FALSE;
-                }
-
-                MACRO_CALL(OpenSHC::Global_Func::PlayWillHelpBikFromPlayerToPlayer)(playerID, requestedByPlayerID);
-
-                return TRUE;
-            }
-
-            MACRO_CALL(OpenSHC::Global_Func::PlayWillNotHelp1BikFromPlayerToPlayer)(playerID, requestedByPlayerID);
+        if (DAT_GameState::instance.playerDataArray[playerID].isNotNervousByEnemyTroopValue != 0) {
+            MACRO_CALL_MEMBER(AICState_Func::playWillNotHelp1BikFromPlayerToPlayer, this)(playerID, requestedByPlayerID);
+            return FALSE;
         }
 
-        return FALSE;
+        if (DAT_GameState::instance.playerDataArray[playerID].totalAttackTroops
+            < this->aics[aicIndex].AttForceSupportAllyThreshold) {
+            MACRO_CALL_MEMBER(AICState_Func::playWillNotHelp2BikFromPlayerToPlayer, this)(playerID, requestedByPlayerID);
+            return FALSE;
+        }
+
+        MACRO_CALL_MEMBER(AICState_Func::playWillHelpBikFromPlayerToPlayer, this)(playerID, requestedByPlayerID);
+        return TRUE;
     }
 
 }
