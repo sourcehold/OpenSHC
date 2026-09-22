19d5677deca2cc61801f3fe839c65a6dec61882e
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:13:52 +0200

    reimplement: SHC_3BB0A8C1_0x004CDB20 23.30% Loop/register structure differs

--- src/OpenSHC/AI/AICState/chooseRandomBuildingIDFromPlayersArrayOfBuildings.cpp
+++ src/OpenSHC/AI/AICState/chooseRandomBuildingIDFromPlayersArrayOfBuildings.cpp
@@ -1,7 +1,5 @@
 #include "../AICState.func.hpp"
 
-#include "OpenSHC/Map/Buildings/BuildingTypeInt.hpp"
-
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 #include "OpenSHC/Globals/DAT_SkirmishDefinedData.hpp"
@@ -10,72 +8,35 @@
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::Map::Buildings::BuildingTypeInt;
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CDB20
-    int AICState ::chooseRandomBuildingIDFromPlayersArrayOfBuildings(int playerID)
-
+    int AICState::chooseRandomBuildingIDFromPlayersArrayOfBuildings(int playerID)
     {
+        int chosenBuildingID = 0;
+        int chosenPriority = 46;
+        int buildingCount = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildingsTracker;
+        int rng = SEC_RNG::instance.currentNumber2 & 7;
+        for (int i = 0; i < buildingCount; i++) {
+            int buildingID = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildings[i];
+            if (buildingID == 0)
+                continue;
 
-        int _count = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildingsTracker;
+            int buildingType = DAT_BuildingsState::instance.buildings[buildingID].buildingType;
+            for (int j = 0; j < chosenPriority; j++) {
+                bool isMatch;
+                if (rng <= 1)
+                    isMatch = buildingType == DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet3[j];
+                else if (rng <= 3)
+                    isMatch = buildingType == DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet2[j];
+                else
+                    isMatch = buildingType == DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet1[j];
 
-        int _chosenBuilding = 0;
-
-        int _chosenBuildingTypeIndex = 46;
-
-        if (0 < _count) {
-
-            int _rng = (int)(unsigned char)SEC_RNG::instance.currentNumber2 & 7;
-
-            short* _ptr = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildings;
-
-            do {
-
-                int _buildingID = (int)*_ptr;
-
-                if (_buildingID != 0) {
-
-                    int _selectionIndex = 0;
-
-                    if (0 < _chosenBuildingTypeIndex) {
-
-                        BuildingTypeInt _buildingType
-                            = (BuildingTypeInt)(short)DAT_BuildingsState::instance.buildings[_buildingID].buildingType;
-
-                        do {
-
-                            BuildingTypeInt _cmpType;
-
-                            if (_rng < 2) {
-                                _cmpType
-                                    = DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet3[_selectionIndex];
-                            } else if (_rng < 4) {
-                                _cmpType
-                                    = DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet2[_selectionIndex];
-                            } else {
-                                _cmpType
-                                    = DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet1[_selectionIndex];
-                            }
-
-                            if (_buildingType == _cmpType) {
-                                _chosenBuilding = _buildingID;
-                                _chosenBuildingTypeIndex = _selectionIndex;
-                            }
-
-                            _selectionIndex = _selectionIndex + 1;
-
-                        } while (_selectionIndex < _chosenBuildingTypeIndex);
-                    }
+                if (isMatch) {
+                    chosenBuildingID = buildingID;
+                    chosenPriority = j;
                 }
-
-                _ptr = _ptr + 1;
-
-                _count = _count - 1;
-
-            } while (_count != 0);
+            }
         }
-
-        return _chosenBuilding;
+        return chosenBuildingID;
     }
 
 }
