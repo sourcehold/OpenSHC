09a271042d2415a314863c6f6e5afd1367de1f64
Gynt <Gynt@users.noreply.github.com>
2026-09-22 16:00:06 +0200

    reimplement: SHC_3BB0A8C1_0x004D2660 59.57% Branch layout differs

--- src/OpenSHC/AI/AICState/assignUnitToATribe.cpp
+++ src/OpenSHC/AI/AICState/assignUnitToATribe.cpp
@@ -1,6 +1,5 @@
 #include "../AICState.func.hpp"
 
-#include "OpenSHC/Global.func.hpp"
 #include "OpenSHC/Map/Units/TribesState.func.hpp"
 #include "OpenSHC/AI/AITypeA.hpp"
 
@@ -14,38 +13,28 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D2660
     void AICState::assignUnitToATribe(int unitID)
     {
-        short* _unitTypeIndex;
-        int _isOfType;
-        int _tribeID;
-        int _playerID = (int)DAT_UnitsState::instance.units[unitID].owner;
-        if (DAT_GameState::instance.playerDataArray[_playerID].aiType != AITA_NULL) {
-            int iVar1;
-            if ((DAT_GameState::instance.playerDataArray[_playerID].aivCurrentPauseIndex == 1)
-                && (0 < DAT_GameState::instance.playerDataArray[_playerID].aivPauses[1])) {
-                iVar1 = 1;
-            } else {
-                iVar1 = 0;
-            }
-            _unitTypeIndex = (short*)MACRO_CALL(Global_Func::GetUnitTypeIndexForUnitID)(unitID, iVar1);
-            int _tribeSize = DAT_GameState::instance.playerDataArray[_playerID]
-                                 .aivUnitLocationSlotLocationCount[(int)_unitTypeIndex];
-            DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType = 1;
-            if (_tribeSize < 1) {
-                _isOfType = MACRO_CALL_MEMBER(AICState_Func::unitIDIsRangedOrArmored, this)(unitID);
-                if (_isOfType != 0) {
-                    _tribeID = MACRO_CALL_MEMBER(AICState_Func::createTribeForUnitType, this)(_playerID, 0);
-                    MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
-                        unitID, _tribeID);
-                    return;
-                }
-                _tribeID = MACRO_CALL_MEMBER(AICState_Func::createTribeForUnitType, this)(_playerID, 1);
-                MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, _tribeID);
+        int owner = DAT_UnitsState::instance.units[unitID].owner;
+        if (DAT_GameState::instance.playerDataArray[owner].aiType == AITA_NULL)
+            return;
+
+        AIVUnitType aivUnitType = MACRO_CALL_MEMBER(AICState_Func::getUnitTypeIndexForUnitID, this)(unitID,
+            DAT_GameState::instance.playerDataArray[owner].aivCurrentPauseIndex == 1
+                    && DAT_GameState::instance.playerDataArray[owner].aivPauses[1] > 0
+                ? 1
+                : 0);
+        DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType = 1;
+        if (DAT_GameState::instance.playerDataArray[owner].aivUnitLocationSlotLocationCount[aivUnitType] <= 0) {
+            if (MACRO_CALL_MEMBER(AICState_Func::unitIDIsRangedOrArmored, this)(unitID) != 0) {
+                MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
+                    unitID, MACRO_CALL_MEMBER(AICState_Func::createTribeForUnitType, this)(owner, 0));
                 return;
             }
-            _tribeID = MACRO_CALL_MEMBER(AICState_Func::smallestTribeOfUnitType, this)(
-                _playerID, (OpenSHC::Map::Units::UnitType)((int)_unitTypeIndex));
-            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, _tribeID);
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
+                unitID, MACRO_CALL_MEMBER(AICState_Func::createTribeForUnitType, this)(owner, 1));
+            return;
         }
+        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
+            unitID, MACRO_CALL_MEMBER(AICState_Func::smallestTribeOfUnitType, this)(owner, aivUnitType));
     }
 }
 }
