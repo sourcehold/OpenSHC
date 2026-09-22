8a8ac98722cbcee09d6d453bd6baec122e8410ea
Gynt <Gynt@users.noreply.github.com>
2026-09-22 09:58:29 +0200

    reimplement: SHC_3BB0A8C1_0x004D0A80 100%

--- src/OpenSHC/AI/AICState/playThanksBikFromPlayerToPlayer.cpp
+++ src/OpenSHC/AI/AICState/playThanksBikFromPlayerToPlayer.cpp
@@ -14,23 +14,30 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D0A80
     void AICState::playThanksBikFromPlayerToPlayer(int playerID, int targetPlayerID)
     {
-        DWORD DVar2;
-        int iVar1
-            = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID);
-        if (iVar1 != 0) {
-            iVar1 = (int)DAT_GameState::instance.playerDataArray[playerID].aiType - 1;
-            DAT_GameState::instance.playerDataArray[playerID].aiRequestDelay = this->aics[iVar1].aiRequestDelay;
-            if (((targetPlayerID == DAT_GameSynchronyState::instance.currentPlayerSlotID)
-                    && (DAT_GameState::instance.playerDataArray[DAT_GameSynchronyState::instance.currentPlayerSlotID]
-                            .lordKilledByPlayerID
-                        == 0))
-                && ((DVar2 = timeGetTime(),
-                    DWORD_00eb0b18::instance == 0 || (1199999 < DVar2 - DWORD_00eb0b18::instance)))) {
-                DWORD_00eb0b18::instance = DVar2;
-                MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
-                    playerID, iVar1, 0xf);
-            }
+        if (MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID) == 0) {
+            return;
         }
+
+        int aicIndex = DAT_GameState::instance.playerDataArray[playerID].aiType - 1;
+        DAT_GameState::instance.playerDataArray[playerID].aiRequestDelay = this->aics[aicIndex].aiRequestDelay;
+
+        if (targetPlayerID != DAT_GameSynchronyState::instance.currentPlayerSlotID) {
+            return;
+        }
+        if (DAT_GameState::instance.playerDataArray[DAT_GameSynchronyState::instance.currentPlayerSlotID]
+                .lordKilledByPlayerID
+            != 0) {
+            return;
+        }
+
+        DWORD now = timeGetTime();
+        if (DWORD_00eb0b18::instance != 0 && now - DWORD_00eb0b18::instance < 1200000) {
+            return;
+        }
+
+        DWORD_00eb0b18::instance = now;
+        MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
+            playerID, aicIndex, 15);
     }
 }
 }
