eb8b1715518b7270f9c4012b58a4d3251d2925f0
Gynt <Gynt@users.noreply.github.com>
2026-09-22 14:42:08 +0200

    reimplement: SHC_3BB0A8C1_0x004CDBC0 65.04% Loop counter spilled in orig

--- src/OpenSHC/AI/AICState/getTargetableBuildingForPlayerID.cpp
+++ src/OpenSHC/AI/AICState/getTargetableBuildingForPlayerID.cpp
@@ -1,8 +1,5 @@
 #include "../AICState.func.hpp"
 
-#include "OpenSHC/Game/Player/PlayerData.hpp"
-#include "OpenSHC/Map/Buildings/BuildingTypeInt.hpp"
-
 #include "OpenSHC/Globals/DAT_BuildingsState.hpp"
 #include "OpenSHC/Globals/DAT_GameState.hpp"
 #include "OpenSHC/Globals/DAT_SkirmishDefinedData.hpp"
@@ -10,106 +7,40 @@
 namespace OpenSHC {
 namespace AI {
 
-    using OpenSHC::Game::Player::PlayerData;
-    using OpenSHC::Map::Buildings::BuildingTypeInt;
-
-    /*
-      decompilerscript: committed: 2025-01-30 21:57:43.216000 */
-
     // FUNCTION: STRONGHOLDCRUSADER 0x004CDBC0
-    int AICState ::getTargetableBuildingForPlayerID(int playerID, int param_2)
-
+    int AICState::getTargetableBuildingForPlayerID(int playerID, int param_2)
     {
-
-        int* piVar1;
-
-        PlayerData* pPVar2;
-
-        int iVar3;
-
-        int _candidateBuilding;
-
-        BuildingTypeInt* _pTargets;
-
-        int _index;
-
-        int _aiType;
-
-        int _outerPatrolGroupsCount;
-
-        int _tracker;
-
-        iVar3 = playerID;
-
-        _aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
-
-        if ((_aiType != 0)
-            && (_outerPatrolGroupsCount = *(int*)((int)this + (_aiType + -1) * 0x2a4 + 0x174),
-                0 < _outerPatrolGroupsCount)) {
-
-            _tracker = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildingsTracker;
-
-            if (0 < _tracker) {
-
-                piVar1 = &DAT_GameState::instance.playerDataArray[playerID].someCounter2;
-
-                *piVar1 = *piVar1 + 1;
-
-                if (_tracker <= DAT_GameState::instance.playerDataArray[playerID].someCounter2) {
-
-                    DAT_GameState::instance.playerDataArray[playerID].someCounter2 = 0;
-                }
-
-                pPVar2 = DAT_GameState::instance.playerDataArray + playerID;
-
-                playerID = 0;
-
-                _index = ((_tracker / _outerPatrolGroupsCount) * param_2 + pPVar2->someCounter2) % _tracker;
-
-                if (0 < _tracker) {
-
-                    do {
-
-                        _candidateBuilding
-                            = (int)DAT_GameState::instance.playerDataArray[iVar3].top100TargetableBuildings[_index];
-
-                        _index = _index + 1;
-
-                        if (_tracker <= _index) {
-
-                            _index = 0;
-                        }
-
-                        if (_candidateBuilding != 0) {
-
-                            _pTargets = DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet1;
-
-                            do {
-
-                                if ((int)(short)DAT_BuildingsState::instance.buildings[_candidateBuilding].buildingType
-                                    == *_pTargets) {
-
-                                    return _candidateBuilding;
-                                }
-
-                                _pTargets = _pTargets + 1;
-
-                                /*
-                                                Iterates only over resource buildings, so if resource building, return
-                                 */
-
-                            } while ((int)_pTargets < 0xb427c8);
-                        }
-
-                        playerID = playerID + 1;
-
-                    } while (playerID < _tracker);
-                }
-            }
-
+        int aiType = DAT_GameState::instance.playerDataArray[playerID].aiType;
+        if (aiType == 0)
             return 0;
-        }
+        int outerPatrolGroupsCount = this->aics[aiType - 1].OuterPatrolGroupsCount;
+        if (outerPatrolGroupsCount <= 0)
+            return 0;
+        int tracker = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildingsTracker;
+        if (tracker <= 0)
+            return 0;
 
+        DAT_GameState::instance.playerDataArray[playerID].someCounter2++;
+        if (DAT_GameState::instance.playerDataArray[playerID].someCounter2 >= tracker)
+            DAT_GameState::instance.playerDataArray[playerID].someCounter2 = 0;
+
+        int index = (tracker / outerPatrolGroupsCount * param_2
+                        + DAT_GameState::instance.playerDataArray[playerID].someCounter2)
+            % tracker;
+        for (int i = 0; i < tracker; i++) {
+            int buildingID = DAT_GameState::instance.playerDataArray[playerID].top100TargetableBuildings[index];
+            index++;
+            if (index >= tracker)
+                index = 0;
+            if (buildingID == 0)
+                continue;
+
+            for (int j = 0; j < 8; j++) {
+                if (DAT_BuildingsState::instance.buildings[buildingID].buildingType
+                    == DAT_SkirmishDefinedData::instance.BuildingTargetPrioritySet1[j])
+                    return buildingID;
+            }
+        }
         return 0;
     }
 
