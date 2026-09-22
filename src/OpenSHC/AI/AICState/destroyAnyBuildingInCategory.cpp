d9747325126b4bb007425bfb46e2c2f22423e717
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:20:08 +0200

    reimplement: SHC_3BB0A8C1_0x004D0280 98.19% Reimplemented

--- src/OpenSHC/AI/AICState/destroyAnyBuildingInCategory.cpp
+++ src/OpenSHC/AI/AICState/destroyAnyBuildingInCategory.cpp
@@ -8,67 +8,51 @@
 namespace OpenSHC {
 namespace AI {
 
+    using namespace OpenSHC::Map::Buildings;
+
     // FUNCTION: STRONGHOLDCRUSADER 0x004D0280
     void AICState::destroyAnyBuildingInCategory(int playerID, AINervousDestroyBuildingCategory buildingCategory)
     {
-        int _buildingID = 1;
-        if (1 < DAT_BuildingsState::instance.maxBuildingsCount) {
-            Map::Buildings::Building* pBVar2 = &DAT_BuildingsState::instance.buildings[1];
-            do {
-                if (((pBVar2->logicalState != 0) && (pBVar2->logicalState != Map::Buildings::BLS_REMOVE))
-                    && (pBVar2->owner == playerID)) {
+        for (int buildingID = 1; buildingID < DAT_BuildingsState::instance.maxBuildingsCount; buildingID++) {
+            if (DAT_BuildingsState::instance.buildings[buildingID].logicalState == 0)
+                continue;
+            if (DAT_BuildingsState::instance.buildings[buildingID].logicalState == BLS_REMOVE)
+                continue;
+            if (DAT_BuildingsState::instance.buildings[buildingID].owner != playerID)
+                continue;
 
-                    bool _matches = false;
-                    Map::Buildings::BuildingTypeShort BVar1 = pBVar2->buildingType;
+            bool matches = false;
+            if (buildingCategory == AINDBC_FEAR_FACTOR_BUILDINGS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_CESSPIT || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_BURNINGSTAKE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_GIBBET || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DUNGEON
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_STRETCHINGRACK || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_RACKFLOGGING || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_CHOPPINGBLOCK
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DUNKINGSTOOL || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DOGCAGE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_STATUE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_SHRINE
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DANCINGBEAR || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_POND || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_GALLOWS || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_STOCKS
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_WITCHHOIST || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_MAYPOLE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_GARDEN;
+            else if (buildingCategory == AINDBC_WORKSHOPS_AND_RELIGIOUS_BUILDINGS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_FLETCHER || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_BLACKSMITH || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_POLETURNER || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_ARMOURER
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TANNER || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_INN || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_APOTHECARY || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_CHAPEL
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_CHURCH || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_CATHEDRAL;
+            else if (buildingCategory == AINDBC_GATEHOUSES_TOWERS_GUILDS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_GATEHOUSELARGE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_GATEHOUSESMALL || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DRAWBRIDGE
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TOWER1 || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TOWER2 || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TOWER3 || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TOWER4
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TOWER5 || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_ENGINEERSGUILD || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_TUNNELERSGUILD
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_OILSMELTER;
+            else if (buildingCategory == AINDBC_BARRACKS_MERCPOST_STABLES)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_MERCENARYPOST || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_BARRACKS || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_STABLES;
+            else if (buildingCategory == AINDBC_WATER_BUILDINGS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_WELL || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_WATERPOT;
+            else if (buildingCategory == AINDBC_RESOURCE_BUILDINGS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_OXTETHER || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_IRONMINE || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_PITCHRIG || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_BAKERY
+                    || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_BREWERY || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_QUARRY || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_MILL;
+            else if (buildingCategory == AINDBC_FARMS)
+                matches = DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_WHEATFARM || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_HOPFARM || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_APPLEFARM || DAT_BuildingsState::instance.buildings[buildingID].buildingType == BT_DAIRYFARM;
 
-                    if (buildingCategory == AINDBC_FEAR_FACTOR_BUILDINGS) {
-                        _matches = (BVar1 == Map::Buildings::BT_CESSPIT || BVar1 == Map::Buildings::BT_BURNINGSTAKE
-                            || BVar1 == Map::Buildings::BT_GIBBET || BVar1 == Map::Buildings::BT_DUNGEON
-                            || BVar1 == Map::Buildings::BT_STRETCHINGRACK || BVar1 == Map::Buildings::BT_RACKFLOGGING
-                            || BVar1 == Map::Buildings::BT_CHOPPINGBLOCK || BVar1 == Map::Buildings::BT_DUNKINGSTOOL
-                            || BVar1 == Map::Buildings::BT_DOGCAGE || BVar1 == Map::Buildings::BT_STATUE
-                            || BVar1 == Map::Buildings::BT_SHRINE || BVar1 == Map::Buildings::BT_DANCINGBEAR
-                            || BVar1 == Map::Buildings::BT_POND || BVar1 == Map::Buildings::BT_GALLOWS
-                            || BVar1 == Map::Buildings::BT_STOCKS || BVar1 == Map::Buildings::BT_WITCHHOIST
-                            || BVar1 == Map::Buildings::BT_MAYPOLE || BVar1 == Map::Buildings::BT_GARDEN);
-                    } else if (buildingCategory == AINDBC_WORKSHOPS_AND_RELIGIOUS_BUILDINGS) {
-                        _matches = (BVar1 == Map::Buildings::BT_FLETCHER || BVar1 == Map::Buildings::BT_BLACKSMITH
-                            || BVar1 == Map::Buildings::BT_POLETURNER || BVar1 == Map::Buildings::BT_ARMOURER
-                            || BVar1 == Map::Buildings::BT_TANNER || BVar1 == Map::Buildings::BT_INN
-                            || BVar1 == Map::Buildings::BT_APOTHECARY || BVar1 == Map::Buildings::BT_CHAPEL
-                            || BVar1 == Map::Buildings::BT_CHURCH || BVar1 == Map::Buildings::BT_CATHEDRAL);
-                    } else if (buildingCategory == AINDBC_GATEHOUSES_TOWERS_GUILDS) {
-                        _matches = (BVar1 == Map::Buildings::BT_GATEHOUSELARGE
-                            || BVar1 == Map::Buildings::BT_GATEHOUSESMALL || BVar1 == Map::Buildings::BT_DRAWBRIDGE
-                            || BVar1 == Map::Buildings::BT_TOWER1 || BVar1 == Map::Buildings::BT_TOWER2
-                            || BVar1 == Map::Buildings::BT_TOWER3 || BVar1 == Map::Buildings::BT_TOWER4
-                            || BVar1 == Map::Buildings::BT_TOWER5 || BVar1 == Map::Buildings::BT_ENGINEERSGUILD
-                            || BVar1 == Map::Buildings::BT_TUNNELERSGUILD || BVar1 == Map::Buildings::BT_OILSMELTER);
-                    } else if (buildingCategory == AINDBC_BARRACKS_MERCPOST_STABLES) {
-                        _matches = (BVar1 == Map::Buildings::BT_MERCENARYPOST || BVar1 == Map::Buildings::BT_BARRACKS
-                            || BVar1 == Map::Buildings::BT_STABLES);
-                    } else if (buildingCategory == AINDBC_WATER_BUILDINGS) {
-                        _matches = (BVar1 == Map::Buildings::BT_WELL || BVar1 == Map::Buildings::BT_WATERPOT);
-                    } else if (buildingCategory == AINDBC_RESOURCE_BUILDINGS) {
-                        _matches = (BVar1 == Map::Buildings::BT_OXTETHER || BVar1 == Map::Buildings::BT_IRONMINE
-                            || BVar1 == Map::Buildings::BT_PITCHRIG || BVar1 == Map::Buildings::BT_BAKERY
-                            || BVar1 == Map::Buildings::BT_BREWERY || BVar1 == Map::Buildings::BT_QUARRY
-                            || BVar1 == Map::Buildings::BT_MILL);
-                    } else if (buildingCategory == AINDBC_FARMS) {
-                        _matches = (BVar1 == Map::Buildings::BT_WHEATFARM || BVar1 == Map::Buildings::BT_HOPFARM
-                            || BVar1 == Map::Buildings::BT_APPLEFARM || BVar1 == Map::Buildings::BT_DAIRYFARM);
-                    }
+            if (!matches)
+                continue;
 
-                    if (_matches) {
-                        MACRO_CALL_MEMBER(Map::Buildings::BuildingsState_Func::giveBackResourceForDestroyedBuilding,
-                            DAT_BuildingsState::ptr)(_buildingID, playerID, 0x32);
-                        MACRO_CALL_MEMBER(
-                            Map::Buildings::BuildingsState_Func::destroyBuilding, DAT_BuildingsState::ptr)(_buildingID);
-                    }
-                }
-                _buildingID = _buildingID + 1;
-                pBVar2 = pBVar2 + 1;
-            } while (_buildingID < DAT_BuildingsState::instance.maxBuildingsCount);
+            MACRO_CALL_MEMBER(BuildingsState_Func::giveBackResourceForDestroyedBuilding, DAT_BuildingsState::ptr)(
+                buildingID, playerID, 50);
+            MACRO_CALL_MEMBER(BuildingsState_Func::destroyBuilding, DAT_BuildingsState::ptr)(buildingID);
         }
     }
 }
