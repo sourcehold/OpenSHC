8f428beafff96cf48bc1fc07e84965e23ae769a2
Gynt <Gynt@users.noreply.github.com>
2026-09-22 09:57:52 +0200

    reimplement: SHC_3BB0A8C1_0x004D0C70 100%

--- src/OpenSHC/AI/AICState/playVictory2BikFromPlayerToPlayer.cpp
+++ src/OpenSHC/AI/AICState/playVictory2BikFromPlayerToPlayer.cpp
@@ -14,21 +14,30 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004D0C70
     void AICState::playVictory2BikFromPlayerToPlayer(int playerID, int targetPlayerID)
     {
-        int iVar1;
-        bool bVar2;
-        if ((((DAT_GameState::instance.playerDataArray[targetPlayerID].aiType != AITA_NULL)
-                 && (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[playerID] != -1))
-                && (iVar1 = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
-                        targetPlayerID),
-                    iVar1 != 0))
-            && (((iVar1 = MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(
-                      playerID),
-                     iVar1 != 0 && (DAT_GameState::instance.playerDataArray[targetPlayerID].someAiCountdown4 == 0))
-                && (bVar2 = playerID == DAT_GameSynchronyState::instance.currentPlayerSlotID,
-                    DAT_GameState::instance.playerDataArray[targetPlayerID].someAiCountdown4 = 0x30, bVar2)))) {
-            MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
-                targetPlayerID, (int)DAT_GameState::instance.playerDataArray[targetPlayerID].aiType - 1, 0xb);
+        if (DAT_GameState::instance.playerDataArray[targetPlayerID].aiType == AITA_NULL) {
+            return;
         }
+        if (DAT_GameSynchronyState::instance.currentPlayerFullIDArray[playerID] == -1) {
+            return;
+        }
+        if (MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(targetPlayerID)
+            == 0) {
+            return;
+        }
+        if (MACRO_CALL_MEMBER(Map::Units::UnitsState_Func::getAliveLordForPlayer, DAT_UnitsState::ptr)(playerID) == 0) {
+            return;
+        }
+        if (DAT_GameState::instance.playerDataArray[targetPlayerID].someAiCountdown4 != 0) {
+            return;
+        }
+
+        DAT_GameState::instance.playerDataArray[targetPlayerID].someAiCountdown4 = 48;
+        if (playerID != DAT_GameSynchronyState::instance.currentPlayerSlotID) {
+            return;
+        }
+
+        MACRO_CALL_MEMBER(Rendering::Bink::AIMessageQueue_Func::playBikVideoFromPlayer, DAT_VideoBikQueue::ptr)(
+            targetPlayerID, DAT_GameState::instance.playerDataArray[targetPlayerID].aiType - 1, 11);
     }
 }
 }
