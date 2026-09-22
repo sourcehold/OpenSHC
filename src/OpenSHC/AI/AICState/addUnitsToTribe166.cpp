f9dc9049955d1ef7437fcab823e353f6286808d2
Gynt <Gynt@users.noreply.github.com>
2026-09-22 15:34:23 +0200

    reimplement: SHC_3BB0A8C1_0x004CC720 95.24% Reimplemented

--- src/OpenSHC/AI/AICState/addUnitsToTribe166.cpp
+++ src/OpenSHC/AI/AICState/addUnitsToTribe166.cpp
@@ -12,25 +12,23 @@ namespace AI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004CC720
     void AICState::addUnitsToTribe166(int playerID)
     {
-        int tribeID;
-        uint unitID = 1;
-        if (1 < (int)DAT_UnitsState::instance.maxUnitCount) {
-            Map::Units::Unit* pUVar1 = &DAT_UnitsState::instance.units[1];
-            do {
-                if ((((pUVar1->owner == playerID) && (pUVar1->logicalState == Map::Units::ULS_NORMAL))
-                        && (pUVar1->dying == 0))
-                    && (pUVar1->aiUnitBehaviourType == 6)) {
-                    tribeID = (int)DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[166];
-                    if (pUVar1->tribeID != tribeID) {
-                        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::removeUnitFromTribe, DAT_TribesState::ptr)(
-                            unitID, (int)pUVar1->tribeID);
-                        MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(
-                            unitID, tribeID);
-                    }
-                }
-                unitID = unitID + 1;
-                pUVar1 = pUVar1 + 1;
-            } while ((int)unitID < (int)DAT_UnitsState::instance.maxUnitCount);
+        for (int unitID = 1; unitID < (int)DAT_UnitsState::instance.maxUnitCount; unitID++) {
+            if (DAT_UnitsState::instance.units[unitID].owner != playerID)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].logicalState != Map::Units::ULS_NORMAL)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].dying != 0)
+                continue;
+            if (DAT_UnitsState::instance.units[unitID].aiUnitBehaviourType != 6)
+                continue;
+
+            int tribeID = DAT_GameState::instance.playerDataArray[playerID].aiTribeIDs[166];
+            if (DAT_UnitsState::instance.units[unitID].tribeID == tribeID)
+                continue;
+
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::removeUnitFromTribe, DAT_TribesState::ptr)(
+                unitID, DAT_UnitsState::instance.units[unitID].tribeID);
+            MACRO_CALL_MEMBER(Map::Units::TribesState_Func::addUnitToTribe, DAT_TribesState::ptr)(unitID, tribeID);
         }
     }
 }
