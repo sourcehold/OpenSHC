c242cdda4ecd50b4de776b8ab154281be5589760
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:32 +0200

    reimplement: SHC_3BB0A8C1_0x004591B0 100.00% Reimplemented

--- src/OpenSHC/Game/GameStateStructures/getNumberToDisplayBribeIncome.cpp
+++ src/OpenSHC/Game/GameStateStructures/getNumberToDisplayBribeIncome.cpp
@@ -6,13 +6,13 @@ namespace Game {
     // FUNCTION: STRONGHOLDCRUSADER 0x004591B0
     int GameStateStructures::getNumberToDisplayBribeIncome(int playerIndex, int taxStep, int currentPopulation)
     {
-        int iVar1 = 0;
+        int bribeIncome = 0;
         if (taxStep < 3) {
-            iVar1 = MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::calculateTaxBribeExpenseForPlayer, this)(
-                playerIndex, taxStep, currentPopulation);
-            iVar1 = iVar1 * 4;
+            bribeIncome = MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::calculateTaxBribeExpenseForPlayer,
+                              this)(playerIndex, taxStep, currentPopulation)
+                * 4;
         }
-        return iVar1 / 10;
+        return bribeIncome / 10;
     }
 
 }
