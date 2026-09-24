32cb57715f4736ec7a5e53c10a448c1cf6cfdc99
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:06 +0200

    reimplement: SHC_3BB0A8C1_0x004AC250 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/TriggerEvent/MenuModalRenderFunction_TriggerEvent.cpp
+++ src/OpenSHC/UI/MenuModals/TriggerEvent/MenuModalRenderFunction_TriggerEvent.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x004AC250
             void MenuModalRenderFunction_TriggerEvent(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(199, 0x17, x, y, width);
             }
