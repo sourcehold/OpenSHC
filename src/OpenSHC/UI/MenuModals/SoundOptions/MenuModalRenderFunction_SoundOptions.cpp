b0ce3586d83c3ad107cbd8bcad64e4b6bc26cdd4
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:05 +0200

    reimplement: SHC_3BB0A8C1_0x00492660 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/SoundOptions/MenuModalRenderFunction_SoundOptions.cpp
+++ src/OpenSHC/UI/MenuModals/SoundOptions/MenuModalRenderFunction_SoundOptions.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x00492660
             void MenuModalRenderFunction_SoundOptions(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(0x4a, 5, x, y, width);
             }
