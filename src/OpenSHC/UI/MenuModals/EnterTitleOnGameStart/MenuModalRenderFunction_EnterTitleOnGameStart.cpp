aa657d67a8d631198f0114e696e64863a29377b5
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:03 +0200

    reimplement: SHC_3BB0A8C1_0x00491BF0 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/EnterTitleOnGameStart/MenuModalRenderFunction_EnterTitleOnGameStart.cpp
+++ src/OpenSHC/UI/MenuModals/EnterTitleOnGameStart/MenuModalRenderFunction_EnterTitleOnGameStart.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x00491BF0
             void MenuModalRenderFunction_EnterTitleOnGameStart(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(5, 6, x, y, width);
             }
