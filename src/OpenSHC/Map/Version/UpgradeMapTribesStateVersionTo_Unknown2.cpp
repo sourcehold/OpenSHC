cbd0f182652c3fc7d18492e37ed41bc435e87dd0
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:43 +0200

    reimplement: SHC_3BB0A8C1_0x00523EB0 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeMapTribesStateVersionTo_Unknown2.cpp
+++ src/OpenSHC/Map/Version/UpgradeMapTribesStateVersionTo_Unknown2.cpp
@@ -9,7 +9,7 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x00523EB0
     void Version::UpgradeMapTribesStateVersionTo_Unknown2()
     {
-        for (int tribeID = 1; tribeID < 1250; tribeID++) {
+        for (int tribeID = 1; tribeID < 1250; ++tribeID) {
             if (DAT_TribesState::instance.tribes[tribeID].tribeState != 0) {
                 MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::setWhetherTribeContainsAnyUnits,
                     DAT_TribesState::ptr)(tribeID);
