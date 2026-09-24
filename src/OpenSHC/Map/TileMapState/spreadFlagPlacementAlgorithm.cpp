10c61dcbc78b06a0e4b25239c7b0750f7421dbfc
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:28 +0200

    reimplement: SHC_3BB0A8C1_0x00501180 100.00% Reimplemented

--- src/OpenSHC/Map/TileMapState/spreadFlagPlacementAlgorithm.cpp
+++ src/OpenSHC/Map/TileMapState/spreadFlagPlacementAlgorithm.cpp
@@ -7,10 +7,10 @@ namespace OpenSHC {
 namespace Map {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x00501180
-    int TileMapState::spreadFlagPlacementAlgorithm(int param_1, uint param_2, uint param_3)
+    int TileMapState::spreadFlagPlacementAlgorithm(int playerID, uint x, uint y)
     {
         return MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::spreadAlgorithmForFlagsAndBraziersUnk,
-            DAT_PathFindingState::ptr)(param_1, param_2, param_3, 4, 0);
+            DAT_PathFindingState::ptr)(playerID, x, y, 4, 0);
     }
 
 }
