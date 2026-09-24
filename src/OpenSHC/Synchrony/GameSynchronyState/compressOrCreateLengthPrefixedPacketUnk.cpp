87132643b973e513147404f09b46006227c00bd5
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:09 +0200

    reimplement: SHC_3BB0A8C1_0x0047E7E0 100.00% Reimplemented

--- src/OpenSHC/Synchrony/GameSynchronyState/compressOrCreateLengthPrefixedPacketUnk.cpp
+++ src/OpenSHC/Synchrony/GameSynchronyState/compressOrCreateLengthPrefixedPacketUnk.cpp
@@ -9,10 +9,7 @@ namespace Synchrony {
     // FUNCTION: STRONGHOLDCRUSADER 0x0047E7E0
     int GameSynchronyState::compressOrCreateLengthPrefixedPacketUnk(int size, char* src, void* dst)
     {
-        int iVar1;
-
-        iVar1 = MACRO_CALL_MEMBER(OpenSHC::IO::DecoderState_Func::compressRLE, DAT_DecoderState::ptr)(src, dst, size);
-        return iVar1;
+        return MACRO_CALL_MEMBER(OpenSHC::IO::DecoderState_Func::compressRLE, DAT_DecoderState::ptr)(src, dst, size);
     }
 
 }
