f26f07bd86c8597de156a34a9294c6f964084e90
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:34 +0200

    reimplement: SHC_3BB0A8C1_0x0045AF00 100.00% Reimplemented

--- src/OpenSHC/Game/GameStateStructures/resetSomethingBuildingRelatedForAllPlayers.cpp
+++ src/OpenSHC/Game/GameStateStructures/resetSomethingBuildingRelatedForAllPlayers.cpp
@@ -6,10 +6,10 @@ namespace Game {
     // FUNCTION: STRONGHOLDCRUSADER 0x0045AF00
     void GameStateStructures::resetSomethingBuildingRelatedForAllPlayers()
     {
-        for (int playerID = 1; playerID < 9; playerID++) {
-            for (int iVar1 = 0; iVar1 < 20; iVar1++) {
+        for (int playerID = 1; playerID < 9; ++playerID) {
+            for (int category = 0; category < 20; ++category) {
                 MACRO_CALL_MEMBER(OpenSHC::Game::GameStateStructures_Func::validateBuildingCategoryReference, this)(
-                    playerID, iVar1);
+                    playerID, category);
             }
         }
     }
