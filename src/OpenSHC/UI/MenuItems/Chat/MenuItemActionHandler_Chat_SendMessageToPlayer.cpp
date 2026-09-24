caaa0c449ad2f6542d01e87f400ccc3ff0b901e4
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:11:55 +0200

    reimplement: SHC_3BB0A8C1_0x0047FFA0 100.00% Reimplemented

--- src/OpenSHC/UI/MenuItems/Chat/MenuItemActionHandler_Chat_SendMessageToPlayer.cpp
+++ src/OpenSHC/UI/MenuItems/Chat/MenuItemActionHandler_Chat_SendMessageToPlayer.cpp
@@ -8,11 +8,11 @@ namespace UI {
         namespace Chat {
 
             // FUNCTION: STRONGHOLDCRUSADER 0x0047FFA0
-            void MenuItemActionHandler_Chat_SendMessageToPlayer(int param_1, ...)
+            void MenuItemActionHandler_Chat_SendMessageToPlayer(int receiver, ...)
             {
-                if (param_1 < 0) {
-                    unsigned int param = -param_1;
-                    DAT_GameSynchronyState::instance.DAT_ChatMessageReceiverArray[param] ^= 1;
+                if (receiver < 0) {
+                    unsigned int const playerID = -receiver;
+                    DAT_GameSynchronyState::instance.DAT_ChatMessageReceiverArray[playerID] ^= 1;
                 }
                 DAT_GameSynchronyState::instance
                     .DAT_ChatMessageReceiverArray[DAT_GameSynchronyState::instance.currentPlayerSlotID] = 1;
