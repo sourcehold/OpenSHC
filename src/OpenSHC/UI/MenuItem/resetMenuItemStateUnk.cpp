3af54bece902884c2dd1999d8665fef993e98d6b
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:18 +0200

    reimplement: SHC_3BB0A8C1_0x004F4C30 100.00% Reimplemented

--- src/OpenSHC/UI/MenuItem/resetMenuItemStateUnk.cpp
+++ src/OpenSHC/UI/MenuItem/resetMenuItemStateUnk.cpp
@@ -11,16 +11,14 @@ namespace UI {
     // FUNCTION: STRONGHOLDCRUSADER 0x004F4C30
     void MenuItem::resetMenuItemStateUnk()
     {
-        MenuItemTypeInt MVar1;
-
         this->hovering = 0;
         this->clicked = 0;
-        (this->secondItemTypeData).buttonState.someTimestamp_1_0x0 = 0;
-        (this->secondItemTypeData).buttonState.clickTimestamp_0x4 = 0;
-        (this->secondItemTypeData).buttonState.countTo100 = 0;
-        MVar1 = this->menuItemType & Enums::MIT_MENU_ITEM_TYPE_ID_PARTUnk;
-        if ((MVar1 == OpenSHC::UI::Enums::MIT_SLIDERUnk) || (MVar1 == OpenSHC::UI::Enums::MIT_SCROLLBARUnk)) {
-            (this->secondItemTypeData).buttonState.currentButtonPictureInGm_0xc = -1000;
+        this->secondItemTypeData.buttonState.someTimestamp_1_0x0 = 0;
+        this->secondItemTypeData.buttonState.clickTimestamp_0x4 = 0;
+        this->secondItemTypeData.buttonState.countTo100 = 0;
+        MenuItemTypeInt const itemType = this->menuItemType & Enums::MIT_MENU_ITEM_TYPE_ID_PARTUnk;
+        if (itemType == OpenSHC::UI::Enums::MIT_SLIDERUnk || itemType == OpenSHC::UI::Enums::MIT_SCROLLBARUnk) {
+            this->secondItemTypeData.buttonState.currentButtonPictureInGm_0xc = -1000;
         }
     }
 
