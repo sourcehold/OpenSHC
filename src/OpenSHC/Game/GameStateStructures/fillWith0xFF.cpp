4852220e0076bd6a2570cb75e4e2b9e2b20dc51e
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:23 +0200

    reimplement: SHC_3BB0A8C1_0x00459BE0 100.00% Reimplemented

--- src/OpenSHC/Game/GameStateStructures/fillWith0xFF.cpp
+++ src/OpenSHC/Game/GameStateStructures/fillWith0xFF.cpp
@@ -10,11 +10,10 @@ namespace Game {
     // FUNCTION: STRONGHOLDCRUSADER 0x00459BE0
     void GameStateStructures::fillWith0xFF()
     {
-        // fixme: This could have just been a memset(-1) to the whole region
-        for (int hotkeyEntry = 0; hotkeyEntry < 10; hotkeyEntry++) {
-            for (int iVar2 = 0; iVar2 < 2500; iVar2++) {
-                this->hotkeyTribes[hotkeyEntry].units[iVar2].id = -1;
-                this->hotkeyTribes[hotkeyEntry].units[iVar2].uid = -1;
+        for (int hotkeyID = 0; hotkeyID < 10; ++hotkeyID) {
+            for (int i = 0; i < 2500; ++i) {
+                this->hotkeyTribes[hotkeyID].units[i].id = -1;
+                this->hotkeyTribes[hotkeyID].units[i].uid = -1;
             }
         }
     }
