7d88a75e9ec62d7b5688f1713545ceafd0769ac3
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:42 +0200

    reimplement: SHC_3BB0A8C1_0x0041A0A0 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeFirst9Buildings.cpp
+++ src/OpenSHC/Map/Version/UpgradeFirst9Buildings.cpp
@@ -9,7 +9,7 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x0041A0A0
     void Version::UpgradeFirst9Buildings()
     {
-        for (int buildingID = 1; buildingID < 9; buildingID++) {
+        for (int buildingID = 1; buildingID < 9; ++buildingID) {
             MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupBarracksCampgroundPositions,
                 DAT_BuildingsState::ptr)(buildingID);
             MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::setupMercenaryPostCampgroundPositions,
