2e59405f8465046a92968ab39c4edb6cc955dd4b
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:10 +0200

    reimplement: SHC_3BB0A8C1_0x00490340 100.00% Reimplemented

--- src/OpenSHC/Synchrony/GameSynchronyState/throttledMultiplayerSyncUpdate.cpp
+++ src/OpenSHC/Synchrony/GameSynchronyState/throttledMultiplayerSyncUpdate.cpp
@@ -1,6 +1,8 @@
 #include "OpenSHC/Synchrony/GameSynchronyState.func.hpp"
 #include "OpenSHC/Game/GameMode.hpp"
 
+#include "OpenSHC/Globals/DAT_GameSynchronyState.hpp"
+
 namespace OpenSHC {
 namespace Synchrony {
 
@@ -9,13 +11,14 @@ namespace Synchrony {
     // FUNCTION: STRONGHOLDCRUSADER 0x00490340
     void GameSynchronyState::throttledMultiplayerSyncUpdate()
     {
-        if ((this->currentGameMode != OpenSHC::Game::GM_SOLITARY)
-            && (this->currentGameMode != OpenSHC::Game::GM_SKIRMISH_SINGLE_PLAYER)) {
-            DWORD _now = timeGetTime();
-            if (10 < _now - this->field309_0x109e94) {
-                this->field309_0x109e94 = _now;
-                MACRO_CALL_MEMBER(OpenSHC::Synchrony::GameSynchronyState_Func::sendPeriodicSyncMessages, this)();
-            }
+        if (DAT_GameSynchronyState::instance.currentGameMode == OpenSHC::Game::GM_SOLITARY
+            || DAT_GameSynchronyState::instance.currentGameMode == OpenSHC::Game::GM_SKIRMISH_SINGLE_PLAYER) {
+            return;
+        }
+        DWORD const now = timeGetTime();
+        if (now - this->field309_0x109e94 > 10) {
+            this->field309_0x109e94 = now;
+            MACRO_CALL_MEMBER(OpenSHC::Synchrony::GameSynchronyState_Func::sendPeriodicSyncMessages, this)();
         }
     }
 
