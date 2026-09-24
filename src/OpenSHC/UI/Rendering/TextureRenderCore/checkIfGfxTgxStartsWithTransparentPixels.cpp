1d1e4d5aaea84b34e8748a4e09a516d39008be1b
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:11 +0200

    reimplement: SHC_3BB0A8C1_0x004549C0 40.00% Register allocation differs

--- src/OpenSHC/UI/Rendering/TextureRenderCore/checkIfGfxTgxStartsWithTransparentPixels.cpp
+++ src/OpenSHC/UI/Rendering/TextureRenderCore/checkIfGfxTgxStartsWithTransparentPixels.cpp
@@ -12,8 +12,7 @@ namespace UI {
         // FUNCTION: STRONGHOLDCRUSADER 0x004549C0
         BOOLEnum TextureRenderCore::checkIfGfxTgxStartsWithTransparentPixels(int gfxIndex)
         {
-            // fixme: compiler picks different registers
-            return (*(byte*)(this->loadedGfxArray[gfxIndex].offsetInBuffer + 8 + (int)this->gmAndGfxImageDataBuffer)
+            return (((byte*)this->gmAndGfxImageDataBuffer)[this->loadedGfxArray[gfxIndex].offsetInBuffer + 8]
                        & OpenSHC::IO::Graphics::TT_TGX_PIXEL_HEADER)
                 == OpenSHC::IO::Graphics::TT_TRANSPARENT_PIXELS;
         }
