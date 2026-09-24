25d7356ba69a7ff570b29e21bea641b25d0c512e
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:11 +0200

    reimplement: SHC_3BB0A8C1_0x00454990 100.00% Reimplemented

--- src/OpenSHC/UI/Rendering/TextureRenderCore/setRenderingRect.cpp
+++ src/OpenSHC/UI/Rendering/TextureRenderCore/setRenderingRect.cpp
@@ -5,12 +5,12 @@ namespace UI {
     namespace Rendering {
 
         // FUNCTION: STRONGHOLDCRUSADER 0x00454990
-        void TextureRenderCore::setRenderingRect(int xPos, int yPos, int width, int heigth)
+        void TextureRenderCore::setRenderingRect(int xPos, int yPos, int width, int height)
         {
             this->renderingRect_16c854.left = xPos;
             this->renderingRect_16c854.top = yPos;
             this->renderingRect_16c854.right = width;
-            this->renderingRect_16c854.bottom = heigth;
+            this->renderingRect_16c854.bottom = height;
         }
 
     }
