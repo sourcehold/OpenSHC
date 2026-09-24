281078ce22d1a1d089da7139eef600f3936264ab
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:41 +0200

    reimplement: SHC_3BB0A8C1_0x004A94E0 100.00% Reimplemented

--- src/OpenSHC/Map/Navigation/PathFindingState/clearAllClimbData.cpp
+++ src/OpenSHC/Map/Navigation/PathFindingState/clearAllClimbData.cpp
@@ -8,8 +8,8 @@ namespace Map {
         void PathFindingState::clearAllClimbData()
         {
             this->numberOfClimbTeleports = 0;
-            for (int iVar1 = 0; iVar1 < 200; iVar1++) {
-                MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::clearClimbData, this)(iVar1);
+            for (int climbID = 0; climbID < 200; ++climbID) {
+                MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::clearClimbData, this)(climbID);
             }
         }
 
