2a904b011912edb7451df1a3905f53447bacb95c
Gynt <Gynt@users.noreply.github.com>
2026-09-24 07:49:49 +0200

    reimplement: SHC_3BB0A8C1_0x0040E990 100.00% Reimplemented

--- src/OpenSHC/Map/Buildings/BuildingsState/getActiveEngineerForBuilding.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/getActiveEngineerForBuilding.cpp
@@ -17,11 +17,19 @@ namespace Map {
         {
             int unitID = DAT_BuildingsState::instance.buildings[buildingID].workerID[0];
             if (DAT_BuildingsState::instance.buildings[buildingID].workerUID[0]
-                    != DAT_UnitsState::instance.units[unitID].uid
-                || DAT_UnitsState::instance.units[unitID].logicalState != OpenSHC::Map::Units::ULS_NORMAL
-                || DAT_UnitsState::instance.units[unitID].unitType != OpenSHC::Map::Units::UT_E_ENGINEER
-                || DAT_UnitsState::instance.units[unitID].dying != 0
-                || DAT_UnitsState::instance.units[unitID].workplaceBuildingID_1 != buildingID) {
+                != DAT_UnitsState::instance.units[unitID].uid) {
+                return 0;
+            }
+            if (DAT_UnitsState::instance.units[unitID].logicalState != OpenSHC::Map::Units::ULS_NORMAL) {
+                return 0;
+            }
+            if (DAT_UnitsState::instance.units[unitID].unitType != OpenSHC::Map::Units::UT_E_ENGINEER) {
+                return 0;
+            }
+            if (DAT_UnitsState::instance.units[unitID].dying != 0) {
+                return 0;
+            }
+            if (DAT_UnitsState::instance.units[unitID].workplaceBuildingID_1 != buildingID) {
                 return 0;
             }
             return DAT_UnitsState::instance.units[unitID].field252_0x3c4 != 0 ? unitID : 0;
