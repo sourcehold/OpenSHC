2d2b08d300d4e7dd76f54f9071e0b424b3696f3b
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:28:18 +0200

    reimplement: SHC_3BB0A8C1_0x0040C5F0 78.57% Reimplemented functionally, orig keeps ecx over calls

--- src/OpenSHC/Map/Buildings/BuildingsState/getBuildingCost.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/getBuildingCost.cpp
@@ -13,23 +13,6 @@ namespace Map {
             *pStone = this->buildingCosts[buildingType].requiredStone_0x4;
             *pGold = this->buildingCosts[buildingType].requiredGold;
             switch (commandBuildingType) {
-            case OpenSHC::Commands::M_MAPPER_KILLING_PIT:
-            case OpenSHC::Commands::M_MAPPER_BRAZIER:
-                *pGold = 5;
-                return;
-            case OpenSHC::Commands::M_MAPPER_PITCH_DITCH:
-                *pGold = 2;
-                return;
-            case OpenSHC::Commands::M_MAPPER_MOAT:
-            case OpenSHC::Commands::M_MAPPER_PEOPLE_LADDERMEN:
-                *pGold = 1;
-                return;
-            case OpenSHC::Commands::M_MAPPER_MANGONEL:
-                *pGold = this->buildingCosts[BT_MANGONEL].requiredGold;
-                return;
-            case OpenSHC::Commands::M_MAPPER_BALLISTA:
-                *pGold = this->buildingCosts[BT_BALLISTA].requiredGold;
-                return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_ARCHERS:
                 *pGold = 12;
                 return;
@@ -46,11 +29,22 @@ namespace Map {
             case OpenSHC::Commands::M_MAPPER_PEOPLE_KNIGHTS:
                 *pGold = 40;
                 return;
+            case OpenSHC::Commands::M_MAPPER_MOAT:
+            case OpenSHC::Commands::M_MAPPER_PEOPLE_LADDERMEN:
+                *pGold = 1;
+                return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_ENGINEERS:
                 *pGold = 30;
                 return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_MONKS:
                 *pGold = 10;
+                return;
+            case OpenSHC::Commands::M_MAPPER_PITCH_DITCH:
+                *pGold = 2;
+                return;
+            case OpenSHC::Commands::M_MAPPER_KILLING_PIT:
+            case OpenSHC::Commands::M_MAPPER_BRAZIER:
+                *pGold = 5;
                 return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_CATAPULTS:
                 *pGold = this->buildingCosts[BT_CATAPULT].requiredGold;
@@ -58,15 +52,21 @@ namespace Map {
             case OpenSHC::Commands::M_MAPPER_PEOPLE_TREBUCHETS:
                 *pGold = this->buildingCosts[BT_TREBUCHET].requiredGold;
                 return;
-            // Note: the battering ram and siege tower costs are swapped (or their building types are)
-            case OpenSHC::Commands::M_MAPPER_PEOPLE_BATTERING_RAMS:
-                *pGold = this->buildingCosts[BT_SIEGETOWER].requiredGold;
+            case OpenSHC::Commands::M_MAPPER_MANGONEL:
+                *pGold = this->buildingCosts[BT_MANGONEL].requiredGold;
+                return;
+            case OpenSHC::Commands::M_MAPPER_BALLISTA:
+                *pGold = this->buildingCosts[BT_BALLISTA].requiredGold;
+                return;
+            case OpenSHC::Commands::M_MAPPER_PEOPLE_PORTABLE_SHIELDS:
+                *pGold = this->buildingCosts[BT_SHIELD].requiredGold;
                 return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_SIEGE_TOWERS:
                 *pGold = this->buildingCosts[BT_BATTERINGRAM].requiredGold;
                 return;
-            case OpenSHC::Commands::M_MAPPER_PEOPLE_PORTABLE_SHIELDS:
-                *pGold = this->buildingCosts[BT_SHIELD].requiredGold;
+            // Note: the battering ram and siege tower costs are swapped (or their building types are)
+            case OpenSHC::Commands::M_MAPPER_PEOPLE_BATTERING_RAMS:
+                *pGold = this->buildingCosts[BT_SIEGETOWER].requiredGold;
                 return;
             case OpenSHC::Commands::M_MAPPER_PEOPLE_ARAB_BALLISTA:
                 *pGold = this->buildingCosts[BT_FIREBALLISTA].requiredGold;
