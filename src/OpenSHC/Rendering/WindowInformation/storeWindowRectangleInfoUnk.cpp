1c9c11d767551744ba2c5ad28ef5fd4cb1b6eff7
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:07 +0200

    reimplement: SHC_3BB0A8C1_0x00467F60 100.00% Reimplemented

--- src/OpenSHC/Rendering/WindowInformation/storeWindowRectangleInfoUnk.cpp
+++ src/OpenSHC/Rendering/WindowInformation/storeWindowRectangleInfoUnk.cpp
@@ -4,12 +4,12 @@ namespace OpenSHC {
 namespace Rendering {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x00467F60
-    void WindowInformation::storeWindowRectangleInfoUnk(LONG param_1, LONG param_2, LONG param_3, LONG param_4)
+    void WindowInformation::storeWindowRectangleInfoUnk(LONG left, LONG right, LONG top, LONG bottom)
     {
-        this->RECT_SomeGameWindowRectUnk.left = param_1;
-        this->RECT_SomeGameWindowRectUnk.top = param_3;
-        this->RECT_SomeGameWindowRectUnk.right = param_2;
-        this->RECT_SomeGameWindowRectUnk.bottom = param_4;
+        this->RECT_SomeGameWindowRectUnk.left = left;
+        this->RECT_SomeGameWindowRectUnk.top = top;
+        this->RECT_SomeGameWindowRectUnk.right = right;
+        this->RECT_SomeGameWindowRectUnk.bottom = bottom;
     }
 
 }
