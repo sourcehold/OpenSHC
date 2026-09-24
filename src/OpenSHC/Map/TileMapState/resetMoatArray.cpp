fdb7467062ae9e54aecdd0087f69993dd792ff85
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:27 +0200

    reimplement: SHC_3BB0A8C1_0x00500680 100.00% Reimplemented

--- src/OpenSHC/Map/TileMapState/resetMoatArray.cpp
+++ src/OpenSHC/Map/TileMapState/resetMoatArray.cpp
@@ -12,9 +12,9 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x00500680
     void TileMapState::resetMoatArray()
     {
-        for (int i = 0; i < 16000; i++) {
+        for (int i = 0; i < 16000; ++i) {
             MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
-                0x10, 0, &this->moats[i]);
+                sizeof(this->moats[i]), 0, &this->moats[i]);
         }
         this->moatTileCount = 0;
     }
