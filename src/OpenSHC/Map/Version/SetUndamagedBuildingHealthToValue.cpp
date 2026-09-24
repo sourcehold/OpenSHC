7ed9fffe2ca43bb2fb408be23569398201bcdcdb
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:40 +0200

    reimplement: SHC_3BB0A8C1_0x0041A060 100.00% Reimplemented

--- src/OpenSHC/Map/Version/SetUndamagedBuildingHealthToValue.cpp
+++ src/OpenSHC/Map/Version/SetUndamagedBuildingHealthToValue.cpp
@@ -14,15 +14,16 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x0041A060
     void Version::SetUndamagedBuildingHealthToValue()
     {
-        for (int _buildingID = 1; _buildingID < 2000; _buildingID += 1) {
-            if ((DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0))
-                && (DAT_BuildingsState::instance.buildings[_buildingID].currentHealth
-                    == DAT_BuildingsState::instance.buildings[_buildingID].maxHealth)) {
-                uint health = DAT_BuildingDefinedData::instance
-                                  .BuildingHP[(short)DAT_BuildingsState::instance.buildings[_buildingID].buildingType]
-                                  .ushortValue;
-                DAT_BuildingsState::instance.buildings[_buildingID].maxHealth = health;
-                DAT_BuildingsState::instance.buildings[_buildingID].currentHealth = health;
+        for (int buildingID = 1; buildingID < 2000; ++buildingID) {
+            if (DAT_BuildingsState::instance.buildings[buildingID].logicalState != (BuildingLogicalState)0
+                && DAT_BuildingsState::instance.buildings[buildingID].currentHealth
+                    == DAT_BuildingsState::instance.buildings[buildingID].maxHealth) {
+                uint const health
+                    = DAT_BuildingDefinedData::instance
+                          .BuildingHP[(short)DAT_BuildingsState::instance.buildings[buildingID].buildingType]
+                          .ushortValue;
+                DAT_BuildingsState::instance.buildings[buildingID].maxHealth = health;
+                DAT_BuildingsState::instance.buildings[buildingID].currentHealth = health;
             }
         }
     }
