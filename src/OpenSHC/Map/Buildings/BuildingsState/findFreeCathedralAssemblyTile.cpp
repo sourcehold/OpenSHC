33a061a2426a844e4a77fa16c548abaa9777730c
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:28:22 +0200

    reimplement: SHC_3BB0A8C1_0x0040E610 90.12% Reimplemented functionally, code layout differs

--- src/OpenSHC/Map/Buildings/BuildingsState/findFreeCathedralAssemblyTile.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/findFreeCathedralAssemblyTile.cpp
@@ -16,41 +16,35 @@ namespace Map {
         undefined4 BuildingsState::findFreeCathedralAssemblyTile(int param_1, int param_2)
         {
             // param_1 is the player, param_2 the unit
-            short pointX = DAT_GameState::instance.playerDataArray[param_1].cathedralAssemblyPointX;
-            if (pointX == 0) {
-                return 0;
-            }
-            int i = DAT_GameState::instance.playerDataArray[param_1].someCount30;
-            short pointY = DAT_GameState::instance.playerDataArray[param_1].cathedralAssemblyPointY;
-            int x;
-            int y;
-            for (;; ++i) {
-                if (i >= 49) {
-                    return 0;
+            if (DAT_GameState::instance.playerDataArray[param_1].cathedralAssemblyPointX != 0) {
+                int pointX = (short)DAT_GameState::instance.playerDataArray[param_1].cathedralAssemblyPointX;
+                int pointY = (short)DAT_GameState::instance.playerDataArray[param_1].cathedralAssemblyPointY;
+                for (int i = (short)DAT_GameState::instance.playerDataArray[param_1].someCount30; i < 49; ++i) {
+                    // Note: the original always uses the same search offset instead of the i-th one
+                    int y = DAT_BuildingDefinedData::instance.SearchRelatedXYOffsets_1[3].y + pointY;
+                    int x = DAT_BuildingDefinedData::instance.SearchRelatedXYOffsets_1[3].x + pointX;
+                    int tile = DAT_ViewportRenderState::instance.translationMatrix[y].addXgetTile + x;
+                    int unitOnTile = (short)DAT_TileMapState::instance.UnitLayer[tile];
+                    if ((unitOnTile == 0 || unitOnTile == param_2
+                            || DAT_UnitsState::instance.units[param_2].movementRelated != 8)
+                        && MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::
+                                                 calculateCanPlayerUnitsNavigateToAreaFromArea,
+                               DAT_PathFindingState::ptr)(param_1,
+                               (short)DAT_TileMapState::instance
+                                   .PathConnectionLayer[DAT_UnitsState::instance.units[param_2].tile],
+                               (short)DAT_TileMapState::instance.PathConnectionLayer[tile], 0)
+                            != 0) {
+                        this->DAT_TempXOffset = x;
+                        this->DAT_TempYOffset = y;
+                        if (x == DAT_UnitsState::instance.units[param_2].x
+                            && y == DAT_UnitsState::instance.units[param_2].y) {
+                            return 0;
+                        }
+                        return 1;
+                    }
                 }
-                // Note: the original always uses the same search offset instead of the i-th one
-                y = DAT_BuildingDefinedData::instance.SearchRelatedXYOffsets_1[3].y + pointY;
-                x = DAT_BuildingDefinedData::instance.SearchRelatedXYOffsets_1[3].x + pointX;
-                int tile = DAT_ViewportRenderState::instance.translationMatrix[y].addXgetTile + x;
-                int unitOnTile = (short)DAT_TileMapState::instance.UnitLayer[tile];
-                if ((unitOnTile == 0 || unitOnTile == param_2
-                        || DAT_UnitsState::instance.units[param_2].movementRelated != 8)
-                    && MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::PathFindingState_Func::
-                                             calculateCanPlayerUnitsNavigateToAreaFromArea,
-                           DAT_PathFindingState::ptr)(param_1,
-                           (short)DAT_TileMapState::instance
-                               .PathConnectionLayer[DAT_UnitsState::instance.units[param_2].tile],
-                           (short)DAT_TileMapState::instance.PathConnectionLayer[tile], 0)
-                        != 0) {
-                    break;
-                }
             }
-            this->DAT_TempXOffset = x;
-            this->DAT_TempYOffset = y;
-            if (x == DAT_UnitsState::instance.units[param_2].x && y == DAT_UnitsState::instance.units[param_2].y) {
-                return 0;
-            }
-            return 1;
+            return 0;
         }
 
     }
