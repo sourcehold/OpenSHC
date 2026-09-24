4288b522517fd0af902874fe1a4642c309d5d693
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:10 +0200

    reimplement: SHC_3BB0A8C1_0x0045D0B0 100.00% Reimplemented

--- src/OpenSHC/UI/Rendering/AlphaAndButtonSurface/freeMemory.cpp
+++ src/OpenSHC/UI/Rendering/AlphaAndButtonSurface/freeMemory.cpp
@@ -8,7 +8,7 @@ namespace UI {
         // FUNCTION: STRONGHOLDCRUSADER 0x0045D0B0
         void AlphaAndButtonSurface::freeMemory()
         {
-            if (this->surfacePtr != (ushort*)0x0) {
+            if (this->surfacePtr != NULL) {
                 MACRO_CALL(OpenSHC::OS_Func::_free_base)(this->surfacePtr);
             }
         }
