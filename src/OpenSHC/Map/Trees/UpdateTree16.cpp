f94cfa54617e12354cc30964ed170d3840d5292e
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:29 +0200

    reimplement: SHC_3BB0A8C1_0x004F28F0 100.00% Reimplemented

--- src/OpenSHC/Map/Trees/UpdateTree16.cpp
+++ src/OpenSHC/Map/Trees/UpdateTree16.cpp
@@ -9,9 +9,9 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x004F28F0
     void Trees::UpdateTree16()
     {
-        int iVar2 = (DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].rng1 & 7U) + 10;
-        DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].animationFrameUnk = iVar2;
-        DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].animationFrame2Unk = iVar2;
+        int const frame = (DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].rng1 & 7U) + 10;
+        DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].animationFrameUnk = frame;
+        DAT_LandscapeState::instance.trees[DAT_CurrentTreeID::instance].animationFrame2Unk = frame;
     }
 
 }
