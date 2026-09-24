a39bb069dab66adc5d249ba49d22ad5107652180
Gynt <Gynt@users.noreply.github.com>
2026-09-24 11:21:06 +0200

    reimplement: SHC_3BB0A8C1_0x0040F460 66.67% Reimplemented functionally, register allocation differs

--- src/OpenSHC/Map/Buildings/BuildingsState/determinePeasantSitPosition.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/determinePeasantSitPosition.cpp
@@ -12,10 +12,14 @@ namespace Map {
         // FUNCTION: STRONGHOLDCRUSADER 0x0040F460
         undefined4 BuildingsState::determinePeasantSitPosition(int campfireID, int availablePeasants)
         {
+            // Matching note: The remaining difference is the register choice for this and the building
+            // coordinates.
             if (availablePeasants >= 24) {
                 availablePeasants = 23;
             }
 
+            int buildingY = (short)DAT_BuildingsState::instance.buildings[campfireID].y;
+            int buildingX = (short)DAT_BuildingsState::instance.buildings[campfireID].x;
             int tile = (&DAT_BuildingsState::instance.buildings[campfireID].tileRef1)[availablePeasants];
             int y = DAT_ViewportRenderState::instance.tileTranslationMatrix_YComponent[tile];
             int x = tile - DAT_ViewportRenderState::instance.translationMatrix[y].addXgetTile;
@@ -23,8 +27,7 @@ namespace Map {
             this->campfireSpotX = x;
             MACRO_CALL_MEMBER(
                 OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::calculatePreferredRelativeOrientation,
-                DAT_DirectionAlgorithmState::ptr)(x, y, (short)DAT_BuildingsState::instance.buildings[campfireID].x + 3,
-                (short)DAT_BuildingsState::instance.buildings[campfireID].y + 3);
+                DAT_DirectionAlgorithmState::ptr)(x, y, buildingX + 3, buildingY + 3);
             this->campfireSpotOrientation = DAT_DirectionAlgorithmState::instance.orientation;
             return 1;
         }
