bf8ad0c5d22a841ede1ffa377209459eac53953d
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:42 +0200

    reimplement: SHC_3BB0A8C1_0x0041A5C0 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeKillingPitField.cpp
+++ src/OpenSHC/Map/Version/UpgradeKillingPitField.cpp
@@ -15,11 +15,11 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x0041A5C0
     void Version::UpgradeKillingPitField()
     {
-        for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
-            if ((DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0))
-                && (DAT_BuildingsState::instance.buildings[_buildingID].buildingType
-                    == OpenSHC::Map::Buildings::BT_KILLINGPIT)) {
-                DAT_BuildingsState::instance.buildings[_buildingID].killingPitField = 0;
+        for (int buildingID = 1; buildingID < 2000; ++buildingID) {
+            if (DAT_BuildingsState::instance.buildings[buildingID].logicalState != (BuildingLogicalState)0
+                && DAT_BuildingsState::instance.buildings[buildingID].buildingType
+                    == OpenSHC::Map::Buildings::BT_KILLINGPIT) {
+                DAT_BuildingsState::instance.buildings[buildingID].killingPitField = 0;
             }
         }
     }
