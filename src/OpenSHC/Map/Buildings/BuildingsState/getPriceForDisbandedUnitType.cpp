69f146dd969e0ed67bd8605ef23d219ed55f3bcd
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:28:18 +0200

    reimplement: SHC_3BB0A8C1_0x0040C800 91.36% Reimplemented functionally, register allocation differs

--- src/OpenSHC/Map/Buildings/BuildingsState/getPriceForDisbandedUnitType.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/getPriceForDisbandedUnitType.cpp
@@ -8,10 +8,6 @@ namespace Map {
         void BuildingsState::getPriceForDisbandedUnitType(UnitType unitType, int* outPrice)
         {
             switch (unitType) {
-            case OpenSHC::Map::Units::UT_TUNNELER:
-            case OpenSHC::Map::Units::UT_E_XBOW:
-                *outPrice = 20;
-                return;
             case OpenSHC::Map::Units::UT_E_ARCHER:
                 *outPrice = 12;
                 return;
@@ -22,6 +18,10 @@ namespace Map {
                 *outPrice = 20;
                 return;
             case OpenSHC::Map::Units::UT_E_MACE:
+                *outPrice = 20;
+                return;
+            case OpenSHC::Map::Units::UT_TUNNELER:
+            case OpenSHC::Map::Units::UT_E_XBOW:
                 *outPrice = 20;
                 return;
             case OpenSHC::Map::Units::UT_E_SWORD:
@@ -48,21 +48,22 @@ namespace Map {
             case OpenSHC::Map::Units::UT_S_MANGONEL:
                 *outPrice = this->buildingCosts[BT_MANGONEL].requiredGold;
                 return;
+            case OpenSHC::Map::Units::UT_S_BALLISTA:
+                *outPrice = this->buildingCosts[BT_BALLISTA].requiredGold;
+                return;
+            case OpenSHC::Map::Units::UT_S_FBALLISTA:
+                *outPrice = this->buildingCosts[BT_FIREBALLISTA].requiredGold;
+                return;
+
+            case OpenSHC::Map::Units::UT_S_SHIELD:
+                *outPrice = this->buildingCosts[BT_SHIELD].requiredGold;
+                return;
             // Note: the battering ram and siege tower costs are swapped (or their building types are)
             case OpenSHC::Map::Units::UT_S_TOWER:
                 *outPrice = this->buildingCosts[BT_BATTERINGRAM].requiredGold;
                 return;
             case OpenSHC::Map::Units::UT_S_BATTERINGRAM:
                 *outPrice = this->buildingCosts[BT_SIEGETOWER].requiredGold;
-                return;
-            case OpenSHC::Map::Units::UT_S_SHIELD:
-                *outPrice = this->buildingCosts[BT_SHIELD].requiredGold;
-                return;
-            case OpenSHC::Map::Units::UT_S_BALLISTA:
-                *outPrice = this->buildingCosts[BT_BALLISTA].requiredGold;
-                return;
-            case OpenSHC::Map::Units::UT_S_FBALLISTA:
-                *outPrice = this->buildingCosts[BT_FIREBALLISTA].requiredGold;
                 return;
             }
         }
