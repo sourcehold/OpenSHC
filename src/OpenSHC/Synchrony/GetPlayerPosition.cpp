9a02c449f6cc9921a9a45d1f10490d44ecac8a37
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:10 +0200

    reimplement: SHC_3BB0A8C1_0x0042AC60 100.00% Reimplemented

--- src/OpenSHC/Synchrony/GetPlayerPosition.cpp
+++ src/OpenSHC/Synchrony/GetPlayerPosition.cpp
@@ -7,9 +7,9 @@ namespace OpenSHC {
 // FUNCTION: STRONGHOLDCRUSADER 0x0042AC60
 int Synchrony::GetPlayerPosition(int playerID)
 {
-    for (int _position = 0; _position < 8; _position++) {
-        if (DAT_GameSynchronyState::instance.playerPositionsArray[_position] + 1 == playerID) {
-            return _position + 1;
+    for (int position = 0; position < 8; ++position) {
+        if (DAT_GameSynchronyState::instance.playerPositionsArray[position] + 1 == playerID) {
+            return position + 1;
         }
     }
     return 0;
