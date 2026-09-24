d063d8fd4436f1cd2496af0bae96c623e6efcdd0
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:18 +0200

    reimplement: SHC_3BB0A8C1_0x004F4C60 100.00% Reimplemented

--- src/OpenSHC/UI/MenuHandlerState/setupBuildMenuState.cpp
+++ src/OpenSHC/UI/MenuHandlerState/setupBuildMenuState.cpp
@@ -7,13 +7,13 @@ namespace UI {
     using OpenSHC::WindowsHelper::Enums::BOOLEnum;
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004F4C60
-    void MenuHandlerState::setupBuildMenuState(dword param_1, dword param_2)
+    void MenuHandlerState::setupBuildMenuState(dword const017, dword const516)
     {
         this->isBuildMenuTransitioning_0x18 = FALSE;
         this->buildMenuBackgroundLeftShift_0x24 = 0;
         this->buildMenuTransitionDirection_0x2c = -1;
-        this->const017 = param_1;
-        this->const516 = param_2;
+        this->const017 = const017;
+        this->const516 = const516;
     }
 
 }
