16ac60ac3a9d596120e1b084c90494d69f6dacb3
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:07 +0200

    reimplement: SHC_3BB0A8C1_0x004D6EF0 100.00% Reimplemented

--- src/OpenSHC/UI/MenuViews/CampaignUnk/MenuView_CampaignUnk_DoEveryFrame.cpp
+++ src/OpenSHC/UI/MenuViews/CampaignUnk/MenuView_CampaignUnk_DoEveryFrame.cpp
@@ -15,18 +15,16 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x004D6EF0
             void MenuView_CampaignUnk_DoEveryFrame()
             {
-                int iVar1;
-
-                iVar1 = MACRO_CALL(OpenSHC::UI::Helpers_Func::TicksSinceCounterStart)();
-                if (iVar1 != 0) {
-                    if (DAT_GameCore::instance.missionNumber1to20 == 1) {
-                        MACRO_CALL_MEMBER(OpenSHC::Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
-                            OpenSHC::UI::Enums::MVT_SCENARIO_DESCRIPTION, 0);
-                        return;
-                    }
-                    MACRO_CALL_MEMBER(OpenSHC::Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
-                        OpenSHC::UI::Enums::MVT_UNKNOWN_26_CAMPAIGN_RELATEDUnk, 0);
+                if (MACRO_CALL(OpenSHC::UI::Helpers_Func::TicksSinceCounterStart)() == 0) {
+                    return;
                 }
+                if (DAT_GameCore::instance.missionNumber1to20 == 1) {
+                    MACRO_CALL_MEMBER(OpenSHC::Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
+                        OpenSHC::UI::Enums::MVT_SCENARIO_DESCRIPTION, 0);
+                    return;
+                }
+                MACRO_CALL_MEMBER(OpenSHC::Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
+                    OpenSHC::UI::Enums::MVT_UNKNOWN_26_CAMPAIGN_RELATEDUnk, 0);
             }
 
         }
