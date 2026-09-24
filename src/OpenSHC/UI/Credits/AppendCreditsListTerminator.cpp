4e73877e26cde3484ab283e7f6e00ce792d74a4a
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:00:11 +0200

    reimplement: SHC_3BB0A8C1_0x004DA810 100.00% Reimplemented

--- src/OpenSHC/UI/Credits/AppendCreditsListTerminator.cpp
+++ src/OpenSHC/UI/Credits/AppendCreditsListTerminator.cpp
@@ -10,9 +10,9 @@ namespace UI {
         // FUNCTION: STRONGHOLDCRUSADER 0x004DA810
         void AppendCreditsListTerminator()
         {
-            if (DAT_UnknownBinkCount::instance < 288) {
+            if ((int)DAT_UnknownBinkCount::instance < 288) {
                 DAT_ARRAY_00eb9b68::instance[DAT_UnknownBinkCount::instance].field0_0x0 = 0x20;
-                DAT_UnknownBinkCount::instance = DAT_UnknownBinkCount::instance + 1;
+                DAT_UnknownBinkCount::instance += 1;
             }
         }
 
