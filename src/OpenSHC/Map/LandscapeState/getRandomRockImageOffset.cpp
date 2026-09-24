198b1d9f784c8593a4d0d0c91d0e480fc73c1b65
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:38 +0200

    reimplement: SHC_3BB0A8C1_0x004F2A10 100.00% Reimplemented

--- src/OpenSHC/Map/LandscapeState/getRandomRockImageOffset.cpp
+++ src/OpenSHC/Map/LandscapeState/getRandomRockImageOffset.cpp
@@ -6,9 +6,9 @@ namespace OpenSHC {
 namespace Map {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x004F2A10
-    int LandscapeState::getRandomRockImageOffset(int param_1)
+    int LandscapeState::getRandomRockImageOffset(int index)
     {
-        return DAT_OrganismDefinedData::instance.RandomRockImageGMImageOffsets[param_1];
+        return DAT_OrganismDefinedData::instance.RandomRockImageGMImageOffsets[index];
     }
 
 }
