e1d8a1313da91c81d25990ea5f487b058be35bce
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:29:16 +0200

    reimplement: SHC_3BB0A8C1_0x0041C310 43.73% Reimplemented functionally, code layout differs

--- src/OpenSHC/Map/Buildings/BuildingsState/processResourceGain.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/processResourceGain.cpp
@@ -26,73 +26,72 @@ namespace Map {
                 return FALSE;
             }
 
-            if (amount > 0) {
-                BuildingType storageType = MACRO_CALL_MEMBER(
-                    OpenSHC::Map::Buildings::BuildingsState_Func::getBuildingStorageTypeForResourceType, this)(
-                    resourceType);
-                if (storageType != BT_MANORHOUSE) {
-                    if (storageType == BT_ARMORY) {
-                        MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToArmory, this)(
-                            resourceType, playerID, amount);
+            if (amount <= 0) {
+                return TRUE;
+            }
+            BuildingType storageType
+                = MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::getBuildingStorageTypeForResourceType,
+                    this)(resourceType);
+            if (storageType == BT_MANORHOUSE) {
+                DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] += amount;
+                return TRUE;
+            }
+            if (storageType == BT_ARMORY) {
+                MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToArmory, this)(
+                    resourceType, playerID, amount);
+                return TRUE;
+            }
+            if (storageType == BT_GRANARY) {
+                MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToGranary, this)(
+                    resourceType, playerID, amount);
+                MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(playerID);
+                return TRUE;
+            }
+
+            // Fill up the stockpiles that already store this resource first. Note: the original checks
+            // currentNumberOfResource twice.
+            for (int i = 1; i < this->maxBuildingsCount; ++i) {
+                if (this->buildings[i].logicalState != BLS_NORMAL || this->buildings[i].owner != playerID
+                    || this->buildings[i].buildingType != storageType || this->buildings[i].currentNumberOfResource == 0
+                    || this->buildings[i].resources[resourceType] == 0
+                    || this->buildings[i].currentNumberOfResource == 0
+                    || this->buildings[i].resources[resourceType]
+                        >= DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]) {
+                    continue;
+                }
+                int space = DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]
+                    - this->buildings[i].resources[resourceType];
+                for (int n = 0; n < space; ++n) {
+                    MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToStockpile,
+                        DAT_BuildingsState::ptr)(i, this->buildings[i].uid, resourceType, 1,
+                        DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType], 1);
+                    --amount;
+                    if (amount <= 0) {
                         return TRUE;
-                    }
-                    if (storageType == BT_GRANARY) {
-                        MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToGranary, this)(
-                            resourceType, playerID, amount);
-                        MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(
-                            playerID);
-                        return TRUE;
-                    }
-                    {
-                        // Fill up the stockpiles that already store this resource first
-                        for (int i = 1; i < this->maxBuildingsCount; ++i) {
-                            if (this->buildings[i].logicalState != BLS_NORMAL || this->buildings[i].owner != playerID
-                                || this->buildings[i].buildingType != storageType
-                                || this->buildings[i].currentNumberOfResource == 0
-                                || this->buildings[i].resources[resourceType] == 0
-                                || this->buildings[i].resources[resourceType]
-                                    >= DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]) {
-                                continue;
-                            }
-                            int space = DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]
-                                - this->buildings[i].resources[resourceType];
-                            for (int n = 0; n < space; ++n) {
-                                MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToStockpile,
-                                    DAT_BuildingsState::ptr)(i, this->buildings[i].uid, resourceType, 1,
-                                    DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType], 1);
-                                --amount;
-                                if (amount <= 0) {
-                                    return TRUE;
-                                }
-                            }
-                        }
-                        // Then continue with the empty ones
-                        for (int i = 1; i < this->maxBuildingsCount; ++i) {
-                            if (this->buildings[i].logicalState != BLS_NORMAL || this->buildings[i].owner != playerID
-                                || this->buildings[i].buildingType != storageType
-                                || this->buildings[i].currentNumberOfResource != 0) {
-                                continue;
-                            }
-                            int space = DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]
-                                - this->buildings[i].resources[resourceType];
-                            for (int n = 0; n < space; ++n) {
-                                MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToStockpile,
-                                    DAT_BuildingsState::ptr)(i, this->buildings[i].uid, resourceType, 1,
-                                    DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType], 1);
-                                --amount;
-                                if (amount <= 0) {
-                                    return TRUE;
-                                }
-                            }
-                        }
-                        MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(
-                            playerID);
-                        return FALSE;
                     }
                 }
             }
-            DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] += amount;
-            return TRUE;
+            // Then continue with the empty ones
+            for (int i = 1; i < this->maxBuildingsCount; ++i) {
+                if (this->buildings[i].logicalState != BLS_NORMAL || this->buildings[i].owner != playerID
+                    || this->buildings[i].buildingType != storageType
+                    || this->buildings[i].currentNumberOfResource != 0) {
+                    continue;
+                }
+                int space = DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType]
+                    - this->buildings[i].resources[resourceType];
+                for (int n = 0; n < space; ++n) {
+                    MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::addResourceToStockpile,
+                        DAT_BuildingsState::ptr)(i, this->buildings[i].uid, resourceType, 1,
+                        DAT_BuildingDefinedData::instance.StorageLimitResourceTypeArray[resourceType], 1);
+                    --amount;
+                    if (amount <= 0) {
+                        return TRUE;
+                    }
+                }
+            }
+            MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(playerID);
+            return FALSE;
         }
 
     }
