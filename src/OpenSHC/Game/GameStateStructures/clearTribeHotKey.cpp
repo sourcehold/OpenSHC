0ad0dbf7a9994a76d13737bb7a71bb09e89d20dd
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:23 +0200

    reimplement: SHC_3BB0A8C1_0x00459BB0 100.00% Reimplemented

--- src/OpenSHC/Game/GameStateStructures/clearTribeHotKey.cpp
+++ src/OpenSHC/Game/GameStateStructures/clearTribeHotKey.cpp
@@ -10,9 +10,9 @@ namespace Game {
     // FUNCTION: STRONGHOLDCRUSADER 0x00459BB0
     void GameStateStructures::clearTribeHotKey(int hotkeyID)
     {
-        for (int iVar2 = 0; iVar2 < 2500; iVar2++) {
-            this->hotkeyTribes[hotkeyID].units[iVar2].id = -1;
-            this->hotkeyTribes[hotkeyID].units[iVar2].uid = -1;
+        for (int i = 0; i < 2500; ++i) {
+            this->hotkeyTribes[hotkeyID].units[i].id = -1;
+            this->hotkeyTribes[hotkeyID].units[i].uid = -1;
         }
     }
 
