df43495e5ecb485cca272ddd9f9b059089ff280b
Gynt <Gynt@users.noreply.github.com>
2026-09-24 07:31:29 +0200

    reimplement: SHC_3BB0A8C1_0x0040E740 100.00% Reimplemented

--- src/OpenSHC/Map/Buildings/BuildingsState/setTileRefsForOilSmelter.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/setTileRefsForOilSmelter.cpp
@@ -13,7 +13,7 @@ namespace Map {
             for (int i = 0; i < 16; ++i) {
                 uint x = DAT_BuildingDefinedData::instance.field412_0xa00c[i].x + (short)this->buildings[buildingID].x;
                 uint y = DAT_BuildingDefinedData::instance.field412_0xa00c[i].y + (short)this->buildings[buildingID].y;
-                if (x < 400 && y < 400
+                if (x <= 399 && y <= 399
                     && DAT_ViewportRenderState::instance.DAT_BinaryTileMap400x400[x + y * 400] != 0) {
                     (&this->buildings[buildingID].tileRef1)[i]
                         = DAT_ViewportRenderState::instance.translationMatrix[y].addXgetTile + x;
