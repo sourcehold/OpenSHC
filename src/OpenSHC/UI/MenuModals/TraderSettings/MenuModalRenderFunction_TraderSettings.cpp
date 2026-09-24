7641ffa0f72c523f67864bfd6d1a6afe19dcbac5
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:06 +0200

    reimplement: SHC_3BB0A8C1_0x004AB4B0 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/TraderSettings/MenuModalRenderFunction_TraderSettings.cpp
+++ src/OpenSHC/UI/MenuModals/TraderSettings/MenuModalRenderFunction_TraderSettings.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x004AB4B0
             void MenuModalRenderFunction_TraderSettings(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(199, 0x26, x, y, width);
             }
