f269f1838452be6f3375a93ce8e3e66dd70374e4
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:28:02 +0200

    reimplement: SHC_3BB0A8C1_0x0040B980 93.48% Reimplemented functionally, code layout differs

--- src/OpenSHC/Map/Buildings/BuildingsState/hasHumanPlayerUnitsOnBuilding.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/hasHumanPlayerUnitsOnBuilding.cpp
@@ -16,21 +16,18 @@ namespace Map {
         BOOLEnum BuildingsState::hasHumanPlayerUnitsOnBuilding(int buildingID)
         {
             int size = this->buildings[buildingID].widthOrHeight;
-            if (size > 0) {
-                int x = (short)this->buildings[buildingID].x;
-                int y = (short)this->buildings[buildingID].y;
-                for (int row = 0; row < size; ++row) {
-                    for (int column = 0; column < size; ++column) {
-                        int unitID
-                            = (short)DAT_TileMapState::instance
-                                  .UnitLayer[DAT_ViewportRenderState::instance.translationMatrix[y + row].addXgetTile
-                                      + x + column];
-                        if (unitID != 0
-                            && DAT_GameSynchronyState::instance
-                                    .currentPlayerFullIDArray[DAT_UnitsState::instance.units[unitID].owner]
-                                != -1) {
-                            return TRUE;
-                        }
+            int x = (short)this->buildings[buildingID].x;
+            int y = (short)this->buildings[buildingID].y;
+            for (int row = 0; row < size; ++row) {
+                for (int column = 0; column < size; ++column) {
+                    int unitID = (short)DAT_TileMapState::instance
+                                     .UnitLayer[DAT_ViewportRenderState::instance.translationMatrix[y + row].addXgetTile
+                                         + x + column];
+                    if (unitID != 0
+                        && DAT_GameSynchronyState::instance
+                                .currentPlayerFullIDArray[DAT_UnitsState::instance.units[unitID].owner]
+                            != -1) {
+                        return TRUE;
                     }
                 }
             }
