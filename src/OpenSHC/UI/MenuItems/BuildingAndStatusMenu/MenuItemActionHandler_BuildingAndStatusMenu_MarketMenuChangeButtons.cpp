5966cc90121d6af95a09fc108199c0d7997aac0c
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:11:54 +0200

    reimplement: SHC_3BB0A8C1_0x00465920 100.00% Reimplemented

--- src/OpenSHC/UI/MenuItems/BuildingAndStatusMenu/MenuItemActionHandler_BuildingAndStatusMenu_MarketMenuChangeButtons.cpp
+++ src/OpenSHC/UI/MenuItems/BuildingAndStatusMenu/MenuItemActionHandler_BuildingAndStatusMenu_MarketMenuChangeButtons.cpp
@@ -12,12 +12,12 @@ namespace UI {
             using OpenSHC::UI::Enums::MenuViewType;
 
             // FUNCTION: STRONGHOLDCRUSADER 0x00465920
-            void MenuItemActionHandler_BuildingAndStatusMenu_MarketMenuChangeButtons(int param_1, ...)
+            void MenuItemActionHandler_BuildingAndStatusMenu_MarketMenuChangeButtons(int tabType, ...)
             {
-                DAT_GameCore::instance.buildingandstatusmenuMenuTabToSwitchTo = param_1;
+                DAT_GameCore::instance.buildingandstatusmenuMenuTabToSwitchTo = tabType;
                 MACRO_CALL_MEMBER(OpenSHC::Game::GameCore_Func::switchToMenuView, DAT_GameCore::ptr)(
                     OpenSHC::UI::Enums::MVT_BUILDING_AND_STATUS_MENU, 0);
-                DAT_GameCore::instance.field61_0x100 = param_1;
+                DAT_GameCore::instance.field61_0x100 = tabType;
             }
 
         }
