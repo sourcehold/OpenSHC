3eaf87d58111bad728c9356616982bf5c999407a
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:42 +0200

    reimplement: SHC_3BB0A8C1_0x005011C0 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeMapLogicToVersion_125.cpp
+++ src/OpenSHC/Map/Version/UpgradeMapLogicToVersion_125.cpp
@@ -8,11 +8,11 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x005011C0
     void Version::UpgradeMapLogicToVersion_125()
     {
-        for (int i = 0; i < 80400; i++) {
-            if (DAT_TileMapState::instance.HeightLayer[i] == 80) {
-                DAT_TileMapState::instance.Logic2Layer[i] = 4;
-            } else if (DAT_TileMapState::instance.HeightLayer[i] == 130) {
-                DAT_TileMapState::instance.Logic2Layer[i] = 8;
+        for (int tile = 0; tile < 80400; ++tile) {
+            if (DAT_TileMapState::instance.HeightLayer[tile] == 80) {
+                DAT_TileMapState::instance.Logic2Layer[tile] = 4;
+            } else if (DAT_TileMapState::instance.HeightLayer[tile] == 130) {
+                DAT_TileMapState::instance.Logic2Layer[tile] = 8;
             }
         }
     }
