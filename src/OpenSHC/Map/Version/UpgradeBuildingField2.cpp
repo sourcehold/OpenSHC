7727174f795dacee916803bfa5aa503686abeaa4
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:41 +0200

    reimplement: SHC_3BB0A8C1_0x0041A3C0 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeBuildingField2.cpp
+++ src/OpenSHC/Map/Version/UpgradeBuildingField2.cpp
@@ -13,10 +13,10 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x0041A3C0
     void Version::UpgradeBuildingField2()
     {
-        for (int _buildingID = 1; _buildingID < 2000; _buildingID++) {
-            if (DAT_BuildingsState::instance.buildings[_buildingID].logicalState != ((BuildingLogicalState)0)) {
-                DAT_BuildingsState::instance.buildings[_buildingID].field206_0x288 = 0;
-                DAT_BuildingsState::instance.buildings[_buildingID].field188_0x264 = 0;
+        for (int buildingID = 1; buildingID < 2000; ++buildingID) {
+            if (DAT_BuildingsState::instance.buildings[buildingID].logicalState != (BuildingLogicalState)0) {
+                DAT_BuildingsState::instance.buildings[buildingID].field206_0x288 = 0;
+                DAT_BuildingsState::instance.buildings[buildingID].field188_0x264 = 0;
             }
         }
     }
