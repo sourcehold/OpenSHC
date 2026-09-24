bc392ef95b5dfd1fe5ea4fcd61edb106f329fc98
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:11:58 +0200

    reimplement: SHC_3BB0A8C1_0x00433230 100.00% Reimplemented

--- src/OpenSHC/UI/MenuItems/InGameMenu/MenuItemActionHandler_InGameMenu_UnknownBinkRelated.cpp
+++ src/OpenSHC/UI/MenuItems/InGameMenu/MenuItemActionHandler_InGameMenu_UnknownBinkRelated.cpp
@@ -11,12 +11,13 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x00433230
             void MenuItemActionHandler_InGameMenu_UnknownBinkRelated(int param_1, ...)
             {
-                if (DAT_GameCore::instance.isBinkVideoPlaying != 0) {
-                    if (DAT_BinkControlState::instance.binkObjPtrArray[1] == NULL) {
-                        DAT_GameCore::instance.isBinkVideoPlaying = 0;
-                    }
-                    DAT_GameCore::instance.countdown = 1;
+                if (DAT_GameCore::instance.isBinkVideoPlaying == 0) {
+                    return;
                 }
+                if (DAT_BinkControlState::instance.binkObjPtrArray[1] == NULL) {
+                    DAT_GameCore::instance.isBinkVideoPlaying = 0;
+                }
+                DAT_GameCore::instance.countdown = 1;
             }
 
         }
