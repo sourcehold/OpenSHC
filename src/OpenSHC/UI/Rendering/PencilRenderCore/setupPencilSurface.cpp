61763ae8fa88cb7605db571aa0ea25042f5fd20a
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:10 +0200

    reimplement: SHC_3BB0A8C1_0x00468C50 100.00% Reimplemented

--- src/OpenSHC/UI/Rendering/PencilRenderCore/setupPencilSurface.cpp
+++ src/OpenSHC/UI/Rendering/PencilRenderCore/setupPencilSurface.cpp
@@ -12,12 +12,11 @@ namespace UI {
         // FUNCTION: STRONGHOLDCRUSADER 0x00468C50
         void PencilRenderCore::setupPencilSurface()
         {
-            Ghidra::ushort* surface = DAT_WindowAndDirectDraw::instance.surfacePointer_mapGame;
+            ushort* surface = DAT_WindowAndDirectDraw::instance.surfacePointer_mapGame;
             if (this->surfaceTarget == OpenSHC::Rendering::Enums::RT_SCREEN_MENU) {
                 surface = DAT_WindowAndDirectDraw::instance.surfacePointer_screenMenu;
             }
             this->surfacePtr = surface;
-
             if (this->surfaceTarget != OpenSHC::Rendering::Enums::RT_SCREEN_MENU) {
                 this->horizontalByteSize = 8112;
                 return;
