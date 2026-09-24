cb76c5bc8ff26acef6bccbe3a469bba7449cee6d
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:41 +0200

    reimplement: SHC_3BB0A8C1_0x0041A620 100.00% Reimplemented

--- src/OpenSHC/Map/Version/UpgradeClearBuildings1000AndHigher.cpp
+++ src/OpenSHC/Map/Version/UpgradeClearBuildings1000AndHigher.cpp
@@ -13,9 +13,10 @@ namespace Map {
     // FUNCTION: STRONGHOLDCRUSADER 0x0041A620
     void Version::UpgradeClearBuildings1000AndHigher()
     {
-        for (int _buildingID = 1000; _buildingID < 2000; _buildingID++) {
+        for (int buildingID = 1000; buildingID < 2000; ++buildingID) {
             MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
-                0x32c, '\0', &DAT_BuildingsState::instance.buildings[_buildingID]);
+                sizeof(DAT_BuildingsState::instance.buildings[buildingID]), 0,
+                &DAT_BuildingsState::instance.buildings[buildingID]);
         }
     }
 
