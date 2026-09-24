a06fb92ce4d4a7861cb8b361c1182b6a5144de27
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:11:53 +0200

    reimplement: SHC_3BB0A8C1_0x00439390 100.00% Reimplemented

--- src/OpenSHC/UI/MenuItems/BuildMenu/MenuItemRenderFunction_BuildMenu_CurrentlySelectedTroopsDeselectHelper.cpp
+++ src/OpenSHC/UI/MenuItems/BuildMenu/MenuItemRenderFunction_BuildMenu_CurrentlySelectedTroopsDeselectHelper.cpp
@@ -9,11 +9,14 @@ namespace UI {
         namespace BuildMenu {
 
             // FUNCTION: STRONGHOLDCRUSADER 0x00439390
-            void MenuItemRenderFunction_BuildMenu_CurrentlySelectedTroopsDeselectHelper(int param_1, ...)
+            void MenuItemRenderFunction_BuildMenu_CurrentlySelectedTroopsDeselectHelper(int selectionIndex, ...)
             {
-                if ((DAT_UnitsState::instance.nHasOwnedUnitInSelection == 0)
-                    || (DAT_ButtonUnknownZero::instance = 0,
-                        ((int*)(&DAT_UnitsState::instance.selectionEngineers))[param_1] == -1)) {
+                if (DAT_UnitsState::instance.nHasOwnedUnitInSelection == 0) {
+                    DAT_ButtonUnknownZero::instance = 1;
+                    return;
+                }
+                DAT_ButtonUnknownZero::instance = 0;
+                if ((&DAT_UnitsState::instance.selectionEngineers)[selectionIndex] == -1) {
                     DAT_ButtonUnknownZero::instance = 1;
                 }
             }
