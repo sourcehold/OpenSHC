0ee2b571499ba0331e8fdd57fbe0a149d5f50a6c
Gynt <Gynt@users.noreply.github.com>
2026-09-24 11:21:29 +0200

    reimplement: SHC_3BB0A8C1_0x00410320 52.94% Reimplemented functionally, this spilled in original

--- src/OpenSHC/Map/Buildings/BuildingsState/updateRepairCostAndReturnIfDamaged.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/updateRepairCostAndReturnIfDamaged.cpp
@@ -1,6 +1,8 @@
 #include "OpenSHC/Map/Buildings/BuildingsState.func.hpp"
 #include "OpenSHC/WindowsHelper/Enums/BOOLEnum.hpp"
 
+#include "OpenSHC/Globals/DAT_BuildingsState.hpp"
+
 namespace OpenSHC {
 namespace Map {
     namespace Buildings {
@@ -10,37 +12,43 @@ namespace Map {
         // FUNCTION: STRONGHOLDCRUSADER 0x00410320
         BOOLEnum BuildingsState::updateRepairCostAndReturnIfDamaged(int buildingIndex)
         {
+            // Matching note: The original spills this to the stack and keeps the stone costs in a
+            // register; ours keeps this in a register and spills the stone costs. Not reproduced from source.
             if (this->buildings[buildingIndex].currentHealth == this->buildings[buildingIndex].maxHealth) {
                 return FALSE;
             }
 
             // The repair costs are the share of the building costs matching the missing health, at least 1
-            int stone = this->buildingCosts[this->buildings[buildingIndex].buildingType].requiredStone_0x4;
-            int wood = this->buildingCosts[this->buildings[buildingIndex].buildingType].requiredWood;
-            if (wood == 0) {
-                this->INT_SelectedBuildingStoneWoodCost = 0;
-            } else if ((this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * wood
-                    / this->buildings[buildingIndex].maxHealth
-                < 1) {
-                this->INT_SelectedBuildingStoneWoodCost = 1;
+            int stone = DAT_BuildingsState::instance.buildingCosts[this->buildings[buildingIndex].buildingType]
+                            .requiredStone_0x4;
+            int wood
+                = DAT_BuildingsState::instance.buildingCosts[this->buildings[buildingIndex].buildingType].requiredWood;
+            if (wood != 0) {
+                if ((this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * wood
+                        / this->buildings[buildingIndex].maxHealth
+                    < 1) {
+                    this->INT_SelectedBuildingStoneWoodCost = 1;
+                } else {
+                    this->INT_SelectedBuildingStoneWoodCost
+                        = (this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth)
+                        * wood / this->buildings[buildingIndex].maxHealth;
+                }
             } else {
-                this->INT_SelectedBuildingStoneWoodCost
-                    = (this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * wood
+                this->INT_SelectedBuildingStoneWoodCost = 0;
+            }
+            if (stone != 0) {
+                if ((this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * stone
+                        / this->buildings[buildingIndex].maxHealth
+                    < 1) {
+                    this->INT_SelectedBuildingStoneRepairCost = 1;
+                    return TRUE;
+                }
+                this->INT_SelectedBuildingStoneRepairCost
+                    = (this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * stone
                     / this->buildings[buildingIndex].maxHealth;
-            }
-            if (stone == 0) {
-                this->INT_SelectedBuildingStoneRepairCost = 0;
                 return TRUE;
             }
-            if ((this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * stone
-                    / this->buildings[buildingIndex].maxHealth
-                < 1) {
-                this->INT_SelectedBuildingStoneRepairCost = 1;
-                return TRUE;
-            }
-            this->INT_SelectedBuildingStoneRepairCost
-                = (this->buildings[buildingIndex].maxHealth - this->buildings[buildingIndex].currentHealth) * stone
-                / this->buildings[buildingIndex].maxHealth;
+            this->INT_SelectedBuildingStoneRepairCost = 0;
             return TRUE;
         }
 
