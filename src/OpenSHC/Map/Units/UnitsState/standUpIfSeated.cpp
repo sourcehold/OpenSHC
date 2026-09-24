5cd9923bd57fe55398ea18d507a6bb52d9064301
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:38 +0200

    reimplement: SHC_3BB0A8C1_0x0052FBB0 100.00% Reimplemented

--- src/OpenSHC/Map/Units/UnitsState/standUpIfSeated.cpp
+++ src/OpenSHC/Map/Units/UnitsState/standUpIfSeated.cpp
@@ -5,13 +5,13 @@ namespace Map {
     namespace Units {
 
         // FUNCTION: STRONGHOLDCRUSADER 0x0052FBB0
-        BOOLEnum UnitsState::standUpIfSeated(int param_1)
+        BOOLEnum UnitsState::standUpIfSeated(int unitID)
         {
-            if (this->units[param_1].seated == 0) {
+            if (this->units[unitID].seated == 0) {
                 return FALSE;
             }
-            this->units[param_1].substate = 100;
-            this->units[param_1].animationCycleNumber = 0;
+            this->units[unitID].substate = 100;
+            this->units[unitID].animationCycleNumber = 0;
             return TRUE;
         }
 
