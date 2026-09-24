305c6036eb8612244cf13ba077d48cde207d99f6
Gynt <Gynt@users.noreply.github.com>
2026-09-24 16:55:41 +0200

    reimplement: SHC_3BB0A8C1_0x004A4B80 100.00% Reimplemented

--- src/OpenSHC/Map/Navigation/PathFindingState/clearClimbData.cpp
+++ src/OpenSHC/Map/Navigation/PathFindingState/clearClimbData.cpp
@@ -8,10 +8,10 @@ namespace Map {
     namespace Navigation {
 
         // FUNCTION: STRONGHOLDCRUSADER 0x004A4B80
-        void PathFindingState::clearClimbData(int param_1)
+        void PathFindingState::clearClimbData(int climbID)
         {
             MACRO_CALL_MEMBER(OpenSHC::IO::LowLevelMemory_Func::fillMemory_ByteValue, DAT_LowLevelMemory::ptr)(
-                0x204, 0, &this->climbData[param_1]);
+                sizeof(this->climbData[climbID]), 0, &this->climbData[climbID]);
         }
 
     }
