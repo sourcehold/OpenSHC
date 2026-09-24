2e19ed09cf7904b9fbe0d3d13ba42a44ac47b5c7
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:12 +0200

    reimplement: SHC_3BB0A8C1_0x0046CEF0 100.00% Reimplemented

--- src/OpenSHC/Util/Timing/Stopwatch/stop.cpp
+++ src/OpenSHC/Util/Timing/Stopwatch/stop.cpp
@@ -10,14 +10,12 @@ namespace Util {
         // FUNCTION: STRONGHOLDCRUSADER 0x0046CEF0
         void Stopwatch::stop()
         {
-            DWORD _currentTime;
-
-            if (this->running_0x4 != FALSE) {
-                _currentTime = timeGetTime();
-                this->stopTime_0xc = _currentTime;
-                this->running_0x4 = FALSE;
-                this->duration_0x0 = _currentTime - this->startTime_0x8;
+            if (this->running_0x4 == FALSE) {
+                return;
             }
+            this->stopTime_0xc = timeGetTime();
+            this->running_0x4 = FALSE;
+            this->duration_0x0 = this->stopTime_0xc - this->startTime_0x8;
         }
 
     }
