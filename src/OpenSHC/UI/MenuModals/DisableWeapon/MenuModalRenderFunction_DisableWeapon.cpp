a25ac4b07438645bdf032dc585debdb4e4defdf1
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:02 +0200

    reimplement: SHC_3BB0A8C1_0x004ABCE0 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/DisableWeapon/MenuModalRenderFunction_DisableWeapon.cpp
+++ src/OpenSHC/UI/MenuModals/DisableWeapon/MenuModalRenderFunction_DisableWeapon.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x004ABCE0
             void MenuModalRenderFunction_DisableWeapon(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(199, 0xbd, x, y, width);
             }
