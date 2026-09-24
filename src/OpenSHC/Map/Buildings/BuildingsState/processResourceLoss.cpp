50475216955ec6da2edd7ffc583791dbc549466f
Gynt <Gynt@users.noreply.github.com>
2026-09-24 09:29:16 +0200

    reimplement: SHC_3BB0A8C1_0x0041BD70 32.80% Reimplemented functionally, code layout differs

--- src/OpenSHC/Map/Buildings/BuildingsState/processResourceLoss.cpp
+++ src/OpenSHC/Map/Buildings/BuildingsState/processResourceLoss.cpp
@@ -22,69 +22,65 @@ namespace Map {
         void BuildingsState::processResourceLoss(int playerID, ResourceType resourceType, int amount, int param_4)
         {
             // param_4 != 0 only marks the amount to take (someResourceNumber) without removing it
-            bool found = false;
-            if (resourceType != 0) {
-                if (DAT_GameCore::instance.gameMode_2 == OpenSHC::Game::GM_SIEGE_THAT) {
-                    if (param_4 == 0) {
-                        DAT_GameState::instance.playerDataArray[playerID].startResources[resourceType] -= amount;
-                        return;
+            int found = 0;
+            if (resourceType == 0) {
+                return;
+            }
+            if (DAT_GameCore::instance.gameMode_2 == OpenSHC::Game::GM_SIEGE_THAT) {
+                if (param_4 != 0) {
+                    return;
+                }
+                DAT_GameState::instance.playerDataArray[playerID].startResources[resourceType] -= amount;
+                return;
+            }
+            BuildingType storageType
+                = MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::getBuildingStorageTypeForResourceType,
+                    this)(resourceType);
+            if (storageType == BT_MANORHOUSE) {
+                if (param_4 != 0) {
+                    return;
+                }
+                DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] -= amount;
+                return;
+            }
+            for (int i = 1; i < this->maxBuildingsCount; ++i) {
+                if (this->buildings[i].logicalState != BLS_NORMAL || this->buildings[i].buildingType != storageType
+                    || this->buildings[i].owner != playerID || this->buildings[i].resources[resourceType] <= 0) {
+                    continue;
+                }
+                if (param_4 == 0) {
+                    this->buildings[i].someResourceNumber = 0;
+                }
+                int stored = this->buildings[i].resources[resourceType];
+                found = 1;
+                if (stored < amount) {
+                    amount -= stored;
+                    if (param_4 != 0) {
+                        this->buildings[i].someResourceNumber = stored;
+                    } else {
+                        this->buildings[i].resources[resourceType] = 0;
+                        DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] -= stored;
                     }
                 } else {
-                    BuildingType storageType = MACRO_CALL_MEMBER(
-                        OpenSHC::Map::Buildings::BuildingsState_Func::getBuildingStorageTypeForResourceType, this)(
-                        resourceType);
-                    if (storageType == BT_MANORHOUSE) {
-                        if (param_4 == 0) {
-                            DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] -= amount;
-                            return;
-                        }
+                    if (param_4 == 0) {
+                        this->buildings[i].resources[resourceType] = stored - amount;
+                        DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] -= amount;
                     } else {
-                        for (int i = 1; i < this->maxBuildingsCount; ++i) {
-                            if (this->buildings[i].logicalState != BLS_NORMAL
-                                || this->buildings[i].buildingType != storageType
-                                || this->buildings[i].owner != playerID
-                                || this->buildings[i].resources[resourceType] <= 0) {
-                                continue;
-                            }
-                            if (param_4 == 0) {
-                                this->buildings[i].someResourceNumber = 0;
-                            }
-                            int stored = this->buildings[i].resources[resourceType];
-                            found = true;
-                            if (stored < amount) {
-                                amount -= stored;
-                                if (param_4 != 0) {
-                                    this->buildings[i].someResourceNumber = stored;
-                                } else {
-                                    this->buildings[i].resources[resourceType] = 0;
-                                    DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType]
-                                        -= stored;
-                                }
-                            } else {
-                                if (param_4 == 0) {
-                                    this->buildings[i].resources[resourceType] = stored - amount;
-                                    DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType]
-                                        -= amount;
-                                } else {
-                                    this->buildings[i].someResourceNumber = amount;
-                                }
-                                amount = 0;
-                            }
-                            if (param_4 == 0) {
-                                MACRO_CALL_MEMBER(
-                                    OpenSHC::Map::Buildings::BuildingsState_Func::computeResourceSumForBuilding, this)(
-                                    i);
-                                MACRO_CALL_MEMBER(
-                                    OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(playerID);
-                            }
-                            MACRO_CALL_MEMBER(
-                                OpenSHC::Map::TileMapState_Func::updateBuildingGraphicsLayer, DAT_TileMapState::ptr)(i);
-                        }
-                        if (found) {
-                            return;
-                        }
+                        this->buildings[i].someResourceNumber = amount;
                     }
+                    amount = 0;
                 }
+                if (param_4 == 0) {
+                    MACRO_CALL_MEMBER(
+                        OpenSHC::Map::Buildings::BuildingsState_Func::computeResourceSumForBuilding, this)(i);
+                    MACRO_CALL_MEMBER(OpenSHC::Map::Buildings::BuildingsState_Func::countPlayerResources, this)(
+                        playerID);
+                }
+                MACRO_CALL_MEMBER(OpenSHC::Map::TileMapState_Func::updateBuildingGraphicsLayer, DAT_TileMapState::ptr)(
+                    i);
+            }
+            if (found != 0) {
+                return;
             }
             DAT_GameState::instance.playerDataArray[playerID].currentResources[resourceType] = 0;
         }
