ff7e3a7dffbc8b1d62fe73becae7c163789f6765
Gynt <Gynt@users.noreply.github.com>
2026-09-25 01:12:07 +0200

    reimplement: SHC_3BB0A8C1_0x004AB510 54.55% Callee header lacks height arg

--- src/OpenSHC/UI/MenuModals/UnusedSiegeAttackingForceUnk/MenuModalRenderFunction_UnusedSiegeAttackingForceUnk.cpp
+++ src/OpenSHC/UI/MenuModals/UnusedSiegeAttackingForceUnk/MenuModalRenderFunction_UnusedSiegeAttackingForceUnk.cpp
@@ -11,7 +11,6 @@ namespace UI {
             // FUNCTION: STRONGHOLDCRUSADER 0x004AB510
             void MenuModalRenderFunction_UnusedSiegeAttackingForceUnk(int x, int y, int width, int height)
             {
-
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
                     DAT_PencilRenderCore::ptr)(199, 0xa4, x, y, width);
             }
