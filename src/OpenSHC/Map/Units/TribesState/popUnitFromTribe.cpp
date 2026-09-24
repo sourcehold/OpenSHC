be34cda5034ffe77f2070a8cd387445fc9b5aa4c
Gynt <Gynt@users.noreply.github.com>
2026-09-25 00:50:32 +0200

    reimplement: SHC_3BB0A8C1_0x00525B70 100.00% Reimplemented

--- src/OpenSHC/Map/Units/TribesState/popUnitFromTribe.cpp
+++ src/OpenSHC/Map/Units/TribesState/popUnitFromTribe.cpp
@@ -7,12 +7,12 @@ namespace Map {
         // FUNCTION: STRONGHOLDCRUSADER 0x00525B70
         int TribesState::popUnitFromTribe(int tribeID)
         {
-            int _unitID
+            int const unitID
                 = MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::getUnitIDForIndexInTribe, this)(tribeID, 0);
-            if (_unitID != 0) {
-                MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::removeUnitFromTribe, this)(_unitID, tribeID);
+            if (unitID != 0) {
+                MACRO_CALL_MEMBER(OpenSHC::Map::Units::TribesState_Func::removeUnitFromTribe, this)(unitID, tribeID);
             }
-            return _unitID;
+            return unitID;
         }
 
     }
