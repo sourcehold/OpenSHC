699a6d509f2a129eadaf25f71bb2ab67643bad05
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:51 +0200

    reimplement: SHC_3BB0A8C1_0x004ABCE0 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/DisableWeapon/MenuModalRenderFunction_DisableWeapon.cpp
+++ src/OpenSHC/UI/MenuModals/DisableWeapon/MenuModalRenderFunction_DisableWeapon.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_DisableWeapon(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(199, 0xbd, x, y, width);
+                    DAT_PencilRenderCore::ptr)(199, 0xbd, x, y, width, height);
             }
 
         }
