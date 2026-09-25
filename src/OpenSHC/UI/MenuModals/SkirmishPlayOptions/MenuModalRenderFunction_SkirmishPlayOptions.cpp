78913549e1592d70ed7b4af96aad04360b0e1e04
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:54 +0200

    reimplement: SHC_3BB0A8C1_0x004ABE80 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/SkirmishPlayOptions/MenuModalRenderFunction_SkirmishPlayOptions.cpp
+++ src/OpenSHC/UI/MenuModals/SkirmishPlayOptions/MenuModalRenderFunction_SkirmishPlayOptions.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_SkirmishPlayOptions(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(0x4f, 0x67, x, y, width);
+                    DAT_PencilRenderCore::ptr)(0x4f, 0x67, x, y, width, height);
             }
 
         }
