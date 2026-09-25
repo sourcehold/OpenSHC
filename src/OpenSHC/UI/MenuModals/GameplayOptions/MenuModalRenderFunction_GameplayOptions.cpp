7c25bbbdccf82b83895e7a84b8ca12958adef910
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:52 +0200

    reimplement: SHC_3BB0A8C1_0x00491E80 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/GameplayOptions/MenuModalRenderFunction_GameplayOptions.cpp
+++ src/OpenSHC/UI/MenuModals/GameplayOptions/MenuModalRenderFunction_GameplayOptions.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_GameplayOptions(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(0x4a, 0x19, x, y, width);
+                    DAT_PencilRenderCore::ptr)(0x4a, 0x19, x, y, width, height);
             }
 
         }
