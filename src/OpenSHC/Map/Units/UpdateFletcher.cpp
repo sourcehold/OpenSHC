e832e456068d54e6d0d73ea5a774e0530dfbd3e4
Gynt <Gynt@users.noreply.github.com>
2026-09-25 21:39:29 +0200

    reimplement: SHC_3BB0A8C1_0x0054D8F0 31.26% Style pass, asm differs

--- src/OpenSHC/Map/Units/UpdateFletcher.cpp
+++ src/OpenSHC/Map/Units/UpdateFletcher.cpp
@@ -319,7 +319,7 @@ namespace Map {
             DAT_UnitsState::instance.units[unitID].field_0x30_animRelated = 0x10;
             DAT_UnitsState::instance.units[unitID].field131_0x2ac = 1;
             DAT_UnitsState::instance.units[unitID].updateTickTracker += 1;
-            if (DAT_UnitsState::instance.units[unitID].updateTickTracker < 11) {
+            if (DAT_UnitsState::instance.units[unitID].updateTickTracker <= 10) {
                 return;
             }
             DAT_UnitsState::instance.units[unitID].updateTickTracker = 0;
