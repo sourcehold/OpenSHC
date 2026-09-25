7737541cbb42fbfd75fce32d3757ba4ac97f153f
Gynt <Gynt@users.noreply.github.com>
2026-09-25 21:39:27 +0200

    reimplement: SHC_3BB0A8C1_0x0055A800 40.11% Style pass, asm differs

--- src/OpenSHC/Map/Units/UpdateCrusaderArcher.cpp
+++ src/OpenSHC/Map/Units/UpdateCrusaderArcher.cpp
@@ -222,7 +222,7 @@ namespace Map {
                 != FALSE) {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].facingDirection = 4;
                 if ((DAT_GameCore::instance.mapTimeInTicks + DAT_CurrentUnitSlotID::instance) % 40 == 0) {
-                    if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].closestEnemyMicroDistance < 433
+                    if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].closestEnemyMicroDistance <= 432
                         && MACRO_CALL_MEMBER(OpenSHC::Map::Units::UnitsState_Func::acquireShootTarget,
                                DAT_UnitsState::ptr)(DAT_CurrentUnitSlotID::instance)
                             != FALSE) {
@@ -269,7 +269,7 @@ namespace Map {
             } else {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationSheetFrameOffset = 1;
                 if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].moveInstructionSpeedDelayTracker
-                    < 71) {
+                    <= 70) {
                     DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].stateBasedSpeed = 0;
                 } else {
                     DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].stateBasedSpeed = -1;
@@ -370,7 +370,7 @@ namespace Map {
             DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationSheetFrameOffset = 1;
             if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].moveInstructionSpeedDelayTracker == 0
                 || DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].moveInstructionSpeedDelayTracker
-                    < 71) {
+                    <= 70) {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].stateBasedSpeed = 0;
             } else {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].stateBasedSpeed = -1;
@@ -1213,7 +1213,7 @@ namespace Map {
                 }
             }
             DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber = 0;
-            if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].closestEnemyMicroDistance < 401
+            if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].closestEnemyMicroDistance <= 400
                 || DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].targetingType
                     == OpenSHC::Map::Units::UIT_UNIT_ATTACK_UNIT
                 || DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].targetingType
