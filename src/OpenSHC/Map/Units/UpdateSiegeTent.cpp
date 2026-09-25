8d684a961f8ea6028435fcddff64aaea2420698e
Gynt <Gynt@users.noreply.github.com>
2026-09-25 20:35:10 +0200

    reimplement: SHC_3BB0A8C1_0x005307B0 69.44% restyled

--- src/OpenSHC/Map/Units/UpdateSiegeTent.cpp
+++ src/OpenSHC/Map/Units/UpdateSiegeTent.cpp
@@ -19,57 +19,38 @@ namespace Map {
 
     // FUNCTION: STRONGHOLDCRUSADER 0x005307B0
     void Units::UpdateSiegeTent()
-
     {
-
         DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].field_0x30_animRelated = 0;
-
-        int uVar5 = DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].fixedRng + 1 & 3;
-
-        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].gfxNumber = uVar5;
-
-        if (4 <= uVar5) {
-
+        int frame = DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].fixedRng + 1 & 3;
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].gfxNumber = frame;
+        if (frame >= 4) {
             DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].gfxNumber = 2;
         }
-
-        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber
-            = DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber + 1;
-
-        if (500 < DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber) {
-
-            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].logicalState
-                = OpenSHC::Map::Units::ULS_TRANSITIONING;
-
-            if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].fixedRng % 4 < 2) {
-
-                DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].unitTypeToChangeInto
-                    = OpenSHC::Map::Units::UT_S_CATAPULT;
-            } else {
-
-                DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].unitTypeToChangeInto
-                    = OpenSHC::Map::Units::UT_S_TREBUCHET;
-            }
-
-            int iVar4 = DAT_GameState::instance.playerDataArray[1].keep.yEntry;
-
-            int iVar3 = DAT_GameState::instance.playerDataArray[1].keep.xEntry;
-
-            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].state_2 = 6;
-
-            MACRO_CALL_MEMBER(
-                OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::calculatePreferredRelativeOrientation,
-                DAT_DirectionAlgorithmState::ptr)(
-                (int)DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].x,
-                (int)((int)(DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].y)), iVar3, iVar4);
-
-            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].facingDirection
-                = DAT_DirectionAlgorithmState::instance.orientation;
-
-            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].field248_0x3bc = 0;
+        // the tent turns into a siege engine after 500 ticks
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber += 1;
+        if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].animationCycleNumber <= 500) {
+            return;
         }
-
-        return;
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].logicalState
+            = OpenSHC::Map::Units::ULS_TRANSITIONING;
+        if (DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].fixedRng % 4 < 2) {
+            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].unitTypeToChangeInto
+                = OpenSHC::Map::Units::UT_S_CATAPULT;
+        } else {
+            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].unitTypeToChangeInto
+                = OpenSHC::Map::Units::UT_S_TREBUCHET;
+        }
+        // face the keep of player 1
+        int keepY = DAT_GameState::instance.playerDataArray[1].keep.yEntry;
+        int keepX = DAT_GameState::instance.playerDataArray[1].keep.xEntry;
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].state_2 = 6;
+        MACRO_CALL_MEMBER(OpenSHC::Map::Navigation::DirectionAlgorithmState_Func::calculatePreferredRelativeOrientation,
+            DAT_DirectionAlgorithmState::ptr)(DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].x,
+            DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].y,
+            keepX, keepY);
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].facingDirection
+            = DAT_DirectionAlgorithmState::instance.orientation;
+        DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].field248_0x3bc = 0;
     }
 
 }
