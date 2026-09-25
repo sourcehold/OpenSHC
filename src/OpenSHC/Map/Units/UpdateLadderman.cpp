3631a97d4b4ede3486f919abac1c7722799750d4
Gynt <Gynt@users.noreply.github.com>
2026-09-25 22:15:07 +0200

    reimplement: SHC_3BB0A8C1_0x00563910 37.35% Style pass, asm differs

--- src/OpenSHC/Map/Units/UpdateLadderman.cpp
+++ src/OpenSHC/Map/Units/UpdateLadderman.cpp
@@ -317,7 +317,7 @@ namespace Map {
             if ((char)DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].disappearFadeAlphaCountdown < 0) {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].disappearFadeAlphaCountdown = 0;
             } else if ((char)DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].disappearFadeAlphaCountdown
-                > 31) {
+                >= 32) {
                 DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].disappearFadeAlphaCountdown = 31;
             }
             DAT_UnitsState::instance.units[DAT_CurrentUnitSlotID::instance].updateTickTracker += 1;
