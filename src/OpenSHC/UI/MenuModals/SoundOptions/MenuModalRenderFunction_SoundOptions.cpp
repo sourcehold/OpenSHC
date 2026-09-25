336d6228a9b72836d0ba2cab055f27d0cd492f66
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:54 +0200

    reimplement: SHC_3BB0A8C1_0x00492660 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/SoundOptions/MenuModalRenderFunction_SoundOptions.cpp
+++ src/OpenSHC/UI/MenuModals/SoundOptions/MenuModalRenderFunction_SoundOptions.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_SoundOptions(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(0x4a, 5, x, y, width);
+                    DAT_PencilRenderCore::ptr)(0x4a, 5, x, y, width, height);
             }
 
         }
