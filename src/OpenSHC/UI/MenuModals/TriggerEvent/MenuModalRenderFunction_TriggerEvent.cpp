ba6fe13f07527a46139b19512bc813e075052c8c
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:55 +0200

    reimplement: SHC_3BB0A8C1_0x004AC250 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/TriggerEvent/MenuModalRenderFunction_TriggerEvent.cpp
+++ src/OpenSHC/UI/MenuModals/TriggerEvent/MenuModalRenderFunction_TriggerEvent.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_TriggerEvent(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(199, 0x17, x, y, width);
+                    DAT_PencilRenderCore::ptr)(199, 0x17, x, y, width, height);
             }
 
         }
