b7a2b67909023b29446181c56d8cba48b2ad921e
Gynt <Gynt@users.noreply.github.com>
2026-09-25 06:42:51 +0200

    reimplement: SHC_3BB0A8C1_0x00491BF0 100.00% Reimplemented

--- src/OpenSHC/UI/MenuModals/EnterTitleOnGameStart/MenuModalRenderFunction_EnterTitleOnGameStart.cpp
+++ src/OpenSHC/UI/MenuModals/EnterTitleOnGameStart/MenuModalRenderFunction_EnterTitleOnGameStart.cpp
@@ -12,7 +12,7 @@ namespace UI {
             void MenuModalRenderFunction_EnterTitleOnGameStart(int x, int y, int width, int height)
             {
                 MACRO_CALL_MEMBER(OpenSHC::UI::Rendering::PencilRenderCore_Func::drawHeaderTextBanner,
-                    DAT_PencilRenderCore::ptr)(5, 6, x, y, width);
+                    DAT_PencilRenderCore::ptr)(5, 6, x, y, width, height);
             }
 
         }
